b58d3dfff611fa4d82ec4a57ca83422fa69e80ad
teramotodaiki <i101323@gmail.com>
2015-04-07 08:36:25 +0900

    Code viewer in admin page
    管理者ページ /a にて、投稿されたステージのCodeを表示する機能を追加。モーダルで表示する。

--- a/view/main.php
+++ a/view/main.php
@@ -7,24 +7,46 @@
 	<title>あそべるプログラミング hackforplay</title>
 	<?php require_once '../library.php' ?>
 	<script type="text/javascript" charset="utf-8">
-$(function(){
-	$("button.accept-button").on('click', function() {
-		$(this).attr('disabled', 'disabled');
-		var $div = $(this).parent();
-		$.post('../makestage.php', {
-            'stage_name':$div.children('.stage_name').text(),
-            'restaging_id':$div.children('.rid').text()
-        }, function(data, textStatus, xhr) {
-        	location.href = "/a";
-        });
+	$(function(){
+		$("button.accept-button").on('click', function() {
+			$(this).attr('disabled', 'disabled');
+			var $div = $(this).parent();
+			$.post('../makestage.php', {
+	            'stage_name':$div.children('.stage_name').text(),
+	            'restaging_id':$div.children('.rid').text()
+	        }, function(data, textStatus, xhr) {
+	        	location.href = "/a";
+	        });
+		});
+		$("button.code-button").on('click', function() {
+			var code = $(this).parent().children('.code').text();
+			$('.modal-body>pre').text(code);
+		});
+
 	});
-});
 	</script>
 </head>
 <body class="">
 	<?php require_once '../analyticstracking.php' ?>
 	<?php require_once '../fb-root.php' ?>
 	<?php require_once '../view/header.php'; ?>
+	<!-- Modal -->
+	<div class="modal fade" id="codeModal" tabindex="-1" role="dialog" aria-labelledby="codeModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+	    		<div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		    	</div>
+			    <div class="modal-body">
+			    	<pre style="font-family: monospace;"></pre>
+			    </div>
+	    		<div class="modal-footer">
+	        		<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+	    		</div>
+			</div>
+		</div>
+	</div>
+	<!-- contents -->
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
@@ -35,14 +57,17 @@ $(function(){
 					<div class="col-md-12 h4p_box-main">
 						<div class="row">
 							<!-- stages list with PHP -->
-							<?php foreach ($restagingcodes as $key => $value) : ?>
+							<?php foreach ($restagingcodes as $key => $value) :
+							 ?>
 							<div class="col-md-3" style="border: 1px solid rgb(224, 224, 224); padding:10px">
 								<img src="<?php echo $value['thumbnail']; ?>" style="width:100%;" alt="">
 								<h4 class="stage_name"><?php echo $value['stage_name']; ?></h4>
 								<h5><?php echo $value['author']; ?></h5>
 								<h5><?php echo $value['time']; ?></h5>
 								<span class="rid hidden"><?php echo $value['id']; ?></span>
+								<span class="code hidden"><?php echo $value['code']; ?></span>
 								<button type="button" class="btn btn-primary btn-block accept-button">Accept</button>
+								<button type="button" class="btn btn-success btn-block code-button" data-toggle="modal" data-target="#codeModal">Code</button>
 							</div>
 							<?php endforeach; ?>
 						</div>
