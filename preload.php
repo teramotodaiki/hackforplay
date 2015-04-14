69f1e749ac6eb188925a226a67eea55df4dd0139
teramotodaiki <i101323@gmail.com>
2015-04-14 16:04:09 +0900

    Debug 一時的に前のDBを使用

--- preload.php
+++ preload.php
@@ -8,8 +8,8 @@ try {
 	if(($useLocalDB && $_SERVER['SERVER_NAME'] == 'localhost')){
 		$pdo = new PDO('mysql:dbname=hackforplay;host=localhost;charset=utf8;', 'hackforplay', 'RtPF7JRSZ5XzFasc');
 	}else{
-		// $pdo = new PDO('mysql:dbname=hackforplay;host=ja-cdbr-azure-west-a.cloudapp.net;charset=utf8;', 'ba714e6056fb6c', 'd66371c3');
-	   	$pdo = new PDO ( "sqlsrv:server = tcp:yadw63xtf8.database.windows.net,1433; Database = hackforplay", "hackforplay@yadw63xtf8", "9PFLn21u9TkiqlKx3ceAbawXSGsBPGT");
+		$pdo = new PDO('mysql:dbname=hackforplay;host=ja-cdbr-azure-west-a.cloudapp.net;charset=utf8;', 'ba714e6056fb6c', 'd66371c3');
+	   	// $pdo = new PDO ( "sqlsrv:server = tcp:yadw63xtf8.database.windows.net,1433; Database = hackforplay", "hackforplay@yadw63xtf8", "9PFLn21u9TkiqlKx3ceAbawXSGsBPGT");
     	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
 }
