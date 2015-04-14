e5491c2a8d7bad0258ed1e6c63a1def9035df02d
teramotodaiki <i101323@gmail.com>
2015-04-14 17:44:47 +0900

    Debug テーブル作成

---
+++ gen.php
@@ -0,0 +1,137 @@
+<?php
+require_once 'preload.php';
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+</head>
+<body>
+<?php
+
+$res = $pdo->query("CREATE TABLE `challenge_reason` (
+`id` int(11) NOT NULL COMMENT 'relation challenge_vote.reason_id',
+  `user_id` int(11) DEFAULT NULL COMMENT 'writer''s ID',
+  `text` varchar(255) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `time` datetime NOT NULL COMMENT 'written time'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `challenge_vote` (
+`id` int(11) NOT NULL,
+  `code_id` int(11) NOT NULL,
+  `stage_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `reason_id` int(11) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Vote to the reason or write new. Text of reasons are in the `challenge_reason`.' AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `code` (
+`id` int(11) NOT NULL,
+  `play_id` int(11) NOT NULL,
+  `stage_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `raw` text NOT NULL,
+  `time` datetime NOT NULL,
+  `error` tinyint(1) NOT NULL,
+  `voted` int(11) NOT NULL DEFAULT '0',
+  `displayed` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `play` (
+`id` int(11) NOT NULL,
+  `token` varchar(32) NOT NULL COMMENT 'unique hex(16bit) each play',
+  `user_id` int(11) NOT NULL,
+  `stage_id` int(11) NOT NULL COMMENT 'stage.id',
+  `clear` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'have cleared',
+  `begin` datetime NOT NULL COMMENT 'when you begin playing',
+  `finish` datetime DEFAULT NULL COMMENT 'when WILL you clear, be NULL before'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Add a row by a playing' AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `restaging` (
+`id` int(11) NOT NULL,
+  `play_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `stage_id` int(11) NOT NULL,
+  `code` text NOT NULL,
+  `time` datetime NOT NULL,
+  `origin_id` int(11) DEFAULT NULL COMMENT 'restaging.id',
+  `author` text NOT NULL,
+  `stage_name` text NOT NULL,
+  `thumbnail` text
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='restaging codes' AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `stage` (
+`id` int(11) NOT NULL,
+  `path` text NOT NULL COMMENT 'directory path',
+  `title` text NOT NULL COMMENT 'max 100 chars',
+  `next` int(11) DEFAULT NULL,
+  `playcount` int(11) NOT NULL DEFAULT '0',
+  `playable` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Can user play this',
+  `type` enum('official','replay') NOT NULL DEFAULT 'official' COMMENT '0:official, 1:replay',
+  `restaging_id` int(11) DEFAULT NULL COMMENT 'restaging.id'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='information of each stages' AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("CREATE TABLE `user` (
+`id` int(11) NOT NULL,
+  `key` varchar(32) NOT NULL COMMENT 'cookie has. made from hex',
+  `beta` tinyint(1) NOT NULL COMMENT 'Are you a beta-user?',
+  `begin` datetime NOT NULL,
+  `last` datetime DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='(beta)User information identify in the key generated on server.' AUTO_INCREMENT=0;");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `challenge_reason`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `text` (`text`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `challenge_vote`
+	ADD PRIMARY KEY (`id`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `code`
+ ADD PRIMARY KEY (`id`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `play`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `token` (`token`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `restaging`
+ ADD PRIMARY KEY (`id`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `stage`
+ ADD PRIMARY KEY (`id`);");
+var_dump($res);
+echo "<br><br>";
+
+$res = $pdo->query("ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `key` (`key`);");
+var_dump($res);
+echo "<br><br>";
+
+ ?>
+</body>
+</html>
