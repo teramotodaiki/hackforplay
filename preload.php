6ceef6d643c8aabceab122995a3353a215cc9114
teramotodaiki <i101323@gmail.com>
2015-04-14 15:54:02 +0900

    Debug Userに@以下を追加

--- preload.php
+++ preload.php
@@ -3,14 +3,15 @@
 
 // 1.Generate PDO object (connecting mysql)
 $pdo = null;
-$useLocalDB = true;
+$useLocalDB = false;
 try {
 	if(($useLocalDB && $_SERVER['SERVER_NAME'] == 'localhost')){
 		$pdo = new PDO('mysql:dbname=hackforplay;host=localhost;charset=utf8;', 'hackforplay', 'RtPF7JRSZ5XzFasc');
 	}else{
-		$pdo = new PDO('mysql:dbname=hackforplay;host=ja-cdbr-azure-west-a.cloudapp.net;charset=utf8;', 'ba714e6056fb6c', 'd66371c3');
-	}
-    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		// $pdo = new PDO('mysql:dbname=hackforplay;host=ja-cdbr-azure-west-a.cloudapp.net;charset=utf8;', 'ba714e6056fb6c', 'd66371c3');
+	   	$pdo = new PDO ( "sqlsrv:server = tcp:yadw63xtf8.database.windows.net,1433; Database = hackforplay", "hackforplay@yadw63xtf8", "9PFLn21u9TkiqlKx3ceAbawXSGsBPGT");
+    	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
 }
 catch ( PDOException $e ) {
     print( "Error connecting to SQL Server." );
