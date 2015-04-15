6e45734fbb9e58f39bbafb65a0f42780f0048c51
teramotodaiki <i101323@gmail.com>
2015-04-15 16:53:38 +0900

    現在時刻を表示するテスト

--- view/main.php
+++ view/main.php
@@ -105,7 +105,7 @@
 			</div>
 		</div>
 	</div>
-	<?php var_dump($user); ?>
+	<?php echo date("Y-m-d H:i:s"); ?>
 	<div class="landingpage">
 		<div class="h4p_landing l-1">
 			<div class="h4p_landing-main">
