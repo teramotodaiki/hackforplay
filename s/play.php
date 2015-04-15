65315901f891ac175dd0b53879fc23de17e1d35e
teramotodaiki <i101323@gmail.com>
2015-04-15 15:46:11 +0900

    ステージプレイ時のPDOエラーをキャッチ

--- s/play.php
+++ s/play.php
@@ -10,29 +10,30 @@ if(!isset($id) || !$id){
 }
 
 // 3.Get stage information
-$stmt	= $pdo->prepare("SELECT * FROM `stage` WHERE `id`=:id;");
-$stmt->bindValue(":id", $id, PDO::PARAM_INT);
-$stmt->execute();
-$stage	= $stmt->fetch(PDO::FETCH_ASSOC);
-if(!$stage || !$stage['playable']){
-	// no stage, go to top page
-	header('Location:../');
-	exit();
+try{
+	$stmt	= $pdo->prepare("SELECT * FROM `stage` WHERE `id`=:id;");
+	$stmt->bindValue(":id", $id, PDO::PARAM_INT);
+	$stmt->execute();
+	$stage	= $stmt->fetch(PDO::FETCH_ASSOC);
+	if(!$stage || !$stage['playable']){
+		// no stage, go to top page
+		header('Location:../');
+		exit();
+	}
+}catch(PDOException $e){
+	die(print_r($e));
 }
-
-// 3-1.Get next stage id
-// $stmt	= $pdo->prepare("SELECT `id` FROM `stage` WHERE `id`>:id AND `playable`=:playable ORDER BY `id` LIMIT 1");
-// $stmt->bindValue(":id", $id, PDO::PARAM_INT);
-// $stmt->bindValue(":playable", 1, PDO::PARAM_BOOL);
-// $stmt->execute();
-// $next = $stmt->fetch(PDO::FETCH_COLUMN);
 
 if(isset($stage['restaging_id'])){
 	// 3-2.Get restaging information
-	$stmt	= $pdo->prepare("SELECT * FROM `restaging` WHERE `id`=:id");
-	$stmt->bindValue(":id", $stage['restaging_id'], PDO::PARAM_INT);
-	$stmt->execute();
-	$restaging = $stmt->fetch(PDO::FETCH_ASSOC);
+	try{
+		$stmt	= $pdo->prepare("SELECT * FROM `restaging` WHERE `id`=:id");
+		$stmt->bindValue(":id", $stage['restaging_id'], PDO::PARAM_INT);
+		$stmt->execute();
+		$restaging = $stmt->fetch(PDO::FETCH_ASSOC);
+	}catch(PDOException $e){
+		die(print_r($e));
+	}
 }
 
 // 4.Generate token
@@ -42,28 +43,40 @@ $token	= bin2hex($bytes); // binaly to hex
 // 5.Record this playing
 if(isset($key)){
 	// 5-1.Get user id
-	$stmt = $pdo->prepare("SELECT `id` FROM `user` WHERE `key`=:key");
-	$stmt->bindValue(":key", $key, PDO::PARAM_STR);
-	$stmt->execute();
-	$user = $stmt->fetch(PDO::FETCH_ASSOC);
+	try{
+		$stmt = $pdo->prepare("SELECT `id` FROM `user` WHERE `key`=:key");
+		$stmt->bindValue(":key", $key, PDO::PARAM_STR);
+		$stmt->execute();
+		$user = $stmt->fetch(PDO::FETCH_ASSOC);
+	}catch(PDOException $e){
+		die(print_r($e));
+	}
 	if(!isset($user['id'])){
 		// Missing user
 	} else {
 		// 5-2.Record token-user_id pair
-		$stmt = $pdo->prepare("INSERT INTO `play` (`id`,`token`,`user_id`,`stage_id`,`begin`) VALUES(NULL, :token, :user_id, :stage_id, :begin)");
-		$stmt->bindValue(":token", $token, PDO::PARAM_STR);
-		$stmt->bindValue(":user_id", $user['id'], PDO::PARAM_INT);
-		$stmt->bindValue(":stage_id", $stage['id'], PDO::PARAM_INT);
-		$stmt->bindValue(":begin", date("Y-m-d H:i:s"), PDO::PARAM_STR);
-		$flag = $stmt->execute();
+		try{
+			$stmt = $pdo->prepare("INSERT INTO `play` (`id`,`token`,`user_id`,`stage_id`,`begin`) VALUES(NULL, :token, :user_id, :stage_id, :begin)");
+			$stmt->bindValue(":token", $token, PDO::PARAM_STR);
+			$stmt->bindValue(":user_id", $user['id'], PDO::PARAM_INT);
+			$stmt->bindValue(":stage_id", $stage['id'], PDO::PARAM_INT);
+			$stmt->bindValue(":begin", date("Y-m-d H:i:s"), PDO::PARAM_STR);
+			$flag = $stmt->execute();
+		}catch(PDOException $e){
+			die(print_r($e));
+		}
 		if(!$flag){
 			// Failed to record
 			$token = "";
 		}else{
 			// increase playcount
-			$stmt = $pdo->prepare("UPDATE `stage` SET `playcount`=`playcount`+1 WHERE `id`=:id");
-			$stmt->bindValue(":id", $id, PDO::PARAM_INT);
-			$stmt->execute();
+			try{
+				$stmt = $pdo->prepare("UPDATE `stage` SET `playcount`=`playcount`+1 WHERE `id`=:id");
+				$stmt->bindValue(":id", $id, PDO::PARAM_INT);
+				$stmt->execute();
+			}catch(PDOException $e){
+				die(print_r($e));
+			}
 		}
 	}
 }
