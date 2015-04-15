665a832d326acd15a200b6a67cb4b46a8b8376d5
teramotodaiki <i101323@gmail.com>
2015-04-15 15:01:10 +0900

    SQLの列の名前などを " でクォート

--- signin.php
+++ signin.php
@@ -13,7 +13,7 @@ if(!isset($key)){ // have not set
 	setcookie('key', $key, time() + (365*24*60*60)); // save for a year
 	// 2a-3.Sign up
 	try{
-		$stmt = $pdo->prepare("INSERT INTO [user] ([key],[beta],[begin],[last]) VALUES (:key, :beta, :begin, NULL);");
+		$stmt = $pdo->prepare('INSERT INTO "user" ("key","beta","begin","last") VALUES (:key, :beta, :begin, NULL)');
 		$stmt->bindValue(":key", $key, PDO::PARAM_STR);
 		$stmt->bindValue(":beta", true, PDO::PARAM_BOOL);
 		$stmt->bindValue(":begin", date("Y-m-d H:i:s"), PDO::PARAM_STR);
@@ -38,25 +38,25 @@ $user = null;
 if(isset($key)){
 	// 3-1.Check the presence of you
 	try{
-		$stmt 	= $pdo->prepare("SELECT * FROM [user] WHERE [key]=:key;");
+		$stmt 	= $pdo->prepare('SELECT * FROM "user" WHERE "key"=:key');
 		$stmt->bindValue(":key", $key, PDO::PARAM_STR);
 		$flag	= $stmt->execute();
 		$user	= $stmt->fetch(PDO::FETCH_ASSOC);
 		if(isset($user['id'])){
 			// 3-2.Update information
-			$stmt	= $pdo->prepare("UPDATE [user] SET [last]=:last WHERE [id]=:id;");
+			$stmt	= $pdo->prepare('UPDATE "user" SET "last"=:last WHERE "id"=:id');
 			$stmt->bindValue(":last", date("Y-m-d H:i:s"), PDO::PARAM_STR);
 			$stmt->bindValue(":id", $user['id'], PDO::PARAM_INT);
 			$flag	= $stmt->execute();
 		} else {
 			// Missing user data, add new row
-			$stmt = $pdo->prepare("INSERT INTO [user] ([key],[beta],[begin],[last]) VALUES (:key, :beta, :begin, NULL);");
+			$stmt = $pdo->prepare('INSERT INTO "user" ("key","beta","begin","last") VALUES (:key, :beta, :begin, NULL)');
 			$stmt->bindValue(":key", $key, PDO::PARAM_STR);
 			$stmt->bindValue(":beta", true, PDO::PARAM_BOOL);
 			$stmt->bindValue(":begin", date("Y-m-d H:i:s"), PDO::PARAM_STR);
 			$flag = $stmt->execute();
 			if($flag){
-				$stmt 	= $pdo->prepare("SELECT * FROM [user] WHERE [key]=:key;");
+				$stmt 	= $pdo->prepare('SELECT * FROM "user" WHERE "key"=:key');
 				$stmt->bindValue(":key", $key, PDO::PARAM_STR);
 				$stmt->execute();
 				$user	= $stmt->fetch(PDO::FETCH_ASSOC);
