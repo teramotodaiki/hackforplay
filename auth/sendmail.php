e472d6ce69cbfa6402b94bc5231841c9c9051ebb
teramotodaiki <i101323@gmail.com>
2015-04-23 10:30:33 +0900

    メールの内容を変更
    mb_send_mail から mail へ

--- auth/sendmail.php
+++ auth/sendmail.php
@@ -1,17 +1,16 @@
 <?php
 $to      = $email;
-$subject = date("s").'メールアドレスの登録 - HackforPlay';
+$subject = 'メールアドレスの登録 - HackforPlay';
 $message =
 	// 'こんにちは。ハックフォープレイ開発者の寺本大輝です！' .
 	// 'このメールは、 https://hackforplay.xyz にご入力いただいたメールアドレスに対して送信されています。' .
 	// 'このまま登録する場合は、次の仮パスワードをハックフォープレイで入力するか、リンクにアクセスしてください。' .
-	'仮パスワード：' . $tmpkey . '' .
-	'リンク：（準備中）' . date("s");
-$headers = 'From: admin@hackforplay.xyz \r\n' . 'X-Mailer: PHP/' . phpversion();
+	'仮パスワード：' . $tmpkey . '\n' .
+	'リンク：（準備中）' . date("c");
+$headers = 'From: noreply@hackforplay.xyz \r\n' . 'X-Mailer: PHP/' . phpversion();
 
-$result = mb_send_mail($to, $subject, $message, $headers);
+$result = mail($to, $subject, $message, $headers);
 if(!$result){
 	die("sendmail-error");
 }
-echo $email;
 ?>
--- auth/signup.php
+++ auth/signup.php
@@ -34,20 +34,7 @@ $tmpkey	= bin2hex($bytes); // binaly to hex
 $hashed = password_hash($tmpkey, PASSWORD_DEFAULT);
 
 // メール送信
-$to      = $email;
-$subject = date("s").'メールアドレスの登録 - HackforPlay';
-$message =
-	// 'こんにちは。ハックフォープレイ開発者の寺本大輝です！' .
-	// 'このメールは、 https://hackforplay.xyz にご入力いただいたメールアドレスに対して送信されています。' .
-	// 'このまま登録する場合は、次の仮パスワードをハックフォープレイで入力するか、リンクにアクセスしてください。' .
-	'仮パスワード：' . $tmpkey . '' .
-	'リンク：（準備中）' . date("s");
-$headers = 'From: admin@hackforplay.xyz \r\n' . 'X-Mailer: PHP/' . phpversion();
-
-$result = mb_send_mail($to, $subject, $message, $headers);
-if(!$result){
-	die("sendmail-error");
-}
+require_once 'sendmail.php';
 
 // ユーザーを追加
 try {
