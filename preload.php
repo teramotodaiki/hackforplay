6fab249b6c086b18eab95e76979a8832dcb31c51
teramotodaiki <i101323@gmail.com>
2015-04-24 17:49:17 +0900

    DBの名称を変更
    `hackforplay` → `hackforplay-localhost`, `hackforplay-old` →
    `hackforplay`
    
    `hackforplay-staging`と`hackforplay-localhost`が対応。

--- preload.php
+++ preload.php
@@ -6,12 +6,11 @@ $pdo = null;
 $useLocalDB = true;
 try {
 	if(($useLocalDB && $_SERVER['SERVER_NAME'] == 'localhost')){
-		$pdo = new PDO('mysql:dbname=hackforplay-old;host=localhost;charset=utf8;', 'hackforplay', 'RtPF7JRSZ5XzFasc');
+		$pdo = new PDO('mysql:dbname=hackforplay;host=localhost;charset=utf8;', 'hackforplay', 'RtPF7JRSZ5XzFasc');
 		$pdo->exec("SET sql_mode='ANSI_QUOTES'");
-		$dbh 	= new PDO("mysql:dbname=hackforplay;host=localhost;charset=utf8", 'hackforplay', 'RtPF7JRSZ5XzFasc');
+		$dbh 	= new PDO("mysql:dbname=hackforplay-localhost;host=localhost;charset=utf8", 'hackforplay', 'RtPF7JRSZ5XzFasc');
 		$dbh->exec("SET sql_mode='ANSI_QUOTES'");
 	}elseif($_SERVER['SERVER_NAME'] == 'hackforplay-staging.azurewebsites.net'){
-		// $pdo = new PDO('mysql:dbname=hackforplay;host=ja-cdbr-azure-west-a.cloudapp.net;charset=utf8;', 'ba714e6056fb6c', 'd66371c3');
     	$pdo = new PDO ( "sqlsrv:server = tcp:yadw63xtf8.database.secure.windows.net,1433; Database = hackforplay-release", "hackforplay@yadw63xtf8", "9PFLn21u9TkiqlKx3ceAbawXSGsBPGT");
     	$dbh = new PDO ( "sqlsrv:server = tcp:yadw63xtf8.database.secure.windows.net,1433; Database = hackforplay-staging", "hackforplay@yadw63xtf8", "9PFLn21u9TkiqlKx3ceAbawXSGsBPGT");
 	}
