1025247215b0ed6e16def9de6d2341796684cf32
teramotodaiki <i101323@gmail.com>
2015-05-02 17:47:09 +0900

    ユーザー情報変更のUI
    タブが開かれたとき、ユーザー情報を非同期で取得して、ボタンをアクティブにする

--- p/view.js
+++ p/view.js
@@ -2,5 +2,46 @@
 Preferences のビューを更新するスクリプト
 */
 $(function(){
-	
+	$('[data-toggle="tooltip"]').tooltip();
+
+	// ユーザー情報（Ajaxで取得して、ボタンをアクティブにする）
+	$('a[data-toggle="tab"][aria-controls="user"]').on('show.bs.tab', function(event) {
+		var $f = $('#usersettings');
+		$f.find('button').attr('disabled', true);
+
+		$.get('../auth/getmyinfo.php', function(data) {
+			console.log(data);
+			switch(data){
+				case 'no-session':
+					$('#signinModal').modal('show');
+					break;
+				case 'parse-error':
+					console.log(data);
+					break;
+				default:
+					var info = jQuery.parseJSON(data);
+					$f.find('#age').val(info.age);
+					$f.find('input[name="gender"][value="' + info.gender + '"]').attr('checked', true);
+					$f.find('#nickname').val(info.nickname);
+					$f.find('button').attr('disabled', false);
+					break;
+			}
+		});
+	}).on('hide.bs.tab', function(event) {
+		$('#usersettings button').attr('disabled', true);
+	});
+	$('a[data-toggle="tab"][aria-controls="user"]').tab('show');
+
+	// パスワードの再設定（Validationしてボタンをアクティブにする）
+
+
+	// サインインしたら、そのタブをもう一度開く
+	$("#authModal,#signinModal").on('hide.bs.modal', function(){
+		checkSigninSession(function(result){
+			if(result === 'success'){
+				// 現在のタブを再度表示（リロード）
+				$('a[data-toggle="tab"][aria-expanded="true"]').tab('show');
+			}
+		});
+	});
 });
--- p/view.php
+++ p/view.php
@@ -12,9 +12,11 @@ Preferences のビュー
 	<?php require_once '../library.php' ?>
 </head>
 <body class="">
-	<?php require_once '../analyticstracking.php' ?>
-	<?php require_once '../fb-root.php' ?>
+	<?php require_once '../analyticstracking.php'; ?>
+	<?php require_once '../fb-root.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
 	<?php require_once '../view/header.php'; ?>
+	<script src="view.js" type="text/javascript" charset="utf-8"></script>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
@@ -23,12 +25,80 @@ Preferences のビュー
 						<h3 class="panel-title">設定</h3>
 					</div>
 					<ul class="col-md-3 nav nav-pills nav-stacked panel-body" role="tablist">
-					    <li role="presentation" class="active"><a href="#user" aria-controls="user" role="tab" data-toggle="tab">ユーザー情報</a></li>
-					    <li role="presentation"><a href="#password" aria-controls="password" role="tab" data-toggle="tab">パスワード</a></li>
+					    <li role="presentation">
+					    	<a href="#user" aria-controls="user" role="tab" data-toggle="tab">ユーザー情報</a>
+					    </li>
+					    <li role="presentation">
+					    	<a href="#setpassword" aria-controls="setpassword" role="tab" data-toggle="tab">パスワード</a>
+					    </li>
 					</ul>
 					<div class="col-md-9 tab-content panel-body">
-					    <div role="tabpanel" class="tab-pane active" id="user">ユーザー情報</div>
-					    <div role="tabpanel" class="tab-pane" id="password">This is Profile</div>
+					    <div role="tabpanel" class="tab-pane active" id="user">
+					    	<form id="usersettings" class="form-horizontal">
+					    		<p class="alert alert-danger hide" role="alert"></p>
+							  	<div class="form-group has-feedback">
+							    	<label for="nickname" class="col-sm-3 control-label">ニックネーム</label>
+							    	<div class="col-sm-8">
+							    		<input type="text" class="form-control" id="nickname">
+							    	</div>
+							    	<div class="col-sm-1" data-toggle="tooltip" data-placement="left" title="本名は使用できません">
+							    		<span class="glyphicon glyphicon-question-sign form-control-feedback"></span>
+							    	</div>
+							  	</div>
+							  	<div class="form-group has-feedback">
+							  		<label class="col-sm-3 control-label" for="gender">性別</label>
+							    	<div id="gender" class="col-sm-8">
+								    	<label class="radio-inline"><input type="radio" name="gender" value="man">男</label>
+								  		<label class="radio-inline"><input type="radio" name="gender" value="woman">女</label>
+							    	</div>
+							  	</div>
+							  	<div class="form-group has-feedback">
+							  		<label for="age" class="col-sm-3 control-label">年齢</label>
+							    	<div class="col-sm-8">
+							    		<input type="number" class="form-control" id="age" value="">
+							    	</div>
+							    	<div class="col-sm-1" data-toggle="tooltip" data-placement="left" title="半角の数値を入力してください">
+							    		<span class="glyphicon glyphicon-question-sign form-control-feedback"></span>
+							    	</div>
+							  	</div>
+							  	<div class="text-right">
+								  	<button type="submit" class="btn btn-primary" disabled="disabled">保存</button>
+							  	</div>
+					    	</form>
+					    </div>
+					    <div role="tabpanel" class="tab-pane" id="setpassword">
+					    	<form class="form-horizontal">
+					    		<div class="form-group has-feedback">
+					    			<label for="current" class="col-sm-3 control-label">現在のパスワード</label>
+							    	<div class="col-sm-8">
+							    		<input type="password" class="form-control" id="current">
+							    	</div>
+							    	<div class="col-sm-1">
+							    	</div>
+					    		</div>
+						    	<div class="form-group has-feedback">
+							    	<label for="new" class="col-sm-3 control-label">パスワード</label>
+							    	<div class="col-sm-8">
+							    		<input type="password" class="form-control" id="new">
+							    	</div>
+							    	<div class="col-sm-1" data-toggle="tooltip" data-placement="left" title="8文字以上の長さが必要です">
+							    		<span class="glyphicon glyphicon-question-sign form-control-feedback"></span>
+							    	</div>
+							  	</div>
+							  	<div class="form-group has-feedback">
+							    	<label for="confirm" class="col-sm-3 control-label">もう一度入力</label>
+							    	<div class="col-sm-8">
+							    		<input type="password" class="form-control" id="confirm">
+							    	</div>
+							    	<div class="col-sm-1" data-toggle="tooltip" data-placement="left" title="上と同じ文字を入力してください">
+							    		<span class="glyphicon glyphicon-question-sign form-control-feedback"></span>
+							    	</div>
+							  	</div>
+							  	<div class="text-right">
+								  	<button type="submit" class="btn btn-primary" disabled="disabled">パスワードを変更</button>
+							  	</div>
+					    	</form>
+					    </div>
 					</div>
 		  		</div>
 			</div>
