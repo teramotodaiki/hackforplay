e2ede18af41a1461e290dc19cfa12f8953b3b9e6
teramotodaiki <i101323@gmail.com>
2015-05-03 16:45:59 +0900

    マイページを `/m` に作成

---
+++ m/view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>マイページ - あそべるプログラミング hackforplay</title>
+	<?php require_once '../library.php' ?>
+</head>
+<body>
+	<?php require_once '../analyticstracking.php'; ?>
+	<?php require_once '../fb-root.php'; ?>
+	<?php require_once '../sendattendance.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
+	<?php require_once '../view/header.php'; ?>
+	<script src="view.js" type="text/javascript" charset="utf-8"></script>
+
+</body>
+</html>
