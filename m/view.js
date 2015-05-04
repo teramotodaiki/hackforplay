166b54e4b4e8ee22221d2904bb8a1093e35fb2c5
teramotodaiki <i101323@gmail.com>
2015-05-04 12:08:41 +0900

    マイページのビュー（あなたのステージ）
    * 自分の作ったステージを取得するAPI
    * *あなたのステージ*を表示するビュー

--- m/view.js
+++ m/view.js
@@ -1,3 +1,65 @@
-$(function () {
+$(function(){
+	// インスタンス
+	var $item = $('<div>').addClass('col-md-4 col-sm-6 col-xs-12 h4p_item').append(
+		$('<a>').append(
+			$('<div>').addClass('h4p_item-thumbnail').append(
+				$('<span>').addClass('h4p_item-src')
+			)
+		)
+	).append(
+		$('<div>').addClass('h4p_item-title').append(
+			$('<a>').append($('<h4>'))
+		)
+	).append(
+		$('<div>').addClass('h4p_item-footer').append(
+			$('<p>').append($('<span>').html('作成者：<b><a></a></b>'))
+		).append(
+			$('<p>').append($('<span>').html('プレイ回数：<b>回</b>'))
+		).append(
+			$('<p>').append($('<span>').html('改造元：<b><a></a></b>'))
+		).append(
+			$('<p>').append($('<span>').addClass('label'))
+		)
+	);
+	// 一覧取得
+	$.post('../stage/fetchmystage.php', {
+		'length': 15
+	}, function(data, textStatus, xhr) {
+		console.log(data);
+		if (data === 'parse-error') {
+		}else{
+			var result = jQuery.parseJSON(data);
+			var $list = $('#h4p_stagelist');
+			result.values.forEach(function(stage){
+				var item = $item.clone(true);
+				item.children('a').attr({
+					href: '/s?id=' + stage.id,
+					title: stage.title
+				}).children('.h4p_item-thumbnail').children('.h4p_item-src').text(stage.thumbnail);
+				item.children('.h4p_item-title').children('a').attr({
+					href: '/s?id=' + stage.id,
+					title: stage.title
+				}).children('h4').text(stage.title.length > 38 ? (stage.title.substr(0, 37) + '…') : stage.title);
+				item.find('.h4p_item-footer p:nth-child(1) a').attr({
+					href: '/__mypagelink__',
+					title: stage.author_name
+				}).text(stage.author_name);
+				item.find('.h4p_item-footer p:nth-child(2) b').prepend(stage.playcount);
+				if (stage.source_mode === 'replay') {
+					item.find('.h4p_item-footer p:nth-child(3) a').attr({
+						href: '/s?id=' + stage.source_id,
+						title: stage.source_title
+					}).text(stage.source_title);
+				}else{
+					item.find('.h4p_item-footer p:nth-child(3) span').text('オリジナルステージ');
+				}
+				var label_lv = (stage.state === 'published' ? 'label-success' :
+								stage.state === 'judging'	? 'label-warning' :
+								stage.state === 'rejected'	? 'label-danger' : 'label-default');
+				item.find('.h4p_item-footer p:nth-child(4) span').addClass(label_lv).text(stage.state);
 
+				item.appendTo($list);
+			});
+		}
+	});
 });
--- m/view.php
+++ m/view.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>マイページ - あそべるプログラミング hackforplay</title>
+	<title>HackforPlay</title>
 	<?php require_once '../library.php' ?>
 </head>
 <body>
@@ -13,6 +13,22 @@
 	<?php require_once '../view/authmodal.php'; ?>
 	<?php require_once '../view/header.php'; ?>
 	<script src="view.js" type="text/javascript" charset="utf-8"></script>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="row h4p_box">
+					<div class="col-md-12 h4p_box-header">
+						<h3>あなたのステージ</h3>
+					</div>
+					<div class="col-md-12 h4p_box-main">
+						<div id="h4p_stagelist" class="row">
+						</div>
+					</div>
+					<div class="col-md-12 h4p_box-footer">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
