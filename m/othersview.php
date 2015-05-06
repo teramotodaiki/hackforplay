5aad69649e6e6d77c143297ec2b88c5c738e211d
teramotodaiki <i101323@gmail.com>
2015-05-06 17:42:33 +0900

    マイページにIDを付加
    GET送信で`id`を送信
    * セッションがある状態で自分のID、あるいはIDをNULLに指定した場合、`ownpage`へ
    * それ以外で`id`が指定されていた場合、`otherspage`へ
    * セッションも`id`もない場合、ひとまずランディングページへ

--- m/othersview.php
+++ m/othersview.php
@@ -12,6 +12,9 @@
 	<?php require_once '../sendattendance.php'; ?>
 	<?php require_once '../view/authmodal.php'; ?>
 	<?php require_once '../view/header.php'; ?>
+	<script type="text/javascript" charset="utf-8">
+	var user_id = <?php echo $id; ?>;
+	</script>
 	<script src="view.js" type="text/javascript" charset="utf-8"></script>
 	<div class="container">
 		<div class="row">
@@ -38,20 +41,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-12">
-				<div class="row h4p_box">
-					<div class="col-md-12 h4p_box-header">
-						<h3>あなたのプロジェクト</h3>
-					</div>
-					<div class="col-md-12 h4p_box-main">
-						<div id="h4p_projectlist" class="row">
-						</div>
-					</div>
-					<div class="col-md-12 h4p_box-footer">
-					</div>
-				</div>
-			</div>
-
 		</div>
 	</div>
 </body>
