0a12c22658ad882697149752b55c4f983e91fda9
teramotodaiki <i101323@gmail.com>
2015-05-10 14:00:22 +0900

    サインアップ時に情報を受け取るテスト

--- auth/signupwithemail.php
+++ auth/signupwithemail.php
@@ -1,16 +1,54 @@
 <?php
 /*
-メールを送信したのち、新しくユーザーを作成し、アカウントを関連づける
-Input: 	mail
+メールを送信したのち、新しくユーザーを作成し、アカウントを関連づける。NULLは許されない
+Input: 	email , gender, nickname , password , birthday , experience_days , timezone_name , timezone_offset , timezone
 Output: invalid , success, reserved , sendmail-error のいずれか
 */
 
 require_once '../preload.php';
 
-$email 	= filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-if($email == FALSE){
-	exit('invalid');
+// Input value
+$invalid_inputs = array();
+$email				= filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+if ($email === FALSE || $email === NULL) {
+	array_push($invalid_inputs, 'email');
 }
+$gender				= filter_input(INPUT_POST, 'gender');
+if($gender !== "man" && $gender !== "woman"){
+	array_push($invalid_inputs, 'gender');
+}
+$nickname			= filter_input(INPUT_POST, 'nickname');
+if($nickname === NULL){
+	array_push($invalid_inputs, 'nickname');
+}
+$password			= filter_input(INPUT_POST, 'password');
+if(strlen($password) < 8){
+	array_push($invalid_inputs, 'password');
+}else{
+	$hashed			= password_hash($password, PASSWORD_DEFAULT);
+}
+$birthday			= filter_input(INPUT_POST, 'birthday');
+if ($birthday === NULL || date_create_from_format('Y-m-d', $birthday) === FALSE) {
+	array($invalid_inputs, 'birth_year');
+}
+$experience_days	= filter_input(INPUT_POST, 'experience_days', FILTER_VALIDATE_INT);
+if ($experience_days === NULL || $experience_days === FALSE) {
+	array($invalid_inputs, 'experience_days');
+}
+$timezone_name		= filter_input(INPUT_POST, 'timezone_name');
+$timezone_offset	= filter_input(INPUT_POST, 'timezone_offset', FILTER_VALIDATE_INT);
+if ($timezone_name === NULL ||	$timezone_offset === NULL || $timezone_offset === FALSE) {
+	array($invalid_inputs, 'timezone');
+}
+
+if(count($invalid_inputs) > 0){
+	exit(json_encode($invalid_inputs));
+}
+
+var_dump($_POST);
+die();
+
+// Javascriptで取得したブラウザのタイムゾーン
 $timezone = filter_input(INPUT_POST, 'timezone', FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(\+|\-)[0-1][0-9]:00$/")));
 if($timezone === FALSE || $timezone === NULL){
 	$timezone = '+00:00';
