909149040d981696491276a956608ee39f437bc9
teramotodaiki <i101323@gmail.com>
2015-05-13 10:56:01 +0900

    Basic認証を追加

--- a/index.php
+++ a/index.php
@@ -1,5 +1,20 @@
 <?php
 require_once '../preload.php';
 
+// ユーザー名・パスワード
+$accept_page_username	= 'hackforplay';
+$accept_page_password	= 'hackforplay';
+
+switch (true) {
+    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+    case $_SERVER['PHP_AUTH_USER'] !== $accept_page_username:
+    case $_SERVER['PHP_AUTH_PW']   !== $accept_page_password:
+        header('WWW-Authenticate: Basic realm="Enter username and password."');
+        header('Content-Type: text/plain; charset=utf-8');
+        die('Sorry!! but in this page, visitors are limited browsing');
+}
+
+header('Content-Type: text/html; charset=utf-8');
+
 include 'view.php';
 ?>
