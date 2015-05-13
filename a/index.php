756222ae1a7250fb0fbdbc143f1b779365485889
teramotodaiki <i101323@gmail.com>
2015-05-13 10:28:30 +0900

    viewのファイルを移動
    旧仕様のスクリプトを削除し、viewファイルを作成

--- a/index.php
+++ a/index.php
@@ -1,22 +1,5 @@
 <?php
-// more view
-//$is_beta = 1; // This is a BETA VERSION. Turn into 0 to be the official ver!
-
-// 1.Preparation
 require_once '../preload.php';
 
-// 2.Sign in or sign up
-require_once '../signin.php';
-
-// 3.Enumrate cleared stage
-require_once '../cleared.php';
-
-// 4.Get all stages
-require_once '../getstages.php';
-
-// 5. Get restaging codes
-require_once '../getrestagingcodes.php';
-
-// 4.Load page html
-include('view/main.php');
+include 'view.php';
 ?>
