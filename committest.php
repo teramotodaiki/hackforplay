ad72d04cc82740e4704375dcc01d504b0387c01b
teramotodaiki <i101323@gmail.com>
2015-05-14 17:01:50 +0900

    テスト環境の設定

--- committest.php
+++ committest.php
@@ -11,23 +11,31 @@
 	$(function(){
 		$('form#commit').submit(function(event) {
 			event.preventDefault();
+			var loading = $(this).find('button[type="submit"]').button('loading');
 
 			var code = $(this).find('.code').val();
 			$.post('project/commitregistration.php',{
 				'code': code
 			}, function(data, textStatus, xhr) {
-				$('.alert').hide().text(data).fadeIn('fast');
+				loading.button('reset');
+				$('.result').text(data);
 			});
 		});
 	});
 	</script>
 	<div class="container">
-		<div class="alert alert-default"></div>
-		<form id="commit">
-			<h4>Code</h4>
-			<textarea name="code" class="code" style="width:100%;height:400px;font-family:monospace;"></textarea>
-			<button type="submit">Commit</button>
-		</form>
+		<div class="row">
+			<div class="col-md-6">
+				<pre class="result"></pre>
+			</div>
+			<div class="col-md-6">
+				<form id="commit">
+					<h4>Code</h4>
+					<textarea name="code" class="code" style="width:100%;height:400px;font-family:monospace;"></textarea>
+					<button type="submit" class="btn btn-lg">Commit</button>
+				</form>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
