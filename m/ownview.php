caed7d2cb5acc365ce191a5ec6aaac3e4c61d278
teramotodaiki <i101323@gmail.com>
2015-05-14 22:47:47 +0900

    Fixed コードの修正

--- m/ownview.php
+++ m/ownview.php
@@ -13,7 +13,7 @@
 	<?php require_once '../view/authmodal.php'; ?>
 	<?php require_once '../view/header.php'; ?>
 	<script type="text/javascript" charset="utf-8">
-	sessionStorage.setItem('view_user_id', <?php echo $userid; ?>);
+	sessionStorage.setItem('view_user_id', <?php echo $session_userid; ?>);
 	function bsAlert (_text) {
 		var _bsalert =
 		$('<div>').addClass('alert alert-dismissible fade in').attr('role', 'alert').append(
