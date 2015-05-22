ea6a6b9b112583126e08717fad2682ef93cdbab4
teramotodaiki <i101323@gmail.com>
2015-05-22 17:28:32 +0900

    親ステージのコードをキャッシュに利用

--- project/getcurrentcodeascache.php
+++ project/getcurrentcodeascache.php
@@ -4,15 +4,31 @@ function getCurrentCodeAsCache($project_id)
 	global $dbh;
 
 	// 最も新しい(値の大きい)ScriptIDを取得
-	$stmt	= $dbh->prepare('SELECT MAX("ID") FROM "Script" WHERE "ProjectID"=:project_id');
+	$stmt			= $dbh->prepare('SELECT MAX("ID") FROM "Script" WHERE "ProjectID"=:project_id');
 	$stmt->bindValue("project_id", $project_id, PDO::PARAM_INT);
 	$stmt->execute();
-	$script_id	= $stmt->fetch(PDO::FETCH_COLUMN, 0);
-	if (empty($script_id)) {
-		return ''; // プロジェクトが空
+	$script_id		= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+
+	if (!$script_id) {
+		// プロジェクトがまだ空の場合、親プロジェクトのプロジェクトの最新のコミットを利用
+		$stmt		= $dbh->prepare('SELECT "ParentID" FROM "Project" WHERE "ProjectID"=:project_id');
+		$stmt->bindValue("project_id", $project_id, PDO::PARAM_INT);
+		$stmt->execute();
+		$parent_id	= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+		if (!$parent_id) {
+			return array(); // キャッシュなし
+		}
+		// 最も新しい(値の大きい)ScriptIDを取得
+		$stmt			= $dbh->prepare('SELECT MAX("ID") FROM "Script" WHERE "ProjectID"=:parent_id');
+		$stmt->bindValue("parent_id", $parent_id, PDO::PARAM_INT);
+		$stmt->execute();
+		$script_id		= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+		if (!$script_id) {
+			return array();
+		}
 	}
 
-	$stmt	= $dbh->prepare('SELECT "Code"."Value","Code"."ID" FROM "Code" INNER JOIN "Line" ON "Line"."CodeID"="Code"."ID" WHERE "Line"."ScriptID"=:script_id ORDER BY "Line"."Line"');
+	$stmt			= $dbh->prepare('SELECT "Code"."Value","Code"."ID" FROM "Code" INNER JOIN "Line" ON "Line"."CodeID"="Code"."ID" WHERE "Line"."ScriptID"=:script_id ORDER BY "Line"."Line"');
 	$stmt->bindValue(":script_id", $script_id, PDO::PARAM_INT);
 	$stmt->execute();
 
