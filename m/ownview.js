a19bcd3adfdeab9b2237058df6b44b4882a597bd
teramotodaiki <i101323@gmail.com>
2015-05-27 16:33:53 +0900

    テスト

--- m/ownview.js
+++ m/ownview.js
@@ -301,6 +301,7 @@ $(function(){
 	}
 
 	function convertLocaleTimeString (datetimeoffset) {
+		console.log(datetimeoffset);
 		var timestamp = new Date(datetimeoffset.replace(' ', 'T')).getTime();
 		var date = new Date(timestamp - 60 * 1000 * new Date().getTimezoneOffset());
 		return date.toLocaleString();
