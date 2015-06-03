e743ab174c7fbc4b12e9fdf7c1599ed059b33d39
teramotodaiki <i101323@gmail.com>
2015-06-03 13:17:16 +0900

    管理者ページのタブビュー

--- a/view.php
+++ a/view.php
@@ -13,7 +13,7 @@
 	<script src="view.js" type="text/javascript" charset="utf-8"></script>
 	<!-- Modal -->
 	<div class="modal fade" id="codeModal" tabindex="-1" role="dialog">
-		<div class="modal-dialog">
+		<div class="modal-dialog modal-lg">
 			<div class="modal-content">
 	    		<div class="modal-header">
 			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -28,8 +28,24 @@
 		</div>
 	</div>
 	<!-- contents -->
-	<div class="container">
-		<div class="row list-judging">
+	<div class="container panel panel-default">
+		<div class="row panel-body">
+			<ul class="col-md-2 nav nav-pills nav-stacked panel-body" role="tablist">
+			    <li role="presentation" class="active">
+			    	<a href="#summary" aria-controls="summary" role="tab" data-toggle="tab">summary</a>
+			    </li>
+			    <li role="presentation">
+			    	<a href="#judge" aria-controls="judge" role="tab" data-toggle="tab">judge</a>
+			    </li>
+			</ul>
+			<div class="col-md-10 tab-content">
+			    <div role="tabpanel" class="tab-pane active" id="summary">
+			    	summary
+			    </div>
+			    <div role="tabpanel" class="tab-pane" id="judge">
+			    	<div class="row list-judging"></div>
+			    </div>
+			</div>
 		</div>
 	</div>
 </body>
