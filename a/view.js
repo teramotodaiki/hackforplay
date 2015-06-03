86c8c6495290b9d448d0e6f55413914b8f6c3811
teramotodaiki <i101323@gmail.com>
2015-06-03 22:57:05 +0900

    Revert "Merge branch 'staging'"
    This reverts commit b4ba72bcd9b241b8e788cb5c96e35758ad24c8b7, reversing
    changes made to 9539157c31edc49f1988121c7d40b06de7430973.

--- a/view.js
+++ a/view.js
@@ -124,35 +124,6 @@ $(function(){
 		}
 	});
 
-	var $exceptionItem =
-	$('<tr>').append(
-		$('<td>').addClass('message')
-	).append(
-		$('<td>').addClass('code')
-	).append(
-		$('<td>').addClass('file')
-	).append(
-		$('<td>').addClass('line')
-	);
-
-	// ExceptionMapのチャート
-	$.post('../analytics/exceptionsummary.php',{
-		'begin' : '2010-01-01 00:00:00',
-		'end' : '2016-01-01 00:00:00',
-		'attendance-token' : sessionStorage.getItem('attendance-token')
-	}, function(data, textStatus, xhr) {
-		var result = $.parseJSON(data);
-		result.values.forEach(function(item){
-			var eItem = $exceptionItem.clone(true);
-			eItem.find('.message').text(item.Message);
-			eItem.find('.code').text(item.Code);
-			eItem.find('.file').text(item.File);
-			eItem.find('.line').text(item.Line);
-			eItem.appendTo('.detail-container');
-			console.log(eItem);
-		});
-	});
-
 	// _level のアラート _text を生成し、jQueryオブジェクトを返す
 	function bsAlert (_level, _text) {
 		var _bsalert =
