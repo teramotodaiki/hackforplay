e7ff7b93f0e19944e7be5be26388191d79980fd6
teramotodaiki <i101323@gmail.com>
2015-06-08 12:12:57 +0900

    セッションのテスト２

--- preload.php
+++ preload.php
@@ -26,11 +26,11 @@ catch ( PDOException $e ) {
 // セッションのテスト
 /* set the cache limiter to 'private' */
 
-// session_cache_limiter('private');
+session_cache_limiter('private');
 $cache_limiter = session_cache_limiter();
 
 /* set the cache expire to 30 minutes */
-// session_cache_expire(48 * 60);
+session_cache_expire(48 * 60);
 $cache_expire = session_cache_expire();
 
 session_start();
