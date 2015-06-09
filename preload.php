a9f6a6aadae1326a89fa33a604499cf81d622d3d
teramotodaiki <i101323@gmail.com>
2015-06-09 19:07:40 +0900

    セッションの仕様を変更

--- preload.php
+++ preload.php
@@ -24,7 +24,7 @@ catch ( PDOException $e ) {
 }
 
 // セッション ユーザーID取得
-session_cache_limiter('private');
+session_cache_limiter('private_no_expire');
 session_cache_expire(48 * 60); // 48時間セッション継続
 session_start();
 if(isset($_SESSION['UserID'])){
