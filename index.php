f9a5a129b3eeb25e33c542749aed1b70ed4ce949
teramotodaiki <i101323@gmail.com>
2015-06-09 17:54:08 +0900

    テスト

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 require_once 'preload.php';
 
 $rewrite = filter_input(INPUT_GET, 'rewrite', FILTER_VALIDATE_BOOLEAN);
+var_dump($rewrite);
+var_dump($session_userid);
 if (isset($session_userid) && $rewrite !== FALSE) {
 	header('Location:../r');
 	exit();
