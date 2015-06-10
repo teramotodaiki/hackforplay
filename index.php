936394c57bf99919f8940aa726b30feb73f23a76
teramotodaiki <i101323@gmail.com>
2015-06-10 11:06:13 +0900

    テスト

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 require_once 'preload.php';
 
 $rewrite = filter_input(INPUT_GET, 'rewrite', FILTER_VALIDATE_BOOLEAN);
+echo 'session_userid';
+var_dump($session_userid);
+echo "\nrewrite";
+var_dump($rewrite);
 if (isset($session_userid) && $rewrite !== FALSE) {
 	header('Location:../r');
 	exit();
