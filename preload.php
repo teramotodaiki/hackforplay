585ef9dce34b90990555ee381f588832db7b5ce2
teramotodaiki <i101323@gmail.com>
2015-06-10 13:27:36 +0900

    テスト終了

--- preload.php
+++ preload.php
@@ -35,9 +35,6 @@ if(isset($_SESSION['UserID'])){
 }
 session_commit();
 
-print_r(session_get_cookie_params());
-exit();
-
 // 暗号化キーの生成
 $encription_key = pack('H*', "29fdebae5e1d48b54763051cef08bc55abe017e2ffb2a00a3bcb04b7e103a0cd");
 
