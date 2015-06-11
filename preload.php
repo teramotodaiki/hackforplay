2c46704f5bcc9ae2fd86e83d1a0e467b9daf712c
teramotodaiki <i101323@gmail.com>
2015-06-11 10:55:57 +0900

    Revert "テスト"
    This reverts commit d16ddf4e351b3f10ffa75847ac9c9a7be277f0cd.

--- preload.php
+++ preload.php
@@ -23,8 +23,6 @@ catch ( PDOException $e ) {
     die();
 }
 
-echo 'test';
-
 try {
 
 	require_once 'sessionsettings.php';
@@ -37,8 +35,6 @@ try {
 	traceData($e);
 	die;
 }
-
-echo 'test2 ' . $session_userid;
 
 // 暗号化キーの生成
 $encription_key = pack('H*', "29fdebae5e1d48b54763051cef08bc55abe017e2ffb2a00a3bcb04b7e103a0cd");
