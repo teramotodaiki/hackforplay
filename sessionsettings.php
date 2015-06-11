21bc59bc031c313e66d3dfff1a730f5f881f2173
teramotodaiki <i101323@gmail.com>
2015-06-11 11:00:12 +0900

    test

--- sessionsettings.php
+++ sessionsettings.php
@@ -3,12 +3,12 @@
 session_cache_limiter('private_no_expire');
 session_cache_expire(48 * 60); // 48時間セッション継続
 session_set_cookie_params(48 * 60 * 60);
-switch ($_SERVER['SERVER_NAME']) {
-	case 'hackforplay-staging.azurewebsites.net':
-		session_save_path('D:\local\Temp\hackforplay-staging');
-		break;
-	case 'hackforplay.xyz':
-		session_save_path('D:\local\Temp\hackforplay');
-		break;
-}
+// switch ($_SERVER['SERVER_NAME']) {
+// 	case 'hackforplay-staging.azurewebsites.net':
+// 		session_save_path('D:\local\Temp\hackforplay-staging');
+// 		break;
+// 	case 'hackforplay.xyz':
+// 		session_save_path('D:\local\Temp\hackforplay');
+// 		break;
+// }
 ?>
