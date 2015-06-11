57f4c329e8d8c805eda012f72ab4f85d07f202c8
teramotodaiki <i101323@gmail.com>
2015-06-11 11:05:38 +0900

    Revert "Fixed ディレクトリ名を変更"
    This reverts commit 0928f450130f2b0904f01987d27032079dba18c3.

--- sessionsettings.php
+++ sessionsettings.php
@@ -5,7 +5,7 @@ session_cache_expire(48 * 60); // 48時間セッション継続
 session_set_cookie_params(48 * 60 * 60);
 switch ($_SERVER['SERVER_NAME']) {
 	case 'hackforplay-staging.azurewebsites.net':
-		session_save_path('D:\local\Temp\hackforplay_staging');
+		session_save_path('D:\local\Temp\hackforplay-staging');
 		break;
 	case 'hackforplay.xyz':
 		session_save_path('D:\local\Temp\hackforplay');
