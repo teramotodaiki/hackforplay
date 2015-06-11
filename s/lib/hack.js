542ab125dedf24df121e561447bc9aeca872f898
teramotodaiki <i101323@gmail.com>
2015-06-11 16:15:41 +0900

    スクリーンショット用の関数

--- s/lib/hack.js
+++ s/lib/hack.js
@@ -1,6 +1,29 @@
+(function(){
+	// Initialize
+	var Hack = {};
+	Object.defineProperty(window, 'Hack', {
+		configurable: true,
+		enumerable: true,
+		get: function(){
+			return Hack;
+		}
+	});
+})();
+
+Hack.saveImage = function(){
+	var canvas = $("#enchant-stage>div").children('canvas').get(0);
+	var data = canvas.toDataURL();
+	sessionStorage.setItem('image', data);
+    window.parent.postMessage('thumbnail', '/');
+};
+Hack.screenShot = function(){
+	window.parent('screenshot', '/');
+	saveImage();
+};
+
 window.addEventListener('load', function() {
     enchant();
-    var game = new enchant.Core();
+    var game = new enchant.Core(480, 320);
     game.start();
     game.addEventListener('load', function(){
 		var label = new enchant.Label('game start!');
