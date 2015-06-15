fb6414f09ee712594da39837f61009f418a505cb
teramotodaiki <i101323@gmail.com>
2015-06-15 13:35:41 +0900

    背景マップの作成

--- s/lib/run.js
+++ s/lib/run.js
@@ -1,8 +1,26 @@
 window.addEventListener('load', function() {
 
-	Hack.on('load', function(event) {
-		Hack.textarea.backgroundColor = '#024';
+	var game = enchant.Core.instance;
+	game.preload(['img/map2.png']);
 
-		Hack.log('This is run game!');
+	Hack.textarea.backgroundColor = '#024';
+
+	Hack.on('load', function(event) {
+
+	});
+
+	game.on('load', function(event) {
+
+		Hack.backgroundImage = [];
+		for (var i = 0; i < 16; i++) {
+			Hack.backgroundImage[i] = new enchant.Map(32, 32);
+			Hack.backgroundImage[i].image = game.assets['img/map2.png'];
+			Hack.backgroundImage[i].loadData([
+				[22],[21],[20],[19],[18],[18],[0],[1],[1],[1]
+			]);
+			Hack.backgroundImage[i].x = i * 32;
+			game.rootScene.addChild(Hack.backgroundImage[i]);
+		};
+
 	});
 });
