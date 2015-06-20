e11c69e6fc73b78204096b589c91bb8740952300
teramotodaiki <i101323@gmail.com>
2015-06-20 11:24:15 +0900

    仮のモーダル Use this

--- resources/view.php
+++ resources/view.php
@@ -12,6 +12,31 @@
 	<?php require_once '../sendattendance.php'; ?>
 	<?php require_once '../view/authmodal.php'; ?>
 	<?php require_once '../view/header.php'; ?>
+	<div class="modal fade" id="useModal" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+	    		<div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <h4 class="modal-title">このコードをステージ改造コードに書き加えてください</h4>
+			        <p class="text-warning"><var>game.onenterframe</var> がすでに書かれている場合は、その中に書き入れてください</p>
+		    	</div>
+			    <div class="modal-body">
+			    	<pre class-"font-family: monospace;">
+game.preload(['img/path']);
+game.onenterframe = function() {
+	createSprite(w, h, {
+		x: 0, y: 0,
+		image: game.assets['img/path']
+	});
+}
+			    	</pre>
+			    </div>
+	    		<div class="modal-footer">
+	        		<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+	    		</div>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div id="anchor-enchantjs" class="col-md-12">
@@ -29,7 +54,7 @@
 									<dd><a href="../s?id=303" title="からのステージ">からのステージ</a></dd>
 									<dd>...</dd>
 								</dl>
-								<button type="button" class="btn btn-primary">View Code</button>
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#useModal">Use This</button>
 							</div>
 						</div>
 					</div>
@@ -45,7 +70,7 @@
 									<dd><a href="../s?id=303" title="からのステージ">からのステージ</a></dd>
 									<dd>...</dd>
 								</dl>
-								<button type="button" class="btn btn-primary">View Code</button>
+								<button type="button" class="btn btn-primary">Use This</button>
 								<button type="button" class="btn btn-info">View Frame</button>
 							</div>
 						</div>
