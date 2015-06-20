1c4d1863548ad138c0a72f75783bcaf78b30b620
teramotodaiki <i101323@gmail.com>
2015-06-20 11:05:04 +0900

    仮のビュー

---
+++ resources/view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head prefix="og: http://ogp.me/ns#">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>リソース hackforplay</title>
+	<?php require_once '../library.php' ?>
+</head>
+<body class="">
+	<?php require_once '../analyticstracking.php' ?>
+	<?php require_once '../fb-root.php' ?>
+	<?php require_once '../sendattendance.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
+	<?php require_once '../view/header.php'; ?>
+	<div class="container">
+		<div class="row">
+			<div id="anchor-enchantjs" class="col-md-12">
+				<h2>enchant.js</h2>
+				<div class="row">
+					<div class="col-md-4 col-sm-6">
+						<div class="thumbnail">
+							<img src="../img/lp.jpg" height="817" width="1917">
+							<div class="caption">
+								<h3>File Name</h3>
+								<dl>
+									<dt>Size</dt>
+									<dd>817x1917</dd>
+									<dt>Example of use</dt>
+									<dd><a href="../s?id=303" title="からのステージ">からのステージ</a></dd>
+									<dd>...</dd>
+								</dl>
+								<button type="button" class="btn btn-primary">View Code</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="thumbnail">
+							<img src="../img/lp.jpg" height="817" width="1917">
+							<div class="caption">
+								<h3>File Name</h3>
+								<dl>
+									<dt>Size</dt>
+									<dd>817x1917</dd>
+									<dt>Example of use</dt>
+									<dd><a href="../s?id=303" title="からのステージ">からのステージ</a></dd>
+									<dd>...</dd>
+								</dl>
+								<button type="button" class="btn btn-primary">View Code</button>
+								<button type="button" class="btn btn-info">View Frame</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php require_once '../view/footer.php' ?>
+</body>
+</html>
