cdac3a75b7b01405ea476adfc7b8f12e38c9f90a
teramotodaiki <i101323@gmail.com>
2015-06-22 16:21:28 +0900

    リファレンスのビュー

---
+++ reference/view.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head prefix="og: http://ogp.me/ns#">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>リファレンス hackforplay</title>
+	<?php require_once '../library.php' ?>
+</head>
+<body class="">
+	<?php require_once '../analyticstracking.php' ?>
+	<?php require_once '../fb-root.php' ?>
+	<?php require_once '../sendattendance.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
+	<?php require_once '../view/header.php'; ?>
+	<script src="view.js" type="text/javascript"></script>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<div id="anchor-hack" class="panel panel-default">
+					<div class="panel-heading">
+						<h2>hack.js <small>すべてのステージで共通して使える機能</small></h2>
+					</div>
+					<div class="panel-body">
+
+					</div>
+				</div>
+				<div id="anchor-typing" class="panel panel-default">
+					<div class="panel-heading">
+						<h2>typing.js <small><a href="../s?id=304" target="_blank" title="タイピングゲーム">タイピングゲーム</a>で使える機能</small></h2>
+					</div>
+					<div class="panel-body">
+
+					</div>
+				</div>
+				<div id="anchor-run" class="panel panel-default">
+					<div class="panel-heading">
+						<h2>run.js <small><a href="../s?id=305" target="_blank" title="ランゲーム">ランゲーム</a>で使える機能</small></h2>
+					</div>
+					<div class="panel-body">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php require_once '../view/footer.php' ?>
+</body>
+</html>
