b06df678898665a7e9addf23f3256fcb347b8807
teramotodaiki <i101323@gmail.com>
2015-06-25 12:04:34 +0900

    コールバックのスクリプト

---
+++ callback.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+require_once 'common.php';
+require_once 'twitteroauth/autoload.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$request_token = [];
+$request_token['oauth_token'] = $_SESSION['oauth_token'];
+$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+
+if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+    die( 'Error!' );
+}
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+
+$_SESSION['access_token'] = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+
+//セッションIDをリジェネレート
+session_regenerate_id();
+
+//マイページへリダイレクト
+header( 'location: /' );
+
+ ?>
