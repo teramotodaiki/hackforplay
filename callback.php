2c6649296a02f95b3de8d1d56486fd482df157ab
teramotodaiki <i101323@gmail.com>
2015-06-25 13:52:23 +0900

    アカウントを参照してUserIDを取得

--- callback.php
+++ callback.php
@@ -1,27 +1,38 @@
 <?php
-session_start();
+
 
 require_once 'common.php';
 require_once 'twitteroauth/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$request_token = [];
-$request_token['oauth_token'] = $_SESSION['oauth_token'];
-$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+try {
 
-if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
-    die( 'Error!' );
+	require_once 'sessionsettings.php';
+	session_start();
+	$request_token = [];
+	$request_token['oauth_token'] = $_SESSION['oauth_token'];
+	$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+
+	if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+	    die( 'Error!' );
+	}
+
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+
+	$_SESSION['access_token'] = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+
+	//セッションIDをリジェネレート
+	session_regenerate_id();
+
+	//リダイレクト
+	header( 'location: /loginsuccess.php' );
+
+} catch (Exception $e) {
+	require_once 'exception/tracedata.php';
+	traceData($e);
+
+	header('Location: e');
 }
-
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
-
-$_SESSION['access_token'] = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
-
-//セッションIDをリジェネレート
-session_regenerate_id();
-
-//マイページへリダイレクト
-header( 'location: /loginsuccess.php' );
 
  ?>
--- loginsuccess.php
+++ loginsuccess.php
@@ -1,18 +1,56 @@
 <?php
 
-session_start();
-
 require_once 'common.php';
 require_once 'twitteroauth/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$access_token = $_SESSION['access_token'];
+try {
 
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	require_once 'preload.php';
 
-$user = $connection->get("account/verify_credentials");
+	require_once 'sessionsettings.php';
+	session_start();
+	$access_token = $_SESSION['access_token'];
 
-var_dump( $user );
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+	$twitter = $connection->get("account/verify_credentials");
+	$json = json_encode($twitter);
+
+	// Accountと照合
+	$stmt	= $dbh->prepare('SELECT "UserID" FROM "Account" WHERE "Type"=:twitter AND "ExternalID"=:twitter_id');
+	$stmt->bindValue(":twitter", 'twitter', PDO::PARAM_STR);
+	$stmt->bindValue(":twitter_id", $twitter->id, PDO::PARAM_INT);
+	$stmt->execute();
+
+	$user_id = $stmt->fetch(PDO::FETCH_COLUMN, 0);
+	if ($user_id) {
+		$_SESSION['UserID'] = $user_id;
+	}
+	session_commit();
+
+} catch (Exception $e) {
+	require_once 'exception/tracedata.php';
+	traceData($e);
+
+	header('Location: e');
+}
 
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title></title>
+ </head>
+ <body>
+ 	<?php echo $user_id; ?>
+ 	<script type="text/javascript" charset="utf-8">
+ 	var json = <?php echo $json; ?>;
+ 	console.log(json);
+ 	</script>
+ </body>
+ </html>
