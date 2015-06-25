a73748020c38e0908a59f6a0d0e787036c31e4a7
teramotodaiki <i101323@gmail.com>
2015-06-25 12:10:06 +0900

    テスト

--- loginsuccess.php
+++ loginsuccess.php
@@ -1,5 +1,18 @@
 <?php
 
-echo 'login success!!';
+session_start();
+
+require_once 'common.php';
+require_once 'twitteroauth/autoload.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$access_token = $_SESSION['access_token'];
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+$user = $connection->get("account/verify_credentials");
+
+var_dump( $user );
 
  ?>
