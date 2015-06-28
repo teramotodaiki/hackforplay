cad148c276c734c1c5a8d922ff25e23015a20113
teramotodaiki <i101323@gmail.com>
2015-06-28 18:00:52 +0900

    Fixed バインドの引数

--- session/sethundler.php
+++ session/sethundler.php
@@ -84,7 +84,7 @@ class SQLSessionHundler implements SessionHandlerInterface
 		date_default_timezone_set('UTC');
 
 		$stmt	= $this->dbh->prepare('DELETE FROM "Sessions" WHERE "Timestamp" < ?');
-		$stmt->bindValue(0, time() - $maxlifetime, PDO::PARAM_INT);
+		$stmt->bindValue(1, time() - $maxlifetime, PDO::PARAM_INT);
 		$stmt->execute();
 		return true;
 	}
