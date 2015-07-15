4d7813d05fd297b71b4272e7310221da1c5dcbee
teramotodaiki <i101323@gmail.com>
2015-07-15 10:03:42 +0900

    コメントのビュー

---
+++ comments/view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HackforPlay</title>
+	<?php require_once '../library.php' ?>
+</head>
+<body>
+	<?php require_once '../analyticstracking.php'; ?>
+	<?php require_once '../fb-root.php'; ?>
+	<?php require_once '../sendattendance.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
+	<?php require_once '../view/header.php'; ?>
+	<script src="view.js" type="text/javascript" charset="utf-8"></script>
+	<!-- Content -->
+	<div class="container">
+		<h4>あなたへのコメント</h4>
+		<div class="row h4p_comment-list">
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
