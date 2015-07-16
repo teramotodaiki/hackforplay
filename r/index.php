7e77b845c3352c479580606ef6151c89ad426232
teramotodaiki <i101323@gmail.com>
2015-07-16 21:09:43 +0900

    ステージ数の計算方法を修正

--- r/index.php
+++ r/index.php
@@ -12,13 +12,37 @@ try {
 		$fetch_start_id	= 0;
 	}
 
-	$stmt				= $dbh->prepare('SELECT COUNT(*) FROM "Stage" WHERE "Mode"=:replay AND "State"=:published');
-	$stmt->bindValue(":replay", 'replay', PDO::PARAM_STR);
-	$stmt->bindValue(":published", 'published', PDO::PARAM_STR);
+	// フィルターを取得
+	$filter			= filter_input(INPUT_GET, 'filter');
+	$stmt			= $dbh->prepare('SELECT "ID" FROM "StageTagData" WHERE "IdentifierString"=:filter');
+	$stmt->bindValue(":filter", $filter, PDO::PARAM_STR);
 	$stmt->execute();
-	$stage_num			= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+	$filter_tag_id	= $stmt->fetch(PDO::FETCH_COLUMN, 0);
 
-	$filter				= filter_input(INPUT_GET, 'filter');
+	// ステージの数を取得
+	$stage_num = 0;
+	if (!$filter_tag_id) {
+
+		// フィルタリングせずに、すべてのステージの数を取得
+		$stmt			= $dbh->prepare('SELECT COUNT(*) FROM "Stage" WHERE "Mode"=:replay AND "State"=:published');
+		$stmt->bindValue(":replay", 'replay', PDO::PARAM_STR);
+		$stmt->bindValue(":published", 'published', PDO::PARAM_STR);
+		$stmt->execute();
+		$stage_num		= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+
+	} else {
+
+		// フィルタリングされたステージの数を取得
+		$stmt			= $dbh->prepare('SELECT COUNT(*) FROM "Stage" WHERE "Mode"=:replay AND "State"=:published AND "ID" IN (SELECT "StageID" FROM "StageTagMap" WHERE "TagID"=:filter_tag_id)');
+		$stmt->bindValue(":replay", 'replay', PDO::PARAM_STR);
+		$stmt->bindValue(":published", 'published', PDO::PARAM_STR);
+		$stmt->bindValue(":filter_tag_id", $filter_tag_id, PDO::PARAM_INT);
+		$stmt->execute();
+		$stage_num		= $stmt->fetch(PDO::FETCH_COLUMN, 0);
+
+	}
+
+
 
 	include('view.php');
 
