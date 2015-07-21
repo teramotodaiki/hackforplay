9555dd83f78aec3c521aa7db8fb8168dcea2def0
teramotodaiki <i101323@gmail.com>
2015-07-21 12:27:23 +0900

    バックスペースキーによるブラウザバックを制限

--- s/lib/rpg.js
+++ s/lib/rpg.js
@@ -7,4 +7,19 @@
 			return window;
 		}
 	});
+
+	var browserback_by_key = false;
+	$(document).on('keydown', function(event) {
+		if (event.keyCode === 8) {
+			browserback_by_key = true;
+			setTimeout(function() {
+				browserback_by_key = false;
+			}, 10);
+		}
+	});
+	$(window).on('beforeunload', function(event) {
+		if (browserback_by_key) {
+			return 'ゲームは ほぞんされて いません\nまえの ページに もどりますか？';
+		}
+	});
 })();
