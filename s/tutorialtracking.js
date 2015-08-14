668b252afd54d405a58f4a8ec268b792f75c5316
teramotodaiki <i101323@gmail.com>
2015-08-14 14:27:08 +0900

    ヒントをみたときのカウントとコードの最適化

--- s/tutorialtracking.js
+++ s/tutorialtracking.js
@@ -31,48 +31,58 @@ $(function () {
 		});
 	})();
 
-	// ゲームのSTARTとCLEARをキャッチしてログを記録する
+	// ログの取得と設定
 	(function() {
 		var storageLogIdentifier = 'tutorial_tracking_log';
-		var log_json = localStorage.getItem(storageLogIdentifier); // ログのJSON値
-		var log = log_json ? $.parseJSON(log_json) : { values: [] }; // ログオブジェクト(localStorageに値がないとき、新しく作る)
-
-		log.values.push({
-			stageid: getParam('id'),
-			field: 'start',
-			value: (new Date().getTime() / 1000) >> 0
-		});
-		log_json = JSON.stringify(log);
-		localStorage.setItem(storageLogIdentifier, log_json);
-
-		$(window).on('message', function(event) {
-			if (event.originalEvent.data === 'clear') {
-				log_json = localStorage.getItem(storageLogIdentifier);
-				log = log_json ? $.parseJSON(log_json) : { values: [] };
-
-				log.values.push({
-					stageid: getParam('id'),
-					field: 'clear',
-					value: (new Date().getTime() / 1000) >> 0
-				});
-				log_json = JSON.stringify(log);
-				localStorage.setItem(storageLogIdentifier, log_json);
-
-				$.post('../stage/logintutorial.php', {
-					key: tracking.key,
-					log: tracking.log
-				} , function(data, textStatus, xhr) {
-					console.log(data);
-				});
-			}
-		});
-
 		Object.defineProperty(tracking, 'log', {
 			get: function() {
-				return log_json;
+				return localStorage.getItem(storageLogIdentifier); // ログのJSON値
+			},
+			set: function(value) {
+				localStorage.setItem(storageLogIdentifier, value); // ログのJSON値
 			}
 		});
 	})();
+
+	(function() {
+
+		// ゲームのSTARTをキャッチしてログを記録する
+		addLog('start', (new Date().getTime() / 1000) >> 0);
+
+		$(window).on('message', function(event) {
+			if (event.originalEvent.data === 'clear') {
+
+				// ゲームのCLEARをキャッチしてログを記録する
+				addLog('clear', (new Date().getTime() / 1000) >> 0);
+			}
+		});
+
+		// ヒントをみたとき、カウントされる
+		$('#youtubeModal').on('show.bs.modal', function() {
+
+			addLog('hint', (new Date().getTime() / 1000) >> 0);
+		});
+	})();
+
+	// ロギング
+	function addLog (_field, _value) {
+
+		// ログオブジェクト(localStorageに値がないとき、新しく作る)
+		var log = tracking.log ? $.parseJSON(tracking.log) : { values: [] };
+		log.values.push({
+			stageid: getParam('id'),
+			field: _field,
+			value: _value
+		});
+		tracking.log = JSON.stringify(log);
+
+		$.post('../stage/logintutorial.php', {
+			key: tracking.key,
+			log: tracking.log
+		} , function(data, textStatus, xhr) {
+			console.log(data);
+		});
+	}
 
 	// ステージパラメータを取得
 	function getParam(key){
