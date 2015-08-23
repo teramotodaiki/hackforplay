9fd881558edfb1d9d8271c90f6fad8259d756313
teramotodaiki <i101323@gmail.com>
2015-08-23 18:50:51 +0900

    タブのpaddingを0に

--- s/view.php
+++ s/view.php
@@ -345,15 +345,15 @@ $retry 	= filter_input(INPUT_GET, "retry", FILTER_VALIDATE_BOOLEAN);
 	</div>
 	<div class="container container-tab hidden">
 		<div class="row">
-			<div class="col-xs-12">
+			<div class="col-xs-12 no-padding">
 				<div class="h4p_tab-top">
 					<img src="img/tab_material_top.png" height="100" width="60" alt="">
 				</div>
 			</div>
-			<div class="col-xs-12">
+			<div class="col-xs-12 no-padding">
 				<div class="h4p_tab-middle"></div>
 			</div>
-			<div class="col-xs-12">
+			<div class="col-xs-12 no-padding">
 				<div class="h4p_tab-bottom">
 					<img src="img/tab_material_bottom.png" height="17" width="60" alt="">
 				</div>
