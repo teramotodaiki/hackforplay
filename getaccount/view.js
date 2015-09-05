2d395cfd48ee8b5b9596591462f1947a4a1f439d
teramotodaiki <i101323@gmail.com>
2015-09-05 18:00:34 +0900

    メールアドレス　Validation

--- getaccount/view.js
+++ getaccount/view.js
@@ -59,6 +59,17 @@ $(function () {
 
 	});
 
+	// メールアドレス　メール送信前のValidation（Validationしてボタンをアクティブにする）
+	$('#modal-signup-email form#signup button[type="submit"]').attr('disabled', true);
+	setInputRoutine('#modal-signup-email form#signup', function(){
+		var policy = $(this).find('#policy').is(':checked');
+		var count = 0;
+		$(this).find('input').each(function(index, el) {
+			if($(el).val() === '') count++;
+		});
+		$(this).find('button[type="submit"]').attr('disabled', count > 0 || !policy);
+	});
+
 	// メールアドレス 仮登録
 	$('#modal-signup-email form#signup').submit(function(event) {
 		event.preventDefault();
@@ -88,4 +99,21 @@ $(function () {
 		$modal.find('.modal-body').addClass('hidden');
 		$modal.find('.modal-page-4').removeClass('hidden');
 	});
+
+	// ユーティリティ
+
+	// 'selector' element内のinputにfocusされている間のみroutineを実行し続ける処理をセット
+	function setInputRoutine (selector, routine) {
+		var _intervalID = null;
+		var $element = $(selector);
+		$element.find('input').on('focus', function() {
+			clearInterval(_intervalID);
+			_intervalID = setInterval(routine.bind($element.get(0)), 100);
+		}).on('blur', function() {
+			clearInterval(_intervalID);
+		});
+		$element.find('select,input').on('change', function() {
+			routine.call($element.get(0));
+		});
+	}
 });
