633a670d2fddf1d199621f3dda004427b378249b
teramotodaiki <i101323@gmail.com>
2015-09-05 12:14:11 +0900

    アカウントを作成するページ　仮のビュー

---
+++ getaccount/view.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head prefix="og: http://ogp.me/ns#">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Get Account hackforplay</title>
+	<?php require_once '../library.php' ?>
+</head>
+<body class="">
+	<?php require_once '../analyticstracking.php' ?>
+	<?php require_once '../fb-root.php' ?>
+	<?php require_once '../sendattendance.php'; ?>
+	<?php require_once '../view/authmodal.php'; ?>
+	<?php require_once '../view/header.php'; ?>
+	<script type="text/javascript" charset="utf-8">
+	// ログインページ用のTwitter OAuth認証
+	$(function() {
+		var authed = '/loginsuccess.php';
+		var login_successed = '/?debug=fromgetaccount'; // トップページに移動
+		$('.panel-login a#button-loginwithtwitter').attr('href',
+			'/loginwithtwitter.php?authed=' + encodeURIComponent(authed) +
+			'&login_successed=' + encodeURIComponent(login_successed));
+	});
+	</script>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 text-center">
+				<img src="../img/768/topreback.png" class="img-responsive center-block" alt="">
+			</div>
+			<div class="col-xs-12 col-lg-offset-3 col-lg-6">
+				<div class="panel panel-default margin-top-sm margin-bottom-lg">
+					<div class="panel-body text-center">
+						<h3><b>アカウントを てにいれた！</b></h3>
+						<p>おめでとうございます。すきなタイプを えらんでください</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>かんたん とうろく</h3>
+					</div>
+					<div class="panel-body">
+						<p>texttexttexttexttexttexttexttexttexttext</p>
+						<button type="button" class="btn btn-lg btn-primary">けってい</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>メールアドレスで とうろく</h3>
+					</div>
+					<div class="panel-body">
+						<p>texttexttexttexttexttexttexttexttexttext</p>
+						<button type="button" class="btn btn-lg btn-primary">けってい</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>ツイッターで とうろく</h3>
+					</div>
+					<div class="panel-body">
+						<p>texttexttexttexttexttexttexttexttexttext</p>
+						<button type="button" class="btn btn-lg btn-primary">けってい</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php require_once '../view/footer.php' ?>
+</body>
+</html>
