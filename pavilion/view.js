323ec4d68d4ec8829d0a8d7dfe8e963ee0520e49
teramotodaiki <i101323@gmail.com>
2015-09-09 11:48:44 +0900

    モーダル　仮のビュー

--- pavilion/view.js
+++ pavilion/view.js
@@ -4,15 +4,34 @@ $(function () {
 	$item = $('.quest-item-sample');
 
 	// レイアウト
-	console.log(result.Quests);
-	result.Quests.forEach(function(item) {
+	result.Quests.forEach(function(item, index) {
 		var current = $item.clone(true, true);
 		current.removeClass('hidden');
 		current.find('.item-ID').text(item.ID);
 		current.find('.item-QuestThumbnail').attr('src', item.Levels[0].Thumbnail);
-		current.find('.item-Link').attr('href', '/s/?mode=quest&level=' + item.Levels[0].ID);
+		current.find('.item-Modal').data('index', index);
 
 		this.append(current);
 
 	}, $item.parent());
+
+	// クエストモーダル
+	$('#questModal').on('show.bs.modal', function(event) {
+
+		var index = $(event.relatedTarget).data('index');
+
+		if (result.Quests[index]) {
+
+			$('#questModal .row').children().remove();
+			result.Quests[index].Levels.forEach(function(item) {
+				var $div = $('<div>').addClass('col-xs-4');
+				var $img = $('<img>').addClass('img-responsive').attr('src', item.Thumbnail);
+
+				$div.append($img);
+				$('#questModal .row').append($div);
+
+			});
+		}
+
+	});
 });
--- pavilion/view.php
+++ pavilion/view.php
@@ -16,6 +16,17 @@
 	var result = JSON.parse('<?php echo $result_json; ?>');
 	</script>
 	<?php include '../view/header.php'; ?>
+	<div class="modal fade" id="questModal" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+			    <div class="modal-body">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <div class="row">
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
@@ -29,8 +40,8 @@
 			<div class="col-xs-12 col-sm-4 quest-item-sample hidden">
 				<p class="item-ID"></p>
 				<img alt="" class="img-responsive item-QuestThumbnail">
-				<a href="#" class="btn btn-link btn-block item-Link">
-					Link
+				<a href="#" class="btn btn-link btn-block item-Modal" data-toggle="modal" data-target="#questModal">
+					Modal
 				</a>
 			</div>
 		</div>
