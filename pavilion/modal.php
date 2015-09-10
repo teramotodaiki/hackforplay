0a69230cd9236ba6294ec744cbed5be6a41d8159
teramotodaiki <i101323@gmail.com>
2015-09-10 16:29:41 +0900

    モーダルとビューを別ファイルに分割

---
+++ pavilion/modal.php
@@ -0,0 +1,55 @@
+<div class="modal fade" id="questModal" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		    <div class="modal-body">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <div class="body-part-1">
+		        	<p class="Cleared">
+		        		<span class="true-text hidden">クエストクリア！</span>
+		        		<span class="false-text hidden"><ruby>条件<rt>じょうけん</rt></ruby>：すべてのステージをクリアする</span>
+		        	</p>
+		        	<p class="Restaged">
+		        		<span class="true-text hidden">ステージを改造した！</span>
+		        		<span class="false-text hidden"><ruby>条件<rt>じょうけん</rt></ruby>：いずれかのステージを改造する</span>
+		        	</p>
+		        </div>
+		        <div class="body-part-2">
+		        	<p>クエスト 1 - <span class="Number"></span></p>
+		        	<p><span class="Challengers"></span>人<ruby>挑戦<rt>ちょうせん</rt></ruby>し、<span class="Winners"></span>人クリアしました</p>
+		        	<p><ruby>主<rt>おも</rt></ruby>なクリエイター</p>
+		        	<p class="Authors">...</p>
+		        </div>
+		        <div class="row">
+		        </div>
+		    </div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="kitModal" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		    <div class="modal-body">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <div class="body-part-1">
+		        	<p class="Restaged">
+		        		<span class="true-text hidden">ステージを作成した！</span>
+		        		<span class="false-text hidden"><ruby>条件<rt>じょうけん</rt></ruby>：ステージを作成する</span>
+		        	</p>
+		        </div>
+		        <div class="body-part-2">
+					<p>オリジナルステージ作成</p>
+					<p>今までに<span class="Restagers"></span>人<ruby>があたらしいステージを作成した
+					<p><span class="Explain"></span></p>
+		        </div>
+		        <div class="row">
+		        	<div class="col-xs-4">
+		        		<a href="#" class="link-restage" title="">
+		        			<img src="" class="img-responsive Thumbnail" alt="">
+		        		</a>
+		        		<p class="text-center Title"></p>
+		        	</div>
+		        </div>
+		    </div>
+		</div>
+	</div>
+</div>
