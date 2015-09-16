637616e6f76ab1e5bd197a8778aa14cc5aa2bdfd
teramotodaiki <i101323@gmail.com>
2015-09-16 14:01:27 +0900

    Fixed パビリオンの解放処理を修正

--- town/index.php
+++ town/index.php
@@ -34,9 +34,8 @@ try {
 	$kit_restaged	= $stmt->fetch(PDO::FETCH_COLUMN);
 
 	// まだ解放されていないPavilionで、RequiredAchievementsが十分なものがあれば、そのIDを取得
-	$stmt		= $dbh->prepare('SELECT p."ID" AS pID,m."ID" AS mID FROM "Pavilion" AS p LEFT OUTER JOIN "PavilionUserMap" AS m ON p."ID"=m."PavilionID" WHERE p."RequiredAchievements"<=:achievements AND (m."ID" IS NULL OR m."UserID"=:userid AND m."Certified"=:false)');
+	$stmt		= $dbh->prepare('SELECT p."ID" AS pID,m."ID" AS mID, m."Certified" FROM "Pavilion" AS p LEFT OUTER JOIN "PavilionUserMap" AS m ON p."ID"=m."PavilionID" AND m."UserID"=:userid WHERE p."RequiredAchievements"<=:achievements');
 	$stmt->bindValue(":achievements", $quest_cleared + $quest_restaged + $kit_restaged, PDO::PARAM_INT);
-	$stmt->bindValue(":false", FALSE, PDO::PARAM_BOOL);
 	$stmt->bindValue(":userid", $session_userid, PDO::PARAM_INT);
 	$stmt->execute();
 	$certifying	= $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -46,23 +45,25 @@ try {
 		$stmt_update	= $dbh->prepare('UPDATE "PavilionUserMap" SET "Certified"=:true WHERE "ID"=:map_id');
 		$stmt_insert	= $dbh->prepare('INSERT INTO "PavilionUserMap" ("PavilionID","UserID","Certified") VALUES(:pavilion_id,:userid,:true)');
 		foreach ($certifying as $key => $value) {
-			if ($value['mID']) {
-				// Mapが存在する 更新
-				$stmt_update->bindValue(":true", TRUE, PDO::PARAM_INT);
-				$stmt_update->bindValue(":map_id", $value['mID'], PDO::PARAM_INT);
-				$stmt_update->execute();
-			} else {
-				// Mapが存在しない 追加
-				$stmt_insert->bindValue(":pavilion_id", $value['pID'], PDO::PARAM_INT);
-				$stmt_insert->bindValue(":userid", $session_userid, PDO::PARAM_INT);
-				$stmt_insert->bindValue(":true", TRUE, PDO::PARAM_INT);
-				$stmt_insert->execute();
+			if (!$value['Certified']) {
+				if ($value['mID']) {
+					// Mapが存在する 更新
+					$stmt_update->bindValue(":true", TRUE, PDO::PARAM_INT);
+					$stmt_update->bindValue(":map_id", $value['mID'], PDO::PARAM_INT);
+					$stmt_update->execute();
+				} else {
+					// Mapが存在しない 追加
+					$stmt_insert->bindValue(":pavilion_id", $value['pID'], PDO::PARAM_INT);
+					$stmt_insert->bindValue(":userid", $session_userid, PDO::PARAM_INT);
+					$stmt_insert->bindValue(":true", TRUE, PDO::PARAM_INT);
+					$stmt_insert->execute();
+				}
 			}
 		}
 	}
 
 	// パビリオンの情報を取得 (とりあえず　すべて) (Certificationと合同にすれば最適化できる)
-	$stmt	= $dbh->prepare('SELECT p."ID",p."DisplayName",p."RequiredAchievements",m."Certified" FROM "Pavilion" AS p LEFT OUTER JOIN "PavilionUserMap" AS m ON p."ID"=m."PavilionID" WHERE m."ID" IS NULL OR m."UserID"=:userid');
+	$stmt	= $dbh->prepare('SELECT p."ID",p."DisplayName",p."RequiredAchievements",m."Certified" FROM "Pavilion" AS p LEFT OUTER JOIN "PavilionUserMap" AS m ON p."ID"=m."PavilionID" AND m."UserID"=:userid');
 	$stmt->bindValue(":userid", $session_userid, PDO::PARAM_INT);
 	$stmt->execute();
 	$pavilions	= $stmt->fetchAll(PDO::FETCH_ASSOC);
