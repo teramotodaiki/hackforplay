62e03ecd1040501abfeb28657ad999e08f3ff800
teramotodaiki <i101323@gmail.com>
2015-10-07 17:56:43 +0900

    音の重なりを除去

--- s/kit/music_game_project/main.js
+++ s/kit/music_game_project/main.js
@@ -690,7 +690,7 @@ window.addEventListener('load', function () {
                 this.state = 1;
                 Hack.point += 1;
                 if (game.assets[Hack.soundEffectPath]) {
-                    game.assets[Hack.soundEffectPath].play(true);
+                    game.assets[Hack.soundEffectPath].play(false);
                 }
             } else {
                 this.state = 2;
