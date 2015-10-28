e215ef4058f09dc5904af85daac5ef9c7e12d063
teramotodaiki <i101323@gmail.com>
2015-10-28 17:19:34 +0900

    コメントの既読トリガー

--- comments/index.php
+++ comments/index.php
@@ -12,6 +12,16 @@ try {
 	$session_userid	= isset($_SESSION['UserID']) ? $_SESSION['UserID'] : NULL;
 	session_commit();
 
+	// コメント通知をすべて既読に
+	$stmt	= $dbh->prepare('UPDATE "Notification" SET "State"=:read,"ReadUnixTime"=:time WHERE "UserID"=:userid AND "State"=:unread AND "Type"=:comment');
+	$stmt->bindValue(":read", 'read', PDO::PARAM_STR);
+	date_default_timezone_set('GMT');
+	$stmt->bindValue(":time", time(), PDO::PARAM_INT);
+	$stmt->bindValue(":userid", $session_userid, PDO::PARAM_INT);
+	$stmt->bindValue(":unread", 'unread', PDO::PARAM_STR);
+	$stmt->bindValue(":comment", 'comment', PDO::PARAM_STR);
+	$stmt->execute();
+
 	include 'view.php';
 
 } catch (Exception $e) {
