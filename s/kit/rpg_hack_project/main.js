bdaece2ae895ea33e44395caf0a234177d75d992
teramotodaiki <i101323@gmail.com>
2015-11-12 17:30:24 +0900

    Restaging Code

--- s/kit/rpg_hack_project/main.js
+++ s/kit/rpg_hack_project/main.js
@@ -1,10 +1,191 @@
 window.addEventListener('load', function(){
 
+	Hack.restagingCode =
+"// Game start\n"+
+"game.onload = function () {\n"+
+"\n"+
+"\tvar map = Hack.maps['room1'];\n"+
+"\tmap.load(); // Load Map;  Hack.defaultParentNode == map.scene\n"+
+"\n"+
+"\t// スライム\n"+
+"\tvar blueSlime = new BlueSlime();\n"+
+"\tblueSlime.locate(9, 5);\n"+
+"\n"+
+"\t// イモムシ\n"+
+"\tvar insect = new Insect();\n"+
+"\tinsect.locate(8, 5);\n"+
+"\n"+
+"\t// クモ\n"+
+"\tvar spider = new Spider();\n"+
+"\tspider.locate(7, 5);\n"+
+"\n"+
+"\t// コウモリ\n"+
+"\tvar bat = new Bat();\n"+
+"\tbat.locate(6, 5);\n"+
+"\n"+
+"\t// ドラゴン\n"+
+"\tvar dragon = new Dragon();\n"+
+"\tdragon.locate(5, 5);\n"+
+"\n"+
+"\t// ミノタウロス\n"+
+"\tvar minotaur = new Minotaur();\n"+
+"\tminotaur.locate(4, 5);\n"+
+"\n"+
+"\t// 男の子\n"+
+"\tvar boy = new Boy();\n"+
+"\tboy.locate(5, 6);\n"+
+"\n"+
+"\t// 女の子\n"+
+"\tvar girl = new Girl();\n"+
+"\tgirl.locate(6, 6);\n"+
+"\n"+
+"\t// 女の人\n"+
+"\tvar woman = new Woman();\n"+
+"\twoman.locate(7, 6);\n"+
+"\n"+
+"\t// のぼりかいだん\n"+
+"\tvar stair = new MapObject('UpStair');\n"+
+"\tstair.locate(1, 7);\n"+
+"\tstair.onplayerenter = function () {\n"+
+"\t\t\n"+
+"\t\t// When enter... ふまれたら...\n"+
+"\t\tHack.changeMap('room2');\n"+
+"\t\t\n"+
+"\t};\n"+
+"\n"+
+"\t// トゲのわな\n"+
+"\tvar trap = new MapObject('Trap');\n"+
+"\ttrap.locate(2, 5);\n"+
+"\ttrap.onplayerenter = function () {\n"+
+"\t\t\n"+
+"\t\t// When enter... ふまれたら...\n"+
+"\t\tthis.frame = MapObject.Dictionaly['UsedTrap'];\n"+
+"\t\t\n"+
+"\t};\n"+
+"\ttrap.onplayerleave = function () {\n"+
+"\t\t\n"+
+"\t\t// When leave... はなれたら\n"+
+"\t\tthis.frame = MapObject.Dictionaly['Trap'];\n"+
+"\t\t\n"+
+"\t};\n"+
+"\ttrap.onattacked = function (event) {\n"+
+"\t\t\n"+
+"\t\t// When attacked... こうげきされたら\n"+
+"\t\tthis.moveBy(event.vecotr.x * 32, event.vector.y * 32);\n"+
+"\t\t\n"+
+"\t};\n"+
+"\n"+
+"\t// プレイヤー（騎士）\n"+
+"\tvar player = Hack.player = new Player();\n"+
+"\tplayer.locate(1, 5);\n"+
+"\n"+
+"\t// room2 に行ったとき\n"+
+"\tHack.maps['room2'].onload = function () {\n"+
+"\t\t\n"+
+"\t\t// くだりかいだん\n"+
+"\t\tvar stair2 = new MapObject('DownStair');\n"+
+"\t\tstair2.locate(1, 7);\n"+
+"\t\tstair2.onplayerenter = function () {\n"+
+"\t\t\t\n"+
+"\t\t\t// When enter... ふまれたら...\n"+
+"\t\t\tHack.changeMap('room1');\n"+
+"\t\t\t\n"+
+"\t\t};\n"+
+"\t\t\n"+
+"\t};\n"+
+"\n"+
+"};\n"+
+"\n"+
+"// Before game start\n"+
+"Hack.onload = function () {\n"+
+"\n"+
+"\tMapObject.Dictionaly = {\n"+
+"\t\t'Pot': 400,\t\t\t'Rock': 401,\t\t'UpStair': 402,\n"+
+"\t\t'Box': 420,\t\t\t'Flower': 421,\t\t'DownStair': 422,\n"+
+"\t\t'Trap': 440,\t\t'UsedTrap': 441,\t'Step': 442,\n"+
+"\t\t'Castle': 500,\t\t'Village': 501,\t\t'Cave': 502,\n"+
+"\t\t'Tree': 520,\t\t'Table': 521\n"+
+"\t};\n"+
+"\n"+
+"\tHack.maps = [];\n"+
+"\tHack.maps['room1'] = new RPGMap(32, 32);\n"+
+"\tHack.maps['room1'].imagePath = 'enchantjs/x2/map1.gif';\n"+
+"\tHack.maps['room1'].bmap.loadData([\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],\n"+
+"\t\t[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]\n"+
+"\t]);\n"+
+"\tHack.maps['room1'].cmap = [\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]\n"+
+"\t];\n"+
+"\tHack.maps['room2'] = new RPGMap(32, 32);\n"+
+"\tHack.maps['room2'].imagePath = 'enchantjs/x2/map1.gif';\n"+
+"\tHack.maps['room2'].bmap.loadData([\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],\n"+
+"\t\t[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]\n"+
+"\t]);\n"+
+"\tHack.maps['room2'].cmap = [\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],\n"+
+"\t\t[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]\n"+
+"\t];\n"+
+"};\n"+
+"\n"+
+"// EnchantBook\n"+
+"Hack.hint = \n"+
+"\t\"//  -            =\\n\"+\n"+
+"\t\"// -  BASIC CODE  =\\n\"+\n"+
+"\t\"//  -            =\\n\"+\n"+
+"\t\"Hack.player.locate(5, 5);  // Teleportation\\n\"+\n"+
+"\t\"Hack.player.direction = 2; // Turn\\n\"+\n"+
+"\t\"\\n\"+\n"+
+"\t\"\\n\"+\n"+
+"\t\"//  *            +\\n\"+\n"+
+"\t\"// *  EXTRA CODE  +  Remove // to use.\\n\"+\n"+
+"\t\"//  *            +   // をけして つかおう!\\n\"+\n"+
+"\t\"\\n\"+\n"+
+"\t\"// Hack.changeMap('room2');\\n\"+\n"+
+"\t\"// Hack.log('wwwwwwww');\\n\"+\n"+
+"\t\"\\n\"+\n"+
+"\t\"\\n\";\n"+
+"\n";
+
 	var game = enchant.Core.instance;
 	game.preload('enchantjs/monster1.gif', 'enchantjs/monster2.gif', 'enchantjs/monster3.gif', 'enchantjs/monster4.gif', 'enchantjs/bigmonster1.gif', 'enchantjs/bigmonster2.gif', 'enchantjs/x2/map1.gif', 'enchantjs/x1.5/chara0.png', 'enchantjs/x1.5/chara5.png', 'hackforplay/enchantbook.png');
 	game.keybind(' '.charCodeAt(0), 'a');
 
-	Hack.onload = function () {
+	Hack.onload = Hack.onload || function () {
 
 		MapObject.Dictionaly = {
 			'Pot': 400,			'Rock': 401,		'UpStair': 402,
@@ -18,53 +199,27 @@ window.addEventListener('load', function(){
 		Hack.maps['room1'] = new RPGMap(32, 32);
 		Hack.maps['room1'].imagePath = 'enchantjs/x2/map1.gif';
 		Hack.maps['room1'].bmap.loadData([
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
-			[  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322],
+			[322,322,322,322,322,322,322,322,322,322,322,322,322,322,322]
 		]);
 		Hack.maps['room1'].cmap = [
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
-		];
-		Hack.maps['room2'] = new RPGMap(32, 32);
-		Hack.maps['room2'].imagePath = 'enchantjs/x2/map1.gif';
-		Hack.maps['room2'].bmap.loadData([
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-			[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-		]);
-		Hack.maps['room2'].cmap = [
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-			[  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
 			[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
 		];
 
@@ -106,65 +261,13 @@ window.addEventListener('load', function(){
 		Hack.textarea.height = 32;
 	});
 
-	game.onload = function () {
+	game.onload = game.onload || function () {
 
         var map = Hack.maps['room1'];
         map.load(); // Load Map;  Hack.defaultParentNode == map.scene
 
-        var blueSlime = new BlueSlime();
-        blueSlime.locate(9, 5);
-
-        var insect = new Insect();
-        insect.locate(8, 5);
-
-        var spider = new Spider();
-        spider.locate(7, 5);
-
-        var bat = new Bat();
-        bat.locate(6, 5);
-
-        var dragon = new Dragon();
-        dragon.locate(5, 5);
-
-        var minotaur = new Minotaur();
-        minotaur.locate(4, 5);
-
-        var boy = new Boy();
-        boy.locate(5, 6);
-
-        var girl = new Girl();
-        girl.locate(6, 6);
-
-        var woman = new Woman();
-        woman.locate(7, 6);
-
-        var stair = new MapObject('UpStair');
-        stair.locate(1, 7);
-        stair.onplayerenter = function () {
-			Hack.changeMap('room2');
-		};
-
-		var trap = new MapObject('Trap');
-		trap.locate(2, 5);
-		trap.onplayerenter = function () {
-			this.frame = MapObject.Dictionaly['UsedTrap'];
-		};
-		trap.onplayerleave = function () {
-			this.frame = MapObject.Dictionaly['Trap'];
-		};
-		trap.onattacked = function (event) {
-		};
-
         var player = Hack.player = new Player();
         player.locate(1, 5);
-
-        Hack.maps['room2'].onload = function () {
-			var stair2 = new MapObject('DownStair');
-			stair2.locate(1, 7);
-			stair2.onplayerenter = function () {
-				Hack.changeMap('room1');
-			};
-        };
 
     };
 
