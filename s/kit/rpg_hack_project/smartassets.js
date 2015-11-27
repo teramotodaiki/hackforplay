f8d70c5ffbdd3099278379ad2a13e58d538714df
teramotodaiki <i101323@gmail.com>
2015-11-27 17:46:48 +0900

    Send Smart Assets

---
+++ s/kit/rpg_hack_project/smartassets.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+	Hack.on('load', function() {
+		this.smartAssets = [
+		{
+			name: 'Warp',
+			lines: [
+			'// きょうぼうな スライム',
+			'var enemy = new BlueSlime();',
+			'enemy.locate(5, 3);']
+		}
+		];
+	});
+});
