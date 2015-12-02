9db9c1d355b4321e52015e40257d3c352a03748a
teramotodaiki <i101323@gmail.com>
2015-12-02 16:11:32 +0900

    Hack.restagingCode can recieve Function

--- s/lib/hack.js
+++ s/lib/hack.js
@@ -556,6 +556,17 @@ window.addEventListener('load', function() {
 			return sessionStorage.getItem('restaging_code');
 		},
 		set: function(code){
+			if (code instanceof Function) {
+				// 関数の文字列化
+				var str = code.toString().match(/^function[^\{]*\{\n?(\s*)([\s\S]*)\}$/);
+				if (str !== null) {
+					var indent = str[1].match(/(.*)$/)[0];
+					code = ('\n' + str[1] + str[2]).split('\n' + indent).join('\n').substr(1);
+				} else {
+					// 切り分けのミス
+					Hack.log('Hack.restagingCode hasnot set the function because hack.js is wrong. See hack.js and fix it');
+				}
+			}
 			switch (sessionStorage.getItem('stage_param_game_mode')) {
 				case 'official':
 				case 'extend':
