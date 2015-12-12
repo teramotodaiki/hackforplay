8c4f08204f72785f970a9c5870d00b37919b0c00
teramotodaiki <i101323@gmail.com>
2015-12-12 19:01:27 +0900

    Fix for compatibility

--- s/kit/rpg_hack_project/main.js
+++ s/kit/rpg_hack_project/main.js
@@ -72,20 +72,26 @@ window.addEventListener('load', function () {
 			});
 		}
 
-		MapObject.dictionary = MapObject.dictionary || {
-			clay: 320,		clayWall: 340,	clayFloor: 323,
-			stone: 321,		stoneWall: 341,	stoneFloor: 342,
-			warp: 324,		warpRed: 325,
-			warpGreen: 326,	warpYellow: 327,
-			pot: 400,		rock: 401,		upStair: 402,
-			box: 420,		flower: 421,	downStair: 422,
-			trap: 440,		usedTrap: 441,	step: 442,
-			castle: 500,	village: 501,	cave: 502,
-			tree: 520,		table: 521,		openedBox: 522,
-			beam: 540,		diamond: 560,	sapphire: 561,
-			ruby: 562,		heart: 563,		skull: 564,
-			coin: 565,		star: 566,		key: 567
-		};
+		// 互換性維持
+		if (MapObject.Dictionaly) {
+			MapObject.dictionary = {
+				clay: 320,		clayWall: 340,	clayFloor: 323,
+				stone: 321,		stoneWall: 341,	stoneFloor: 342,
+				warp: 324,		warpRed: 325,
+				warpGreen: 326,	warpYellow: 327,
+				pot: 400,		rock: 401,		upStair: 402,
+				box: 420,		flower: 421,	downStair: 422,
+				trap: 440,		usedTrap: 441,	step: 442,
+				castle: 500,	village: 501,	cave: 502,
+				tree: 520,		table: 521,		openedBox: 522,
+				beam: 540,		diamond: 560,	sapphire: 561,
+				ruby: 562,		heart: 563,		skull: 564,
+				coin: 565,		star: 566,		key: 567
+			};
+			Object.keys(MapObject.Dictionaly).forEach(function (key) {
+				MapObject.dictionary[key] = MapObject.Dictionaly[key];
+			});
+		}
 	});
 
 	game.on('load', function() {
