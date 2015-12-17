6fbe66bcf9c2e1d58af52dcb318b7f2af76e6070
teramotodaiki <i101323@gmail.com>
2015-12-17 13:52:48 +0900

    Fix attack/walk behavior

--- s/kit/rpg_hack_project/rpgobjects.js
+++ s/kit/rpg_hack_project/rpgobjects.js
@@ -198,6 +198,7 @@ window.addEventListener('load', function () {
 			return stopInterval.bind(this);
 		},
 		attack: function (count, continuous) {
+			if (this.behavior !== BehaviorTypes.Idle) return;
 			var c = typeof count === 'number' ? count >> 0 : 1;
 			var f = this.forward;
 			if (continuous) {
@@ -227,6 +228,7 @@ window.addEventListener('load', function () {
             }
 		},
 		walk: function (distance, continuous) {
+			if (this.behavior !== BehaviorTypes.Idle) return;
 			var f = this.forward, d = typeof distance === 'number' ? distance >> 0 : 1, s = Math.sign(d);
 			var _x = this.mapX + f.x * s, _y = this.mapY + f.y * s;
 			// Map Collision
@@ -266,6 +268,7 @@ window.addEventListener('load', function () {
 					if (continuous) this.behavior = BehaviorTypes.Idle;
 				}, 1);
 			}
+			this._preventFrameHits = hits;
 		}
 	});
 
