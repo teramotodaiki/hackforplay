58a7b6183ca0b5d7483f44579c7c297699f95ec3
teramotodaiki <i101323@gmail.com>
2015-12-22 15:13:58 +0900

    window.random method

--- s/kit/rpg_hack_project/main.js
+++ s/kit/rpg_hack_project/main.js
@@ -277,4 +277,23 @@ window.addEventListener('load', function () {
 		return this;
 	};
 
+	/* random
+	 * Random value between min to max (Detection type)
+	 * (int, int) ===> int
+	 * (float, int|float) ====> float
+	 * (value, value) ====> value ~ value
+	 * (value) ====> 0 ~ value
+	 * () ====> 0 ~ 1
+	*/
+	window.random = window.random || function (min, max) {
+		if (arguments.length === 0) return Math.random();
+		var _min = arguments.length >= 2 ? Math.min(min, max) : 0;
+		var _sub = arguments.length >= 2 ? Math.max(min, max) - _min : min;
+		if (min % 1 === 0 && (max === undefined || max % 1 === 0)) {
+			return _min + Math.random() * _sub >> 0; // integer
+		} else {
+			return _min + Math.random() * _sub;
+		}
+	};
+
 });
