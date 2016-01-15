61d3256f4e34bb98557b28fa30c56f8adfb4305c
teramotodaiki <i101323@gmail.com>
2016-01-15 10:58:59 +0900

    Exclude duplicated time

--- dashboard/sessiontime.php
+++ dashboard/sessiontime.php
@@ -13,9 +13,9 @@ try {
 	$session_userid	= isset($_SESSION['UserID']) ? $_SESSION['UserID'] : NULL;
 	session_commit();
 
-	$lastweek = (new DateTime())->modify('-7 days'); // Timezoneがあるので、多めにさかのぼる
+	$lastweek = (new DateTime())->modify('-8 days'); // Timezoneがあるので、多めにさかのぼる
 
-	$stmt	= $dbh->prepare('SELECT "Begin","End" FROM "Attendance" WHERE "UserID"=:userid AND "End">:lastweek');
+	$stmt	= $dbh->prepare('SELECT "Begin","End" FROM "Attendance" WHERE "UserID"=:userid AND "End">:lastweek ORDER BY "Begin"');
 	$stmt->bindValue(":userid", $session_userid, PDO::PARAM_INT);
 	$stmt->bindValue(":lastweek", $lastweek->format('Y-m-d H:i:s'), PDO::PARAM_STR);
 	$stmt->execute();
@@ -25,6 +25,8 @@ try {
 		$day		= $lastweek->modify('+1 day')->format('m/d');
 		$dist[$day]	= 0;
 	}
+
+	$cursor	= (new DateTime())->modify('-8 days'); // 初期化
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		if (!isset($row['Begin']) || !isset($row['End'])) {
 			continue;
@@ -32,8 +34,11 @@ try {
 		$begin	= new DateTime($row['Begin']);
 		$end	= new DateTime($row['End']);
 		$day	= $begin->format('m/d');
-		if (isset($dist[$day])) {
-			$dist[$day]	= $dist[$day] + $end->format('U') - $begin->format('U');
+		if (isset($dist[$day]) && $cursor < $end) {
+			$nextDay = new DateTime($begin->format('Y-m-') . ($begin->format('d') + 1), new DateTimeZone($begin->format('e')));
+			$diff = min($nextDay, $end)->format('U') - max($cursor, $begin)->format('U');
+			$dist[$day]	+= $diff;
+			$cursor	= min($nextDay, $end);
 		}
 	}
 
