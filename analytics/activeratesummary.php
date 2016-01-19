0f3b667f704e8a815ba58fb449676f5300779781
teramotodaiki <i101323@gmail.com>
2016-01-19 11:42:32 +0900

    Without End timestamp, and get User num

--- analytics/activeratesummary.php
+++ analytics/activeratesummary.php
@@ -1,6 +1,6 @@
 <?php
 /*
-過去1ヶ月間のアクティブ率(DAU/MAU比)を１日(00:00:00GMT - 23:59:59GMT)ごとに取得する
+過去1ヶ月間のアクティブユーザーを１日(00:00:00GMT - 23:59:59GMT)ごとに取得する
 Output:	no-data , parse-error , JSON{summary_of_activerate}
 {
 	labels : [計測スパン],
@@ -12,38 +12,31 @@ require_once '../preload.php';
 
 try {
 
-	$datetime	= (new DateTime())->modify('-29 days'); // 始点
-	$stmt	= $dbh->prepare('SELECT COUNT(DISTINCT "UserID") FROM "Attendance" WHERE "End">:_from AND "Begin"<:_to');
-
-	// MAU
-	$month_from	= $datetime->setTime( 0,  0,  0)->format("Y-m-d H:i:s");
-	$month_to	= (new DateTime())->setTime(23, 59, 59)->format("Y-m-d H:i:s");
-	$stmt->bindValue(":_from", $month_from, PDO::PARAM_STR);
-	$stmt->bindValue(":_to", $month_to, PDO::PARAM_STR);
+	$datetime	= (new DateTime())->modify('-1 month'); // 始点
+	$stmt	= $dbh->prepare('SELECT "UserID","Begin" FROM "Attendance" WHERE "Begin">:lastmonth ORDER BY "UserID"');
+	$stmt->bindValue(":lastmonth", $datetime->format('Y-m-d H:i:s'), PDO::PARAM_STR);
 	$stmt->execute();
-	$MAU		= $stmt->fetchColumn(0);
-	if ((int)$MAU === 0) {
-		exit('no-data');
+
+	// Label
+	$dist	= array();
+	for ($i=0; $i < 30; $i++) {
+		$date = $datetime->modify('+1 day')->format('m/d');
+		$dist[$date] = 0;
 	}
 
-	// DAU
-	for ($i=0; $i < 30; $i++) {
-		$_from	= $datetime->setTime( 0,  0,  0)->format("Y-m-d H:i:s");
-		$_to	= $datetime->setTime(23, 59, 59)->format("Y-m-d H:i:s");
-		$stmt->bindValue(":_from", $_from, PDO::PARAM_STR);
-		$stmt->bindValue(":_to", $_to, PDO::PARAM_STR);
-		$stmt->execute();
-		$DAU	= $stmt->fetchColumn(0);
-
-		$labels[$i]	= $datetime->format("D m/d");
-		$values[$i]	= (int)$DAU / (int)$MAU;
-
-		$datetime->modify('+1 day');
+	$maxUidEachDays = array_fill_keys(array_keys($dist), 0); // dynamic cache
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$date = (new DateTime($row['Begin']))->format('m/d');
+		$user = (int)$row['UserID'];
+		if (isset($maxUidEachDays[$date]) && $user > $maxUidEachDays[$date]) {
+			$dist[$date] ++;
+			$maxUidEachDays[$date] = $user;
+		}
 	}
 
 	$summary_of_activerate		= new stdClass;
-	$summary_of_activerate->values = $values;
-	$summary_of_activerate->labels = $labels;
+	$summary_of_activerate->values = array_values($dist);
+	$summary_of_activerate->labels = array_keys($dist);
 
 	$json = json_encode($summary_of_activerate);
 	if ($json === FALSE) {
