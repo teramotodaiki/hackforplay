8b2e1fa6d80a538b68d05f9c3747f3a9ef8c2d59
teramotodaiki <i101323@gmail.com>
2016-02-18 20:07:53 +0900

    Animation

--- dashboard/view.js
+++ dashboard/view.js
@@ -1,11 +1,20 @@
 $(function(){
 
 	$.post('./playcount.php', {}, function(data, textStatus, xhr) {
-		$('.item-dashboard-playcount').text(data);
+		showAnimation($('.item-dashboard-playcount'), data >> 0);
 	});
 
 	$.post('./restagecount.php', {}, function(data, textStatus, xhr) {
-		$('.item-dashboard-restagecount').text(data);
+		showAnimation($('.item-dashboard-restagecount'), data >> 0);
 	});
+
+	function showAnimation ($ele, value) {
+		var i = 0, end = 30;
+		var timer = setInterval(function () {
+			var showing = (++i) / end * value >> 0;
+			$ele.text(showing);
+			if (i >= end) clearInterval(timer);
+		}, 30);
+	}
 
 });
