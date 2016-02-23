1cd2d34813e4fbdfc6e9f009a593b753725faac7
teramotodaiki <i101323@gmail.com>
2016-02-23 18:00:37 +0900

    Set Script ID of Stage

---
+++ patch1.php
@@ -0,0 +1,20 @@
+<?php
+// Set scriptID to all stage
+
+try {
+
+	require_once 'preload.php';
+
+	$stmt	= $dbh->prepare('UPDATE "Stage" AS s SET s."ScriptID"=(SELECT MAX("ID") FROM "Script" AS script WHERE script."ProjectID"=s."ProjectID") WHERE s."ScriptID" IS NULL');
+	$result	= $stmt->execute();
+
+	var_dump($result);
+	exit();
+
+} catch (Exception $e) {
+	require_once '../exception/tracedata.php';
+	traceData($e);
+	die();
+}
+
+?>
