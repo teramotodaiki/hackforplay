62cdb3ae3e93ac9d31122aec788ccd464cde695b
teramotodaiki <i101323@gmail.com>
2016-02-23 18:42:56 +0900

    Fix patch

--- patch1.php
+++ patch1.php
@@ -5,7 +5,7 @@ try {
 
 	require_once 'preload.php';
 
-	$stmt	= $dbh->prepare('UPDATE "Stage" AS s SET s."ScriptID"=(SELECT MAX("ID") FROM "Script" AS script WHERE script."ProjectID"=s."ProjectID") WHERE s."ScriptID" IS NULL');
+	$stmt	= $dbh->prepare('UPDATE "Stage" SET "ScriptID"=(SELECT MAX("ID") FROM "Script" WHERE "Script"."ProjectID"="Stage"."ProjectID") WHERE "ScriptID" IS NULL');
 	$result	= $stmt->execute();
 
 	var_dump($result);
