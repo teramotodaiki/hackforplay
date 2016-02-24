150c2f88938160cd368ff46384e89b9665d92d28
teramotodaiki <i101323@gmail.com>
2016-02-24 16:48:37 +0900

    show progress

--- patch3.php
+++ patch3.php
@@ -8,6 +8,10 @@ $updated = 0;
 
 require_once 'preload.php';
 
+$stmt	= $dbh->prepare('SELECT COUNT(*) FROM "Script" WHERE "RawCode" IS NULL');
+$stmt->execute();
+$count = $stmt->fetch(PDO::FETCH_COLUMN);
+
 $stmt_se	= $dbh->prepare('SELECT "ID" FROM "Script" WHERE "RawCode" IS NULL');
 $stmt_se->execute();
 
@@ -15,26 +19,33 @@ $stmt_code	= $dbh->prepare('SELECT "Code"."Value" FROM "Code" INNER JOIN "Line" 
 
 $stmt_up	= $dbh->prepare('UPDATE "Script" SET "RawCode"=:code WHERE "ID"=:script_id');
 
-while (($row = $stmt_se->fetch(PDO::FETCH_ASSOC)) && $limit--) {
+try {
+	while (($row = $stmt_se->fetch(PDO::FETCH_ASSOC)) && $limit--) {
 
-	$stmt_code->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
-	$stmt_code->execute();
+		$stmt_code->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
+		$stmt_code->execute();
 
-	$lines	= $stmt_code->fetchAll(PDO::FETCH_COLUMN, 0);
-	if (empty($lines)) {
-		$code	= '';
-	} else {
-		$code	= implode("\n", $lines);
+		$lines	= $stmt_code->fetchAll(PDO::FETCH_COLUMN, 0);
+		if (empty($lines)) {
+			$code	= '';
+		} else {
+			$code	= implode("\n", $lines);
+		}
+
+		$stmt_up->bindValue(":code", $code, PDO::PARAM_STR);
+		$stmt_up->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
+		$stmt_up->execute();
+		$updated ++;
+
 	}
-
-	$stmt_up->bindValue(":code", $code, PDO::PARAM_STR);
-	$stmt_up->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
-	$stmt_up->execute();
-	$updated ++;
-
+} catch (Exception $e) {
+		var_dump($e);
+		echo '<p>';
+} finally {
+		echo '<p>empty lines ' . $count;
+		echo '<p>updated     ' . $update;
 }
 
-echo('success! ' . $updated);
-
+echo('<p>success! ');
 
 ?>
