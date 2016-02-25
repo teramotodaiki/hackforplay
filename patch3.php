cf96c602f745b52aedbd862603b5e009c32e0de2
teramotodaiki <i101323@gmail.com>
2016-02-25 13:18:04 +0900

    Revert "show progress"
    This reverts commit 150c2f88938160cd368ff46384e89b9665d92d28.

--- patch3.php
+++ patch3.php
@@ -8,10 +8,6 @@ $updated = 0;
 
 require_once 'preload.php';
 
-$stmt	= $dbh->prepare('SELECT COUNT(*) FROM "Script" WHERE "RawCode" IS NULL');
-$stmt->execute();
-$count = $stmt->fetch(PDO::FETCH_COLUMN);
-
 $stmt_se	= $dbh->prepare('SELECT "ID" FROM "Script" WHERE "RawCode" IS NULL');
 $stmt_se->execute();
 
@@ -19,33 +15,26 @@ $stmt_code	= $dbh->prepare('SELECT "Code"."Value" FROM "Code" INNER JOIN "Line" 
 
 $stmt_up	= $dbh->prepare('UPDATE "Script" SET "RawCode"=:code WHERE "ID"=:script_id');
 
-try {
-	while (($row = $stmt_se->fetch(PDO::FETCH_ASSOC)) && $limit--) {
+while (($row = $stmt_se->fetch(PDO::FETCH_ASSOC)) && $limit--) {
 
-		$stmt_code->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
-		$stmt_code->execute();
+	$stmt_code->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
+	$stmt_code->execute();
 
-		$lines	= $stmt_code->fetchAll(PDO::FETCH_COLUMN, 0);
-		if (empty($lines)) {
-			$code	= '';
-		} else {
-			$code	= implode("\n", $lines);
-		}
-
-		$stmt_up->bindValue(":code", $code, PDO::PARAM_STR);
-		$stmt_up->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
-		$stmt_up->execute();
-		$updated ++;
-
+	$lines	= $stmt_code->fetchAll(PDO::FETCH_COLUMN, 0);
+	if (empty($lines)) {
+		$code	= '';
+	} else {
+		$code	= implode("\n", $lines);
 	}
-} catch (Exception $e) {
-		var_dump($e);
-		echo '<p>';
-} finally {
-		echo '<p>empty lines ' . $count;
-		echo '<p>updated     ' . $update;
+
+	$stmt_up->bindValue(":code", $code, PDO::PARAM_STR);
+	$stmt_up->bindValue(":script_id", $row['ID'], PDO::PARAM_INT);
+	$stmt_up->execute();
+	$updated ++;
+
 }
 
-echo('<p>success! ');
+echo('success! ' . $updated);
+
 
 ?>
