6afee7cfaa7eec1ad84b7e7a99302603282a021d
teramotodaiki <i101323@gmail.com>
2016-03-13 20:56:37 +0900

    Debug

--- patch0313.php
+++ patch0313.php
@@ -3,32 +3,38 @@
 
 require 'preload.php';
 
-$stmt = $dbh->prepare('SELECT "ID","Thumbnail","Registered" FROM "Stage" WHERE "Mode"=:official AND "ScriptID" IS NULL');
-$stmt->bindValue(':official', 'official', PDO::PARAM_STR);
-$stmt->execute();
+try {
 
-$stmt_in = $dbh->prepare('INSERT INTO "Script" ("Thumbnail","Registered") VALUES (:thumbnail,:registered)');
-$stmt_up = $dbh->prepare('UPDATE "Stage" SET "ScriptID"=:script_id WHERE "ID"=:stage_id');
+  $stmt = $dbh->prepare('SELECT "ID","Thumbnail","Registered" FROM "Stage" WHERE "Mode"=:official AND "ScriptID" IS NULL');
+  $stmt->bindValue(':official', 'official', PDO::PARAM_STR);
+  $stmt->execute();
 
-$count = 0;
+  $stmt_in = $dbh->prepare('INSERT INTO "Script" ("Thumbnail","Registered") VALUES (:thumbnail,:registered)');
+  $stmt_up = $dbh->prepare('UPDATE "Stage" SET "ScriptID"=:script_id WHERE "ID"=:stage_id');
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $count = 0;
 
-  $stmt_in->bindValue(':thumbnail', $row['Thumbnail'], PDO::PARAM_STR);
-  $stmt_in->bindValue(':registered', $row['Registered'], PDO::PARAM_STR);
-  $flag = $stmt_in->execute();
-  if (!$flag) exit('Failed to insert');
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
-  $script_id = $dbh->lastInsertId('Script');
+    $stmt_in->bindValue(':thumbnail', $row['Thumbnail'], PDO::PARAM_STR);
+    $stmt_in->bindValue(':registered', $row['Registered'], PDO::PARAM_STR);
+    $flag = $stmt_in->execute();
+    if (!$flag) exit('Failed to insert');
 
-  $stmt_up->bindValue(':script_id', $script_id, PDO::PARAM_INT);
-  $stmt_up->bindValue(':stage_id', $row['ID'], PDO::PARAM_INT);
-  $flag = $stmt_up->execute();
-  if (!$flag) exit('Failed to update');
+    $script_id = $dbh->lastInsertId('Script');
 
-  $count++;
+    $stmt_up->bindValue(':script_id', $script_id, PDO::PARAM_INT);
+    $stmt_up->bindValue(':stage_id', $row['ID'], PDO::PARAM_INT);
+    $flag = $stmt_up->execute();
+    if (!$flag) exit('Failed to update');
+
+    $count++;
+  }
+
+  echo "count $count";
+
+} catch (Exception $e) {
+  print_r($e);
 }
-
-echo "count $count";
 
 ?>
