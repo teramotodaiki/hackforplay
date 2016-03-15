628851ccc8b305f57b2f33f317089ff8de46baf0
teramotodaiki <i101323@gmail.com>
2016-03-15 09:44:08 +0900

    Authorize

---
+++ devkit/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Dev kit tools
+ * CONFIDENCIAL
+*/
+try {
+
+	// ユーザー名・パスワード
+	$accept_page_username	= 'hackforplay';
+	$accept_page_password	= 'Wbjgqdp6vLq7tFv';
+
+	switch (true) {
+	    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+	    case $_SERVER['PHP_AUTH_USER'] !== $accept_page_username:
+	    case $_SERVER['PHP_AUTH_PW']   !== $accept_page_password:
+	        header('WWW-Authenticate: Basic realm="Enter username and password."');
+	        header('Content-Type: text/plain; charset=utf-8');
+	        die('Sorry!! but in this page, visitors are limited browsing');
+	}
+
+	require_once '../preload.php';
+
+	header('Content-Type: text/html; charset=utf-8');
+
+} catch (Exception $e) {
+
+	require_once '../exception/tracedata.php';
+	traceData($e);
+
+}
+?>
