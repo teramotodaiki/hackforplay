4a4d7f8d85025792afc1659ac2f116cdf2b5aa5f
teramotodaiki <i101323@gmail.com>
2016-03-20 17:52:22 +0900

    Temporary implement - Quest achievement

--- embed/lib/hack.js
+++ embed/lib/hack.js
@@ -340,6 +340,78 @@ window.addEventListener('load', function() {
 				}).tl.moveTo(157-game.rootScene.x, 240-game.rootScene.y, 20, enchant.Easing.CUBIC_EASEOUT);
 			});
 		};
+
+		// Quest時の仮実装 (削除予定)
+		Hack.__QuestGameclearNext = null;
+		Hack.__QuestGameclearReport = false;
+		Hack.__QuestGameclear = function () {
+			// Questの実績を報告
+			window.parent.postMessage('quest_clear_level', '*');
+
+			// 演出
+			var lay = Hack.overlay('rgba(0,0,0,0.4)', 'hackforplay/clear.png');
+			lay.opacity = 0;
+			lay.moveTo(-game.rootScene.x, -game.rootScene.y);
+			lay.tl.fadeIn(30, enchant.Easing.LINEAR).then(function() {
+				if (Hack.__QuestGameclearNext > 0) {
+					// [NEXT]
+					Hack.createSprite(165, 69, {
+						x: 65-game.rootScene.x, y: 320-game.rootScene.y,
+						image: game.assets['hackforplay/new_button_next.png'],
+						defaultParentNode: game.rootScene,
+						ontouchend: function() {
+							// [NEXT] がクリックされたとき
+							window.parent.postMessage('quest_move_next', '*');
+						}
+					}).tl.moveTo(65-game.rootScene.x, 240-game.rootScene.y, 20, enchant.Easing.CUBIC_EASEOUT);
+				} else {
+					// [TOWN]
+					// 仮グラフィック
+					Hack.createSprite(165, 69, {
+						x: 65-game.rootScene.x, y: 320-game.rootScene.y,
+						image: game.assets['hackforplay/new_button_town.png'],
+						defaultParentNode: game.rootScene,
+						ontouchend: function() {
+							// [NEXT] がクリックされたとき
+							window.parent.postMessage('quest_move_next', '*');
+						}
+					}).tl.moveTo(65-game.rootScene.x, 240-game.rootScene.y, 20, enchant.Easing.CUBIC_EASEOUT);
+					if (Hack.__QuestGameclearReport) {
+						// 演出
+						// [Empty]
+						Hack.createSprite(32, 32, {
+							x: 224-game.rootScene.x, y: -32-game.rootScene.y,
+							image: game.assets['hackforplay/achievement_n.png'],
+							defaultParentNode: game.rootScene,
+						}).tl.delay(26).moveBy(0, 92, 14, enchant.Easing.CUBIC_EASEOUT);
+						// [Effect]
+						Hack.createSprite(32, 32, {
+							x: 224-game.rootScene.x, y: 60-game.rootScene.y,
+							image: game.assets['hackforplay/achievement_p.png'],
+							defaultParentNode: game.rootScene,
+							scaleX: 0, scaleY: 0
+						}).tl.delay(56).scaleTo(12, 12, 40).and().fadeOut(40);
+						// [Entity]
+						Hack.createSprite(32, 32, {
+							x: 224-game.rootScene.x, y: 60-game.rootScene.y,
+							image: game.assets['hackforplay/achievement_p.png'],
+							defaultParentNode: game.rootScene,
+							scaleX: 0, scaleY: 0
+						}).tl.delay(56).scaleTo(1, 1, 8);
+					}
+				}
+				// [COMMENT]
+				Hack.createSprite(165, 69, {
+					x: 250-game.rootScene.x, y: 320-game.rootScene.y,
+					image: game.assets['hackforplay/new_button_comment.png'],
+					defaultParentNode: game.rootScene,
+					ontouchend: function() {
+						// [COMMENT] がクリックされたとき
+						window.parent.postMessage('show_comment', '*');
+					}
+				}).tl.moveTo(250-game.rootScene.x, 240-game.rootScene.y, 20, enchant.Easing.CUBIC_EASEOUT);
+			});
+		};
 	})();
 
 	// ゲームメニュー
