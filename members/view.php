bd41f4f37c2cbfaef68b9ecf8bcf3769d3eaa88c
teramotodaiki <i101323@gmail.com>
2016-03-26 18:17:38 +0900

    Flex box view

--- members/view.php
+++ members/view.php
@@ -4,6 +4,17 @@
     <meta charset="utf-8">
     <title>Members - <?php echo $community['DisplayName']; ?></title>
     <?php require_once '../library.php' ?>
+    <style media="screen">
+    .flex-container {
+      display: flex;
+      align-items: baseline;
+      padding: 0px 30px;
+    }
+    .flex-container>* {
+      flex: 1 1 auto;
+      max-width: 200px;
+    }
+    </style>
   </head>
   <body>
   	<?php require_once '../externalcodes.php'; ?>
@@ -12,15 +23,13 @@
     <div class="container">
       <h1><?php echo $community['DisplayName']; ?></h1>
       <?php foreach ($UCMap as $key => $user) : ?>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <h4><?php echo $user['Nickname']; ?></h4>
-            <a class="query-on-click" data-query="delete" href="javascript:void(0);"
-            data-userid="<?php echo $user['UserID']; ?>"
-            data-communityid="<?php echo $community['ID']; ?>">
-              DELETE
-            </a>
-          </div>
+        <div class="panel panel-default flex-container">
+          <h4><?php echo $user['Nickname']; ?></h4>
+          <a class="query-on-click" data-query="delete" href="javascript:void(0);"
+          data-userid="<?php echo $user['UserID']; ?>"
+          data-communityid="<?php echo $community['ID']; ?>">
+            DELETE
+          </a>
         </div>
       <?php endforeach; ?>
     </div>
