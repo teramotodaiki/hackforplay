a26312de483c8c1205bae1c69fe5a0d0f09d697b
teramotodaiki <i101323@gmail.com>
2016-03-29 13:07:19 +0900

    Foundation of a channel page

---
+++ cast/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * /cast/?name={Channel.Name}
+ * キャストを視聴するページ
+*/
+
+try {
+
+  require_once '../preload.php';
+
+  session_start();
+	$session_userid	= isset($_SESSION['UserID']) ? $_SESSION['UserID'] : NULL;
+	session_commit();
+
+  $name = filter_input(INPUT_GET, 'name');
+  if (!$name) {
+    header("HTTP/1.0 404 Not Found");
+    echo 'Invalid input of name';
+    exit();
+  }
+
+  // Channelを取得
+  $stmt = $dbh->prepare('SELECT "ID","CommunityID","Name","DisplayName","Registered" FROM "Channel" WHERE "Name"=:name');
+  $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+  $stmt->execute();
+  $channel = $stmt->fetch(PDO::FETCH_ASSOC);
+  if (!$channel) {
+    header("HTTP/1.0 404 Not Found");
+    echo 'Channel not found';
+    exit();
+  }
+
+  include 'view.php';
+
+} catch (Exception $e) {
+  Rollbar::report_exception($e);
+  die;
+}
+?>
---
+++ cast/view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $channel['DisplayName']; ?></title>
+  </head>
+  <body>
+    <?php echo $channel['DisplayName']; ?>
+  </body>
+</html>
