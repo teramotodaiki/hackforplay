ba06d0d919bc569243d71d1ae9e7fbb4796eb6af
teramotodaiki <i101323@gmail.com>
2016-04-06 15:00:52 +0900

    Revert "Merge pull request #443 from teramotodaiki/resources-src-api"
    This reverts commit 74e56aa694dbe02f2d459aa85d6bbfae05b71157, reversing
    changes made to 857a5147b89eb71c1bceb3ee1b04065d9da43da6.

--- resources/index.php
+++ resources/index.php
@@ -1,22 +1,19 @@
 <?php
-/**
- * /resources/?author={AuthorName}&sub={SubName}
- * get JSON data of resource
-*/
 
-require_once '../preload.php';
+try {
 
-$author = filter_input(INPUT_GET, 'author') or die('invalid-author-name');
-$sub = filter_input(INPUT_GET, 'sub') or die('invalid-sub-name');
+	require_once '../preload.php';
 
-$stmt = $dbh->prepare('SELECT "SpriteWidth","SpriteHeight" FROM "Resource" WHERE "AuthorName"=:author AND "SubName"=:sub');
-$stmt->bindValue(':author', $author, PDO::PARAM_STR);
-$stmt->bindValue(':sub', $sub, PDO::PARAM_STR);
-$stmt->execute();
+	session_start();
+	$session_userid	= isset($_SESSION['UserID']) ? $_SESSION['UserID'] : NULL;
+	session_commit();
 
-$resource = $stmt->fetch(PDO::FETCH_ASSOC) or die('resource-not-found');
-$resource['SourceURL'] = "/src/?author=$author&sub=$sub";
+	include 'view.php';
 
-echo json_encode($resource);
+} catch (Exception $e) {
+	header('Location: ../e');
+	throw $e;
+	die;
+}
 
 ?>
