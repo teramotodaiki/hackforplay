8a5ba9d1d21f41c51b31f0169b35a60f36882c26
teramotodaiki <i101323@gmail.com>
2016-04-08 23:44:13 +0900

    Publishing task

--- waitinglist/view.js
+++ waitinglist/view.js
@@ -1,6 +1,28 @@
 $(function () {
   $('.flex-container-bar').on('click', function () {
     var opened = $(this).toggleClass('opened').hasClass('opened');
-    
-  })
+
+  });
+
+  $('.query-publish').on('click', function () {
+    var id = $(this).data('id');
+    var self = $(this);
+    $.post('../stages/', {
+      id: id,
+      data: JSON.stringify({
+        State: 'published',
+        Published: null
+      })
+    }, function (data) {
+      switch (data) {
+        case 'success':
+          self.off('click').parents('.flex-container-bar').fadeOut('slow');
+          break;
+        default:
+          console.error(data);
+      }
+    });
+    return false;
+  });
+
 });
