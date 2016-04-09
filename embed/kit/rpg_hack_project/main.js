7b8a82b1002c40b14d5d5b068e0e42d210c18a20
teramotodaiki <i101323@gmail.com>
2016-04-09 22:17:17 +0900

    code fix

--- embed/kit/rpg_hack_project/main.js
+++ embed/kit/rpg_hack_project/main.js
@@ -306,15 +306,15 @@ window.addEventListener('load', function () {
 	 * time フレームが経過した時、behavior typeを指定する
 	*/
 	enchant.Timeline.prototype.become = function (type, time) {
-        this.add(new enchant.Action({
-            onactionstart: function() {
+	  this.add(new enchant.Action({
+      onactionstart: function() {
 				var capital = type[0].toUpperCase() + type.substr(1).toLowerCase();
 				if (this instanceof RPGObject && BehaviorTypes.hasOwnProperty(capital)) {
 					this.behavior = BehaviorTypes[capital];
 				}
-            },
-            time: time || 0
-        }));
+      },
+	    time: time || 0
+    }));
 		return this;
 	};
 
