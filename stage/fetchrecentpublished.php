647756ec109c133e7472c960f825696d3de5c222
teramotodaiki <i101323@gmail.com>
2016-04-24 14:21:01 +0900

    Refactoring

--- stage/fetchrecentpublished.php
+++ stage/fetchrecentpublished.php
@@ -34,24 +34,36 @@ if (!$fetch_start) {
 	$fetch_start	= 0;
 }
 
-// ステージ一覧を取得 (フィルターなし)
+// ステージ一覧を取得
 // SQL Serverでは LIMIT 句が使えないので、一旦全データを取得している いずれ直すべき
-$stmt	= $dbh->prepare('SELECT s."ID",s."UserID",s."Title",s."Thumbnail",s."SourceID",s."Playcount",s."Published","User"."Nickname","Stage"."Title" AS SourceTitle,"Stage"."Mode" FROM ("Stage" AS s LEFT OUTER JOIN "User" ON s."UserID"="User"."ID") LEFT OUTER JOIN "Stage" ON s."SourceID"="Stage"."ID" WHERE s."Mode"=:replay AND s."State"=:published ORDER BY "Published" DESC');
+$stmt = $dbh->prepare('SELECT "ID" FROM "Stage" WHERE "Mode"=:replay AND "State"=:published ORDER BY "Published" DESC');
 $stmt->bindValue(":replay", 'replay', PDO::PARAM_STR);
 $stmt->bindValue(":published", 'published', PDO::PARAM_STR);
 $stmt->execute();
+$result	= array_slice($stmt->fetchAll(PDO::FETCH_ASSOC), $fetch_start, $max_fetch_length);
 
-$result = array();
-for ($i=0; $i < $fetch_start; $i++) {
-	$item	= $stmt->fetch();
+// ステージの詳細を取得
+$stmt = $dbh->prepare('SELECT * FROM "Stage" WHERE "ID"=:id');
+foreach ($result as $key => $value) {
+	$stmt->bindValue(':id', $value['ID'], PDO::PARAM_INT);
+	$stmt->execute();
+	$result[$key]['Stage'] = $stmt->fetch(PDO::FETCH_ASSOC);
 }
-for ($i = 0; $i < $max_fetch_length; $i++){
-	$item	= $stmt->fetch(PDO::FETCH_ASSOC);
-	if($item != NULL){
-		array_push($result, $item);
-	}else{
-		break;
-	}
+
+// ユーザーの詳細を取得
+$stmt = $dbh->prepare('SELECT "Nickname" FROM "User" WHERE "ID"=:userid');
+foreach ($result as $key => $value) {
+	$stmt->bindValue(':userid', $value['Stage']['UserID'], PDO::PARAM_INT);
+	$stmt->execute();
+	$result[$key]['User'] = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+// ソースステージの詳細を取得
+$stmt = $dbh->prepare('SELECT "Title","Mode" FROM "Stage" WHERE "ID"=:sourceid');
+foreach ($result as $key => $value) {
+	$stmt->bindValue(':sourceid', $value['Stage']['SourceID'], PDO::PARAM_INT);
+	$stmt->execute();
+	$result[$key]['Source']	= $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 // PlayLogからclearrateを算出する [高負荷]
@@ -69,16 +81,16 @@ $values = array();
 foreach ($result as $key => $value) {
 	$item 	= new stdClass();
 	$item->id 			= $value['ID'];
-	$item->author_id 	= $value['UserID'];
-	$item->author_name 	= $value['Nickname'];
-	$item->title 		= $value['Title'];
-	$item->thumbnail 	= $value['Thumbnail'];
-	$item->source_id 	= $value['SourceID'];
-	$item->source_title	= $value['SourceTitle'];
-	$item->source_mode	= $value['Mode'];
-	$item->playcount 	= $value['Playcount'];
+	$item->author_id 	= $value['Stage']['UserID'];
+	$item->author_name 	= $value['User']['Nickname'];
+	$item->title 		= $value['Stage']['Title'];
+	$item->thumbnail 	= $value['Stage']['Thumbnail'];
+	$item->source_id 	= $value['Stage']['SourceID'];
+	$item->source_title	= $value['Source']['Title'];
+	$item->source_mode	= $value['Source']['Mode'];
+	$item->playcount 	= $value['Stage']['Playcount'];
 	$item->LogCount = $value['LogCount'];
-	$item->published 	= $value['Published'];
+	$item->published 	= $value['Stage']['Published'];
 	array_push($values, $item);
 }
 
