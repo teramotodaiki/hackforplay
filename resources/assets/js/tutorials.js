cbfbd3babffe743da896b649f9da22b2e500770f
teramotodaiki <i101323@gmail.com>
2016-05-04 12:47:41 +0900

    background color

--- resources/assets/js/tutorials.js
+++ resources/assets/js/tutorials.js
@@ -4,6 +4,9 @@ const statics = {
   title: 'The Begining',
   description: 'Here is a first adventure of HackforPlay! Are you ready?',
   hintTitle: 'How to solve',
+  style: {
+    backgroundColor: 'rgb(190,233,213)'
+  }
 };
 
 // App
@@ -11,9 +14,18 @@ const Tutorials = React.createClass({
   getInitialState() {
     return {
       levels: [
-        { id: 1, title: 'Stage 1: Begining', youtube: 'od61KliPeJI', value: 'Next Level' },
-        { id: 2, title: 'Stage 2: Secondly', youtube: 'od61KliPeJI', value: 'Next Level' },
-        { id: 3, title: 'Stage 3: Thirdly', youtube: 'od61KliPeJI', value: 'Last Step' },
+        { id: 1, title: 'Stage 1: Begining', youtube: 'od61KliPeJI', value: 'Next Level',
+          style: { backgroundColor: 'rgb(190,233,213)' } },
+        { id: 2, title: 'Stage 2: Secondly', youtube: 'od61KliPeJI', value: 'Next Level',
+          style: { backgroundColor: 'rgb(255,251,223)' } },
+        { id: 3, title: 'Stage 3: Thirdly', youtube: 'od61KliPeJI', value: 'Next Level',
+          style: { backgroundColor: 'rgb(253,180,151)' } },
+        { id: 4, title: 'Stage 4: Forthly', youtube: 'od61KliPeJI', value: 'Next Level',
+          style: { backgroundColor: 'rgb(255,138,121)' } },
+        { id: 5, title: 'Stage 5: Fifthly', youtube: 'od61KliPeJI', value: 'Next Level',
+          style: { backgroundColor: 'rgb(144,71,88)' } },
+        { id: 6, title: 'Stage 6: Sixly', youtube: 'od61KliPeJI', value: 'Last Step',
+          style: { backgroundColor: 'rgb(67,26,36)' } },
       ],
       choises: [
         { message: 'If you want to play more stages, click here', value: 'PLAYGROUND' },
@@ -26,7 +38,7 @@ const Tutorials = React.createClass({
       return <Level info={item} key={item.id} />;
     });
     return (
-      <div>
+      <div style={statics.style}>
         <Header />
         {levels}
         <Dialog choises={this.state.choises} />
@@ -55,7 +67,7 @@ const Level = React.createClass({
   render() {
     const info = this.props.info;
     return (
-      <div className="container-fluid">
+      <div className="container-fluid" style={info.style}>
         <div className="row">
           <EmbedStage id={info.id} title={info.title} />
           <EmbedYoutube youtube={info.youtube} />
