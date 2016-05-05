1550b46eb634fc3b2c4dcd82aa7947a5668ff01b
teramotodaiki <i101323@gmail.com>
2016-05-05 15:23:41 +0900

    Bring out Section and Scroller

--- resources/assets/js/tutorials.js
+++ resources/assets/js/tutorials.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { Link as ScrollLink, Element as ScrollTarget } from "react-scroll";
 
 import Merger from "./merger";
+import { Section, Scroller } from "./section";
 
 const statics = {
   title: 'The Begining',
   description: 'Here is a first adventure of HackforPlay! Are you ready?',
+  buttonText: 'Start',
   hintTitle: 'How to solve',
   style: {
     backgroundColor: 'rgb(190,233,213)'
   },
   colors: {
     main: 'gray-dark',
-    sub: 'gray-lightest'
+    sub: 'gray-lightest',
+    levels: [
+      null,
+      'rgb(190,233,213)', // Level-1
+      'rgb(255,251,223)', // Level-2
+      'rgb(253,180,151)', // Level-3
+      'rgb(255,138,121)', // Level-4
+      'rgb(144, 71, 88)', // Level-5
+      'rgb( 67, 26, 36)'  // Level-6
+    ]
   }
 
 };
@@ -23,22 +34,22 @@ const Tutorials = React.createClass({
     return {
       levels: [
         { id: 1, title: 'Stage 1: Begining', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(190,233,213)' }, colorName: statics.colors.main,
+          colorName: statics.colors.main,
           link: { value: 'Next Level', to: 'Level-2' } },
         { id: 2, title: 'Stage 2: Secondly', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(255,251,223)' }, colorName: statics.colors.main,
+          colorName: statics.colors.main,
           link: { value: 'Next Level', to: 'Level-3' } },
         { id: 3, title: 'Stage 3: Thirdly', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(253,180,151)' }, colorName: statics.colors.main,
+          colorName: statics.colors.main,
           link: { value: 'Next Level', to: 'Level-4' } },
         { id: 4, title: 'Stage 4: Forthly', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(255,138,121)' }, colorName: statics.colors.sub,
+          colorName: statics.colors.sub,
           link: { value: 'Next Level', to: 'Level-5' } },
         { id: 5, title: 'Stage 5: Fifthly', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(144,71,88)' }, colorName: statics.colors.sub,
+          colorName: statics.colors.sub,
           link: { value: 'Next Level', to: 'Level-6' } },
         { id: 6, title: 'Stage 6: Sixly', youtube: 'od61KliPeJI',
-          style: { minHeight: '100vh', backgroundColor: 'rgb(67,26,36)' }, colorName: statics.colors.sub,
+          colorName: statics.colors.sub,
           link: { value: 'Last Step', to: 'Dialog' } }
       ],
       choises: [
@@ -66,10 +77,19 @@ const Header = React.createClass({
   mixins: [Merger],
   render() {
     return (
-      <div className={this.p({ text: 'xs-center ' + statics.colors.main})}>
-        <h1>{statics.title}</h1>
-        <h2>{statics.description}</h2>
-      </div>
+      <Section name="Header" height="100vh">
+        <div className={this.p({ text: 'xs-center ' + statics.colors.main})}>
+          <h1>{statics.title}</h1>
+          <h2>{statics.description}</h2>
+        </div>
+        <div className="text-xs-center">
+          <Scroller to="Level-1">
+            <span className={this.p({ btn: statics.colors.main + '-outline lg' })}>
+              {statics.buttonText}
+            </span>
+          </Scroller>
+        </div>
+      </Section>
     );
   }
 });
@@ -79,20 +99,22 @@ const Level = React.createClass({
   render() {
     const info = this.props.info;
     return (
-      <ScrollTarget name={'Level-' + info.id}
-        className={'container-fluid text-' + info.colorName}>
-        <div className={this.p({ row: 'vertical-justify' })} style={info.style}>
-          <div className="container-fluid">
-            <div className={this.p({ row: 'horizontal-justify xs-bottom' })}>
-              <EmbedStage info={info} />
-              <EmbedYoutube info={info} />
-            </div>
-          </div>
-          <div className="text-xs-center">
-            <NextLevel info={info} />
+      <Section name={'Level-' + info.id} height="100vh"
+        style={{ backgroundColor: statics.colors.levels[info.id] }}>
+        <div className="container-fluid">
+          <div className={this.p({ row: 'horizontal-justify xs-bottom' })}>
+            <EmbedStage info={info} />
+            <EmbedYoutube info={info} />
           </div>
         </div>
-      </ScrollTarget>
+        <div className="text-xs-center">
+          <Scroller to={info.link.to}>
+            <span className={this.p({ btn: info.colorName + '-outline lg' })}>
+              {info.link.value}
+            </span>
+          </Scroller>
+        </div>
+      </Section>
     );
   }
 });
@@ -104,11 +126,9 @@ const Dialog = React.createClass({
       return <Choise info={item} key={item.value}></Choise>;
     });
     return (
-      <ScrollTarget name="Dialog" className="container">
-        <div className={this.p({ row: 'vertical-justify' })}>
-          {choises}
-        </div>
-      </ScrollTarget>
+      <Section name="Dialog" height="100vh">
+        {choises}
+      </Section>
     );
   }
 });
@@ -120,7 +140,7 @@ const EmbedStage = React.createClass({
     const info = this.props.info;
     return (
       <div className="col-xs-7">
-        <h2>{info.title}</h2>
+        <h2 className={'text-' + info.colorName}>{info.title}</h2>
         <div className={this.p({ 'embed-responsive': '4by3' })} style={{backgroundColor: 'black'}}>
           <iframe ></iframe>
         </div>
@@ -135,25 +155,11 @@ const EmbedYoutube = React.createClass({
     const info = this.props.info;
     return (
       <div className="col-xs-5">
-        <h3>{statics.hintTitle}</h3>
+        <h3 className={'text-' + info.colorName}>{statics.hintTitle}</h3>
         <div className={this.p({ 'embed-responsive': '16by9' })} style={{backgroundColor: 'black'}}>
           <iframe ></iframe>
         </div>
       </div>
-    );
-  }
-});
-
-const NextLevel = React.createClass({
-  mixins: [Merger],
-  render() {
-    const info = this.props.info;
-    const link = info.link;
-    return (
-      <ScrollLink activeClass="active" to={link.to} spy={true} smooth={true} offset={1} duration={500}
-        className={this.p({ btn: info.colorName + '-outline lg' })}>
-        {link.value}
-      </ScrollLink>
     );
   }
 });
