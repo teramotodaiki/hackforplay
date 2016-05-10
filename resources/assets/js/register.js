934129b5222aff7a972ed2d94d5f6a746b827506
teramotodaiki <i101323@gmail.com>
2016-05-10 16:12:28 +0900

    Sections

--- resources/assets/js/register.js
+++ resources/assets/js/register.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { scroller } from "react-scroll";
+import Confirm from "./confirm";
 
-const Register = React.createClass({
-  render () {
+import Merger from "./merger";
+import { Section, Scroller } from "./section";
+
+
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
-      <div></div>
+      <div>
+        <Landing />
+        <Gender />
+        <Nickname />
+        <ID />
+        <Password />
+        <Result />
+      </div>
     );
   }
-})
+}
 
-export default Register
+const Landing = (props) => {
+  return (
+    <Section name="Landing">
+
+    </Section>
+  );
+};
+
+const Gender = (props) => {
+  return (
+    <Section name="Gender">
+
+    </Section>
+  );
+};
+
+const Nickname = (props) => {
+  return (
+    <Section name="Nickname">
+
+    </Section>
+  );
+};
+
+const ID = (props) => {
+  return (
+    <Section name="ID">
+
+    </Section>
+  );
+};
+
+const Password = (props) => {
+  return (
+    <Section name="Password">
+
+    </Section>
+  );
+};
+
+const Result = (props) => {
+  return (
+    <Section name="Result">
+
+    </Section>
+  );
+};
