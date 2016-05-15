6a6a8dfa8a44b3a1d851dbd37fe281e9bd3d720a
teramotodaiki <i101323@gmail.com>
2016-05-15 10:28:22 +0900

    Account model

---
+++ app/Account.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+  /**
+   * http://readouble.com/laravel/5/1/ja/eloquent.html
+   */
+  protected $table = 'Account';
+  public $timestamps = false; // モデルのタイムスタンプを更新しない
+  protected $guarded = array('id'); // idはcreateに含まない
+
+}
