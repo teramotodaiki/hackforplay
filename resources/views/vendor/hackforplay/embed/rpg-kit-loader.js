6a0196b490f5165c69b475497352e21951335a23
teramotodaiki <i101323@gmail.com>
2016-05-21 21:38:45 +0900

    RPG kit loader

---
+++ resources/views/vendor/hackforplay/embed/rpg-kit-loader.js
@@ -0,0 +1,8 @@
+define(function (require, exports, module) {
+
+  require('embed/modules/hack');
+  require('embed/modules/enchant');
+  require('embed/modules/ui.enchant');
+  require('embed/kit/rpg_hack_project/main');
+
+});
