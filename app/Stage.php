133e953d4a26b02e5ea08a862ba82248a2996c49
teramotodaiki <i101323@gmail.com>
2016-05-23 20:03:14 +0900

    Make stage model

---
+++ app/Stage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stage extends Model
+{
+  /**
+   * http://readouble.com/laravel/5/1/ja/eloquent.html
+   */
+  protected $table = 'Stage';
+  public $timestamps = false; // モデルのタイムスタンプを更新しない
+  protected $guarded = array('id'); // idはcreateに含まない
+  protected $primaryKey = 'ID';
+
+}
