537db7cba69622751ae25f851b5c00e830dd54ed
teramotodaiki <i101323@gmail.com>
2016-06-08 13:12:17 +0900

    import redux

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router, Route, Link, browserHistory } from 'react-router';
+import { Providor, createStore } from 'react-redux';
 
 import Tutorials from "./tutorials";
 import Register from "./register";
 import Channel from "./channel";
 
+const store = createStore();
 
 const App = React.createClass({
 
@@ -21,14 +23,16 @@ const App = React.createClass({
 
 render((
 
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="tutorials" component={Tutorials}></Route>
-      <Route path="register" component={Register}></Route>
-      <Route path="channels">
-        <Route path=":id/watch" component={Channel}></Route>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="tutorials" component={Tutorials}></Route>
+        <Route path="register" component={Register}></Route>
+        <Route path="channels">
+          <Route path=":id/watch" component={Channel}></Route>
+        </Route>
       </Route>
-    </Route>
-  </Router>
+    </Router>
+  </Provider>
 
 ), document.getElementById('app'));
