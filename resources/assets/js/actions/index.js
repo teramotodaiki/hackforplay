aa05b7720fe8ec69b2eee4c1688a5e6792394722
teramotodaiki <i101323@gmail.com>
2016-06-08 19:08:23 +0900

    fetch channel action

--- resources/assets/js/actions/index.js
+++ resources/assets/js/actions/index.js
@@ -1,3 +1,4 @@
+import request from 'superagent';
 
 
 export const ADD_CHANNEL = 'ADD_CHANNEL';
@@ -5,3 +6,18 @@ export const ADD_CHANNEL = 'ADD_CHANNEL';
 export const addChannel = (channel) => {
   return { type: ADD_CHANNEL, channel };
 }
+
+
+export const FETCH_CHANNEL = 'FETCH_CHANNEL';
+
+export const fetchChannel = (id) => {
+  return (dispatch) => {
+
+    dispatch({ type: FETCH_CHANNEL });
+
+    return request('/channels/' + id)
+      .then((result) => dispatch({ type: FETCH_CHANNEL, channel: result.body }))
+      .catch((err) => alert(err));
+
+  }
+};
