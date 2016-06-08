bc84564e6b7a965cdb6d2c4cd1ac335d6f162c8c
teramotodaiki <i101323@gmail.com>
2016-06-08 19:08:06 +0900

    thunk middleware

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router, Route, Link, browserHistory } from 'react-router';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import * as reducers from './reducers/';
 import Tutorials from "./tutorials";
 import Register from "./register";
 import Channel from "./channel";
 
-const store = createStore(combineReducers(reducers));
+const store = createStore(
+  combineReducers(reducers),
+  applyMiddleware(thunkMiddleware)
+);
+
 
 const App = React.createClass({
 
