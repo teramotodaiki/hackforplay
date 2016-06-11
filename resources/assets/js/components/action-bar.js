2c3b8891d96843a6ba162f16b9625441289df01b
teramotodaiki <i101323@gmail.com>
2016-06-11 15:35:36 +0900

    action bar style fix

--- resources/assets/js/components/action-bar.js
+++ resources/assets/js/components/action-bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 export default class ActionBar extends Component {
   constructor(props) {
@@ -7,15 +8,39 @@ export default class ActionBar extends Component {
     this.style = {
       position: 'absolute',
       bottom: 0,
+      width: '100%',
+      margin: 0,
     };
   }
 
   render() {
     const { style } = this.props;
+    const colStyle = {
+      margin: 0,
+      padding: 0,
+      height: style.height,
+    };
+    const inputStyle = {
+      border: 'none',
+      outline: 'none',
+      resize: 'none',
+      width: '100%',
+      height: style.height - 4,
+      overflowY: 'scroll',
+      paddingTop: 4,
+    }
 
-    return (<div style={Object.assign({}, this.style, style)}>
-      <input type="text"></input>
-    </div>);
+    return (<Row style={Object.assign({}, this.style, style)}>
+      <Col xs={1} style={colStyle}>
+        <span>😄</span>
+      </Col>
+      <Col xs={10} style={colStyle}>
+        <textarea style={inputStyle}></textarea>
+      </Col>
+      <Col xs={1} style={colStyle}>
+        <span className="fa fa-paper-plane-o"></span>
+      </Col>
+    </Row>);
   }
 }
 
