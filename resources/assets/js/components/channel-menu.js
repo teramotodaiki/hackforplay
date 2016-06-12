08354757c729ee15905537f237418ea8894d8658
teramotodaiki <i101323@gmail.com>
2016-06-12 16:04:57 +0900

    ui

--- resources/assets/js/components/channel-menu.js
+++ resources/assets/js/components/channel-menu.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
+import { Button } from 'react-bootstrap';
+
 export default class ChannelMenu extends Component {
   constructor(props) {
     super(props);
+
+    this.style = {
+      textAlign: 'center',
+    };
   }
 
   render() {
-    return (<div>
+
+    const { style } = this.props;
+
+    return (<div style={Object.assign({}, this.style, style)}>
+      <Button bsStyle="link" bsSize="large">
+        <span className="fa fa-refresh fa-3x"></span>
+      </Button>
     </div>);
   }
 }
