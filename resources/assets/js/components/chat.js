bd9f55cbf31be4ef807a4fd3b0425eb984d8a16f
teramotodaiki <i101323@gmail.com>
2016-06-13 18:49:32 +0900

    fix same key bug

--- resources/assets/js/components/chat.js
+++ resources/assets/js/components/chat.js
@@ -15,8 +15,8 @@ export default class Chat extends Component {
 
     const { style, message } = this.props;
     const body = message
-      .split(URL_REGEX).map((item) =>
-        URL_REGEX.test(item) ? <a key={item} href={item} target="_blank">{item}</a> : item);
+      .split(URL_REGEX).map((item, i) =>
+        URL_REGEX.test(item) ? <a key={i} href={item} target="_blank">{item}</a> : item);
 
     return (<div style={Object.assign({}, this.style, style)}>
       {body}
