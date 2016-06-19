110c6259c1b92ec9086d7e430921acfb9f60007c
teramotodaiki <i101323@gmail.com>
2016-06-19 18:27:53 +0900

    Session store table model

---
+++ app/Session.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+  /**
+   * http://readouble.com/laravel/5/1/ja/eloquent.html
+   */
+  protected $table = 'Sessions';
+  public $timestamps = false; // モデルのタイムスタンプを更新しない
+  protected $guarded = array('id'); // idはcreateに含まない
+}
