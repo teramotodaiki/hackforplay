eeadbc1398cc25eaae6cf37ea1b0c2bfebc4eb56
teramotodaiki <i101323@gmail.com>
2016-06-19 19:07:03 +0900

    Auth result

--- app/Http/Controllers/DefaultAppController.php
+++ app/Http/Controllers/DefaultAppController.php
@@ -1,12 +1,15 @@
 <?php
 namespace App\Http\Controllers;
 
+use Auth;
+
 class DefaultAppController extends Controller
 {
 
   function __construct()
   {
-
+    // 旧仕様のセッションログインから、UserIDだけ拝借してUser情報を構築する
+    $this->middleware(['auth.old']);
   }
 
   public function index()
@@ -14,7 +17,7 @@ class DefaultAppController extends Controller
     // サーバーサイドでレンダリングしたい情報のみ、ここで処理する
     //  LaravelのControllerにはReactのパラメータを載せない
     //  必要なコンポーネントはView（blade）でHTMLに書き出す
-    return view('defaultApp');
+    return view('defaultApp', ['user' => Auth::user()]);
   }
 }
 
