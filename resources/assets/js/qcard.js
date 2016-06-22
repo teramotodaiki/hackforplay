d11e41fc9eb83e6d35925d43cf91e2cc51cd5806
teramotodaiki <i101323@gmail.com>
2016-06-22 14:33:06 +0900

    tmp submit action

--- resources/assets/js/qcard.js
+++ resources/assets/js/qcard.js
@@ -23,6 +23,14 @@ class Qcard extends Component {
     dispatch(updateQcard(qcard));
   }
 
+  createBellWithQcard() {
+    const { params: { id }, qcards, dispatch } = this.props;
+
+    const qcard = Object.assign({}, qcards[id], { is_active: false });
+
+    dispatch(updateQcard(qcard));
+  }
+
   render() {
     const id = +this.props.params.id;
     const qcard = this.props.qcards[id];
@@ -52,7 +60,11 @@ class Qcard extends Component {
         type="checkbox"
         checked={article.checkedRight}
         onChange={() => this.updateArticle({ checkedRight: !article.checkedRight })} />
-      <button type="button">Submit</button>
+      <button
+        type="button"
+        onClick={() => this.createBellWithQcard()}>
+        Submit
+      </button>
     </div>);
   }
 }
