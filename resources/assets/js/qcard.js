aec9673245fb85183791387a1561a1439badb854
teramotodaiki <i101323@gmail.com>
2016-06-22 13:00:49 +0900

    render q card fetched

--- resources/assets/js/qcard.js
+++ resources/assets/js/qcard.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { fetchQcard } from './actions/';
+
 class Qcard extends Component {
   constructor(props) {
     super(props);
+
+    const { dispatch, params } = this.props;
+    dispatch(fetchQcard({ id: params.id }));
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    const id = +this.props.params.id;
+    const qcard = this.props.qcards[id];
+    const article = Object.assign({
+      left: '',
+      right: '',
+    }, qcard ? qcard.article : null);
+
+    return (<div>
+      <textarea
+        value={article.left}>
+      </textarea>
+      <textarea
+        value={article.right}}>
+      </textarea>
+    </div>);
   }
 }
 
