98d707cbb458fabed7e325f1c4a578bbe15955f8
teramotodaiki <i101323@gmail.com>
2016-06-26 09:52:37 +0900

    Update Model

--- app/Channel.php
+++ app/Channel.php
@@ -10,8 +10,7 @@ class Channel extends Model
    * http://readouble.com/laravel/5/1/ja/eloquent.html
    */
   protected $table = 'Channel';
-  public $timestamps = false; // モデルのタイムスタンプを更新しない
-  protected $guarded = array('id'); // idはcreateに含まない
+  protected $guarded = ['id', 'created_at', 'updated_at']; // idはcreateに含まない
   protected $primaryKey = 'ID';
 
   public function project()
