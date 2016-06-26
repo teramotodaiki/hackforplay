8cd218a0f13174cdcde3c8ab0a8a534ee817d350
teramotodaiki <i101323@gmail.com>
2016-06-26 14:40:06 +0900

    Test dump

--- resources/assets/js/channel-list.js
+++ resources/assets/js/channel-list.js
@@ -1,14 +1,48 @@
 import React, {PropTypes} from 'react';
+import { connect } from 'react-redux';
 
-export default class ChannelList extends React.Component {
+import { fetchChannels } from './actions/';
+
+class ChannelList extends React.Component {
   constructor(props) {
     super(props);
+
+    const { dispatch } = this.props;
+
+    dispatch(fetchChannels());
+
   }
 
   render() {
-    return (<div>MyComponent</div>);
+
+    const { channels } = this.props;
+
+    const sorted = Object.keys(channels)
+    .map((key) => channels[key])
+    .sort((a, b) => {
+      return a.updated_at > b.updated_at ? 1 : -1;
+    })
+    .map((channel) => {
+      return (
+        <div key={channel.ID}>
+          <img src={channel.Thumbnail}></img>
+          <p>{channel.user.Nickname}</p>
+          <p>{channel.description}</p>
+        </div>
+      );
+    });
+
+    return (<div>
+      {sorted}
+      </div>);
   }
 }
 
 ChannelList.propTypes = {
 };
+
+const mapStateToProps = (state) => {
+  return Object.assign({}, state);
+};
+
+export default connect(mapStateToProps)(ChannelList);
