0e1b592ec68f6e68ea822e6010e6ac3961e3a3db
teramotodaiki <i101323@gmail.com>
2016-06-26 16:28:29 +0900

    thumbnails and links

--- resources/assets/js/components/channel-card.js
+++ resources/assets/js/components/channel-card.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-import { Col } from 'react-bootstrap';
+import { Col, Thumbnail, Button } from 'react-bootstrap';
 
-export default ({ ID, Thumbnail, user, description }) => {
+export default (props) => {
+
+  const { ID, user, description } = props;
+  const thumbnail = props.Thumbnail;
 
   return (
-    <Col xs={6} sm={4} md={3} lg={2} >
-      <img src={Thumbnail}></img>
-      <p>{user.Nickname}</p>
-      <p>{description}</p>
+    <Col xs={6} sm={4} md={3} lg={2}>
+      <Thumbnail src={thumbnail} >
+        <p>Owner: <a href={`/m?id=${user.ID}`}>{user.Nickname}</a></p>
+        <p>{description}</p>
+        <Button bsStyle="primary" href={`/channels/${ID}/watch`}>watch</Button>
+      </Thumbnail>
     </Col>
   );
 }
