834f50a68c3c99923bedc58d9171ccc2aae88653
teramotodaiki <i101323@gmail.com>
2016-06-27 10:40:42 +0900

    Stateless component

--- resources/assets/js/components/section.js
+++ resources/assets/js/components/section.js
@@ -1,46 +1,42 @@
 import React from "react";
 import { Element, Link } from "react-scroll";
-import classNames from "classNames";
 
-const Section = React.createClass({
-  render () {
+export const Section = (props) => {
 
-    const style = Object.assign({
-      height: '100vh',
-      textAlign: 'center',
-    }, this.props.style);
+  const style = Object.assign({
+    minHeight:  document.documentElement.clientHeight,
+    textAlign: 'center',
+  }, props.style);
 
-    const rowStyle = Object.assign({
-      display: 'flex',
-      flexFlow: 'column nowrap',
-      justifyContent: 'space-around',
-      height: '100%',
-      alignItems: 'center',
-    }, this.props.rowStyle);
+  const rowStyle = Object.assign({
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'space-around',
+    minHeight:  document.documentElement.clientHeight,
+    alignItems: 'center',
+  }, props.rowStyle);
 
-    return (
-      <Element
-        name={this.props.name}
-        style={style}
-        className={this.props.className}
-        >
-        <div
-          style={rowStyle}>
-          {this.props.children}
-        </div>
-      </Element>
-    )
-  }
-});
+  return (
+    <Element
+      name={props.name}
+      style={style}
+      className={props.className}
+      >
+      <div
+        style={rowStyle}>
+        {props.children}
+      </div>
+    </Element>
+  );
 
-const Scroller = React.createClass({
-  render() {
-    return (
-      <Link activeClass="active" to={this.props.to} spy={true} smooth={true} offset={1} duration={this.props.duration || 500}>
-        {this.props.children}
-      </Link>
-    );
-  }
-});
+};
 
-export { Section, Scroller };
+export const Scroller = (props) => {
+
+  return (
+    <Link activeClass="active" to={props.to} spy={true} smooth={true} offset={1} duration={props.duration || 500}>
+      {props.children}
+    </Link>
+  );
+
+};
