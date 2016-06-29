94e13eec5f5484820cfb958e39d2417aa503a57b
teramotodaiki <i101323@gmail.com>
2016-06-29 12:19:48 +0900

    component template

---
+++ resources/assets/js/bell-create.js
@@ -0,0 +1,24 @@
+import React, {PropTypes} from 'react';
+import { connect } from 'react-redux';
+
+class BellCreate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+
+BellCreate.contextTypes = {
+  router: PropTypes.object.isRequired
+};
+BellCreate.propTypes = {
+};
+
+const mapStateToProps = (state) => {
+  return Object.assign({}, state);
+};
+
+export default connect(mapStateToProps)(BellCreate);
