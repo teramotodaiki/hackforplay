f2a07675e83d07a5dc8f9c59a5c893d476c65669
teramotodaiki <i101323@gmail.com>
2016-06-30 14:17:56 +0900

    make controller

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class StaticController extends Controller
+{
+  public function __construct()
+  {
+
+  }
+
+  /**
+   * @param $path file path from
+   */
+  public function index($path)
+  {
+    
+  }
+}
