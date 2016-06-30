30f9363b155b07168c1795e9b7d0538f06fec108
teramotodaiki <i101323@gmail.com>
2016-06-30 14:20:30 +0900

    Add content type from extension

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -19,13 +19,37 @@ class StaticController extends Controller
   public function index($path)
   {
     $filepath =  "{$this->root}{$path}";
+    $pathinfo = pathinfo($path);
 
+    if (empty($pathinfo['extension'])) {
+      return response('', 404);
+    }
     if (!file_exists($filepath)) {
       return response('', 404);
     }
 
+    $type = [
+      'txt' => 'text/plain',
+      'htm' => 'text/html',
+      'html' => 'text/html',
+      'css' => 'text/css',
+      'js' => 'application/javascript',
+      'json' => 'application/json',
+      'xml' => 'application/xml',
+      // images
+      'png' => 'image/png',
+      'jpeg' => 'image/jpeg',
+      'jpg' => 'image/jpeg',
+      'gif' => 'image/gif',
+      'ico' => 'image/vnd.microsoft.icon',
+      // audio/video
+      'mp3' => 'audio/mpeg',
+      'wav' => 'audio/wav',
+    ][$pathinfo['extension']];
+
     $content = file_get_contents($filepath);
 
-    return response($content, 200);
+    return response($content, 200)
+            ->header('Content-Type', $type);
   }
 }
