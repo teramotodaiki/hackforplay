4c3cd61b1c4b93544431a12e0ab809b14822bbfa
teramotodaiki <i101323@gmail.com>
2016-07-03 21:16:29 +0900

    Revert "debug"
    This reverts commit 83da040baba35f8bcfff594b8db13a8cf27202bb.

--- resources/assets/js/actions/index.js
+++ resources/assets/js/actions/index.js
@@ -32,7 +32,6 @@ export const fetchChannel = ({ id, chats }) => {
 export const fetchChannels = (query) => {
   return (dispatch) => {
 
-    console.log('query', query);
     return request
       .get('/channels')
       .query(query)
