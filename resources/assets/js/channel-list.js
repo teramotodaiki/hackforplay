24c2b9d36fe1894fbe546094a15c980a5753a376
teramotodaiki <i101323@gmail.com>
2016-07-03 21:24:53 +0900

    Revert "debug"
    This reverts commit b6d98f3e48ec23b74886f343c47aa9cfcf2a3285.

--- resources/assets/js/channel-list.js
+++ resources/assets/js/channel-list.js
@@ -53,14 +53,8 @@ class ChannelList extends Component {
     const { nextPage, isLoading } = this.state;
 
     const sorted = Object.keys(channels)
-    .forEach((item) => console.log('1', item))
     .map((key) => channels[key])
-    .forEach((item) => console.log('2', item))
-    .filter((channel) => {
-      console.log(channel.ID, channel.is_private);
-      return !channel.is_private;
-    })
-    .forEach((item) => console.log('3', item))
+    .filter((channel) => !channel.is_private)
     .sort((a, b) => {
       return (
         a.updated_at == null ? 1 :
@@ -68,11 +62,9 @@ class ChannelList extends Component {
         a.updated_at < b.updated_at ? 1 : -1
       );
     })
-    .forEach((item) => console.log('4', item))
     .map((channel) => {
       return <ChannelCard key={channel.ID} {...channel}></ChannelCard>;
-    })
-    .forEach((item) => console.log('5', item));
+    });
 
     const next = nextPage ? (
       <Button
