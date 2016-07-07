83aa45ae81bc34173ecc35494039318825a92942
teramotodaiki <i101323@gmail.com>
2016-07-07 13:56:41 +0900

    add columns

---
+++ database/migrations/2016_07_07_044650_add_project_column.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProjectColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      if (Schema::hasTable('project')) {
+        Schema::table('project', function (Blueprint $table)
+        {
+          $table->string('title', 50)->nullable();
+          $table->string('description', 400)->nullable();
+          $table->string('gist_id', 20)->nullable();
+          $table->string('thumbnail', 100)->nullable();
+          $table->timestamps();
+        });
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      if (Schema::hasTable('project')) {
+        Schema::table('project', function (Blueprint $table)
+        {
+          $table->dropColumn([
+            'title',
+            'description',
+            'gist_id',
+            'thumbnail',
+            'created_at',
+            'updated_at',
+          ]);
+        });
+      }
+    }
+}
