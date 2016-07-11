07c984c807e4e0e6aa139bb6088d0fb070c24c63
teramotodaiki <i101323@gmail.com>
2016-07-11 17:36:23 +0900

    put flag

--- resources/views/vendor/hackforplay/stage/putclearedbytoken.php
+++ resources/views/vendor/hackforplay/stage/putclearedbytoken.php
@@ -10,7 +10,7 @@ if (!$token) {
 }
 
 // Fetch playlog
-$stmt	= $dbh->prepare('SELECT "ID","Cleared" FROM "PlayLog" WHERE "Token"=:token');
+$stmt	= $dbh->prepare('SELECT "ID","Cleared","StageID" FROM "PlayLog" WHERE "Token"=:token');
 $stmt->bindValue(":token", $token, PDO::PARAM_STR);
 $stmt->execute();
 $playlog = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -24,6 +24,12 @@ if (!$playlog) {
 $stmt	= $dbh->prepare('UPDATE "PlayLog" SET "Cleared"=:gmt WHERE "ID"=:id');
 $stmt->bindValue(":gmt", gmdate('Y-m-d H:i:s'), PDO::PARAM_STR);
 $stmt->bindValue(":id", $playlog['ID'], PDO::PARAM_INT);
+$stmt->execute() or die('database-error');
+
+// Set is_clearable flag
+$stmt	= $dbh->prepare('UPDATE "Stage" SET "is_clearable"=:ok WHERE "ID"=:id');
+$stmt->bindValue(":ok", 1, PDO::PARAM_INT);
+$stmt->bindValue(":id", $playlog['StageID'], PDO::PARAM_INT);
 $stmt->execute() or die('database-error');
 
 echo 'success';
