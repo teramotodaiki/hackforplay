e22a429ddeefc7748cd3fb549ab272488a74312a
teramotodaiki <i101323@gmail.com>
2016-07-14 11:35:46 +0900

    Need login

--- resources/assets/js/components/header.js
+++ resources/assets/js/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { AppBar, IconMenu, MenuItem, Divider, IconButton, FontIcon } from 'material-ui';
+import { AppBar, IconMenu, MenuItem, Divider, IconButton, FontIcon, FlatButton } from 'material-ui';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const statics = {
@@ -38,25 +38,18 @@ const statics = {
 class Header extends Component {
 
   render() {
+    const meta = document.querySelector('meta[name="login-user-id"]');
+    const user_id = meta ? meta.getAttribute('content') : null;
+
     const style = Object.assign({
       position: 'fixed',
     }, this.props.style);
-
-    const menu = statics.userMenu.map((block) => {
-      return block.map((item) => (
-        <MenuItem
-          key={item.text}
-          primaryText={item.text}
-          onTouchTap={() => window.location.href = item.href}
-          />
-      ));
-    }).reduce((p, c, i) => p.concat(<Divider key={'Divider-' + i} />, c));
 
     return (
       <div>
         <AppBar
           title={this.props.title}
-          iconElementRight={
+          iconElementRight={user_id ? (
             <IconMenu
               iconButtonElement={
                 <IconButton>
@@ -65,10 +58,23 @@ class Header extends Component {
               }
               anchorOrigin={{"horizontal":"right","vertical":"bottom"}}
               targetOrigin={{"horizontal":"right","vertical":"top"}}>
-              {menu}
-            </IconMenu>
-          }
-          />
+              {statics.userMenu.map((block) => {
+                return block.map((item) => (
+                  <MenuItem
+                    key={item.text}
+                    primaryText={item.text}
+                    onTouchTap={() => window.location.href = item.href}
+                    />
+                ));
+              }).reduce((p, c, i) => p.concat(<Divider key={'Divider-' + i} />, c))}
+            </IconMenu>) : (
+              <FlatButton
+                label="Login"
+                onTouchTap={() => window.location.href = '/login'}
+                />
+            )
+          }>
+        </AppBar>
         <div style={{ height: this.context.muiTheme.appBar.height }}></div>
       </div>
     );
