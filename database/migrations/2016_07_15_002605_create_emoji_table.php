963b2a972e2135ae1cdcd4be35d38fbb256fd32d
teramotodaiki <i101323@gmail.com>
2016-07-15 12:19:39 +0900

    schema

---
+++ database/migrations/2016_07_15_002605_create_emoji_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmojiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      if (!Schema::hasTable('emojis')) {
+        Schema::create('emojis', function (Blueprint $table)
+        {
+          $table->bigIncrements('id');
+          $table->bigInteger('stage_id')->unsigned();
+          $table->foreign('stage_id')->references('id')->on('stage');
+          $table->bigInteger('user_id')->unsigned();
+          $table->foreign('user_id')->references('id')->on('user');
+          $table->string('shortcode', 50);
+          $table->timestamp('created_at');
+        });
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('emojis');
+    }
+}
