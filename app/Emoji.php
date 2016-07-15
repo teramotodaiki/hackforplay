d34079d6ea337d965bd69ce996cb626c7246d741
teramotodaiki <i101323@gmail.com>
2016-07-15 13:32:02 +0900

    fix model

--- app/Emoji.php
+++ app/Emoji.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Emoji extends Model
 {
-  protected $table = 'emoji';
   protected $fillable = ['stage_id', 'user_id', 'shortcode', 'created_at'];
   public $timestamps = false;
 }
