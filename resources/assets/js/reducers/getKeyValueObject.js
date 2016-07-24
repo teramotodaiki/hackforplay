e9e824921a38f4f1da539726d79a335cfcf83f3a
teramotodaiki <i101323@gmail.com>
2016-07-24 17:46:05 +0900

    divide function

---
+++ resources/assets/js/reducers/getKeyValueObject.js
@@ -0,0 +1,15 @@
+/**
+ * Input:
+ *  [{ id: 1, name: 'one' }, { id: 2, name: 'two' }]
+ * Output:
+ *  { '1': { id: 1, name: 'one' }, '2': { id: 2, name: 'two' } }
+*/
+
+export default (...args) => {
+  const key = 'id';
+  return Object.assign.apply(null, args.filter((item) => {
+    return typeof item === 'object' && key in item;
+  }).map((item) => {
+    return { [item[key]]: item };
+  }));
+};
