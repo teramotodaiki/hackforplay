878075b73577d8a65485c44bc42733c765eadc43
teramotodaiki <i101323@gmail.com>
2016-07-24 22:04:12 +0900

    fetch action

--- resources/assets/js/actions/users.js
+++ resources/assets/js/actions/users.js
@@ -1,2 +1,21 @@
+import request from './request';
+
+
 export const ADD_USER = 'ADD_USER';
 
+export const addUser = (user) => {
+  return { type: ADD_USER, user };
+};
+
+export const fetchUser = (id) => {
+  return (dispatch) => {
+
+    return request
+      .get('/api/users/' + id)
+      .then((result) => {
+        dispatch({ type: ADD_USER, user: result.body });
+        return result;
+      });
+      
+  };
+};
