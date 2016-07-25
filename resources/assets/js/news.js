653efa86664ad2b02278ff4fc248dcdbda3080cb
teramotodaiki <i101323@gmail.com>
2016-07-25 16:11:27 +0900

    Given theme in news

--- resources/assets/js/news.js
+++ resources/assets/js/news.js
@@ -1,20 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
 import { connect } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Paper } from 'material-ui';
-
-import Header from './components/header';
 
 export default class News extends Component {
   constructor(props) {
     super(props);
-  }
-
-  getChildContext() {
-    return { muiTheme: getMuiTheme(baseTheme) };
   }
 
   componentDidMount() {
@@ -24,10 +15,9 @@ export default class News extends Component {
   }
 
   render() {
-    const theme = getMuiTheme(baseTheme);
 
     const style = {
-      height: window.innerHeight - theme.appBar.height,
+      height: window.innerHeight - this.context.muiTheme.appBar.height,
       width: Math.min(520, window.innerWidth),
       marginLeft: 'auto',
       marginRight: 'auto',
@@ -35,21 +25,18 @@ export default class News extends Component {
     };
 
     return (
-      <MuiThemeProvider>
-        <div>
-          <Header title="News" openImmediately={window.innerWidth > 1033} />
-          <Paper style={style} zDepth={1}>
-            <a
-              className="twitter-timeline"
-              href="https://twitter.com/hashtag/hackforplay"
-              data-widget-id="753485868157906944"
-              width={style.width}
-              height={style.height}>
-              #hackforplay のツイート
-            </a>
-          </Paper>
-        </div>
-      </MuiThemeProvider>
+      <div>
+        <Paper style={style} zDepth={1}>
+          <a
+            className="twitter-timeline"
+            href="https://twitter.com/hashtag/hackforplay"
+            data-widget-id="753485868157906944"
+            width={style.width}
+            height={style.height}>
+            #hackforplay のツイート
+          </a>
+        </Paper>
+      </div>
     );
   }
 }
@@ -57,7 +44,7 @@ export default class News extends Component {
 News.propTypes = {
 };
 
-News.childContextTypes = {
+News.contextTypes = {
   muiTheme: PropTypes.object.isRequired,
 };
 
