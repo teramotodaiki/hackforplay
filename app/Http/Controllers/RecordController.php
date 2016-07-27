02ee4468fb9b172da16c5a7f8a6b604643219c42
teramotodaiki <i101323@gmail.com>
2016-07-27 21:24:44 +0900

    index api

---
+++ app/Http/Controllers/RecordController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Record;
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+      $size = $request->has('size') ? $request->input('size') : 15;
+      $records = Record::orderBy('time', 'desc');
+      return response($records->simplePaginate($size), 200);
+    }
+}
