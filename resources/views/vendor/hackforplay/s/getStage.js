86fb4f9fdfc5ad7b6b27a13e61deef4f01bb8d68
teramotodaiki <i101323@gmail.com>
2016-07-30 17:13:35 +0900

    getStage

---
+++ resources/views/vendor/hackforplay/s/getStage.js
@@ -0,0 +1,38 @@
+(function () {
+
+  // on memory
+  var state = new State();
+
+  // Global interface of state
+  window.getStage = function (id) {
+
+    return state.get(id) ?
+    // hit
+    (new $.Deferred).resolve(state.get(id)) :
+    // miss
+    $.ajax({
+      type: 'GET',
+      url: `/api/stages/${id}`,
+    })
+    .done(function (result) {
+      state.set(result);
+      return result;
+    });
+
+  };
+
+  function State () {
+    var key = 'cached-stages';
+    var prefix = 'id_';
+    var state = $.parseJSON(localStorage.getItem(key) || '{}');
+
+    this.get = function (id) {
+      return state[prefix + id];
+    }
+    this.set = function (stage) {
+      state[prefix + stage.id] = stage;
+      var json = JSON.stringify(state);
+      localStorage.setItem(key, json);
+    }
+  };
+})();
