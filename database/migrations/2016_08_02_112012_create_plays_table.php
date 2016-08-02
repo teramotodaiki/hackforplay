7ed09cd6e5e8303e7527afa5ecc6a75622f4899d
teramotodaiki <i101323@gmail.com>
2016-08-02 21:29:43 +0900

    play table migration

---
+++ database/migrations/2016_08_02_112012_create_plays_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      if (!Schema::hasTable('plays')) {
+        Schema::create('plays', function (Blueprint $table)
+        {
+          $table->bigIncrements('id');
+
+          $table->bigInteger('user_id')->unsigned()->nullable();
+          $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+          $table->bigInteger('stage_id')->unsigned()->nullable();
+          $table->foreign('stage_id')->references('id')->on('stage')->onDelete('cascade');
+
+          $table->string('referrer', 100)->nullable();
+          $table->boolean('is_cleared')->default(false);
+
+          $table->string('token', 32)->unique()->nullable();
+          $table->timestamps();
+        });
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('plays');
+    }
+}
