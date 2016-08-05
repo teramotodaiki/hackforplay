eb50c1becf7db5df287d447dbcd74666b98eabed
teramotodaiki <i101323@gmail.com>
2016-08-05 22:23:01 +0900

    make assign function

--- resources/assets/js/reducers/authors.js
+++ resources/assets/js/reducers/authors.js
@@ -25,27 +25,31 @@ export const authors = (state = {}, action) => {
   }
 };
 
-const assignRecursive = (state, author, plug) => {
-  if (plug) {
-    // 子を追加. 現時点のparent nodeがあれば引き継ぐ, なければ仮オブジェクトを作成する
-    const _author = state[plug.author_id] || { id: plug.author_id };
-    const _state = Object.assign({}, state, {
-      [_author.id]: Object.assign({}, _author, {
-        plugs: Object.assign({}, _author.plugs, {
-          [plug.id]: plug,
+const makeAssignRecursive = (parentName, childName) => {
+  return (state, parent, child) => {
+    if (child) {
+      // 子を追加. 現時点のparent nodeがあれば引き継ぐ, なければ仮オブジェクトを作成する
+      const _parent = state[child[`${parentName}_id`]] || { id: child[`${parentName}_id`] };
+      const _state = Object.assign({}, state, {
+        [_parent.id]: Object.assign({}, _parent, {
+          [`${childName}s`]: Object.assign({}, _parent[`${childName}s`], {
+            [child.id]: child,
+          })
         })
-      })
-    });
-    return assignRecursive(_state, author, null);
-  }
+      });
+      return assignRecursive(_state, parent, null);
+    }
 
-  if (author) {
-    // 子を持つ親を追加. 古いnodeは抹消し、子nodeだけを引き継いだ新たなnodeを作成する.
-    const plugs = 'plugs' in state[author.id] ? state[author.id].plugs : {};
-    return Object.assign({}, state, {
-      [author.id]: Object.assign({}, author, { plugs })
-    });
-  }
+    if (parent) {
+      // 子を持つ親を追加. 古いnodeは抹消し、子nodeだけを引き継いだ新たなnodeを作成する.
+      const _children = `${childName}s` in state[parent.id] ? state[parent.id][`${childName}s`] : {};
+      return Object.assign({}, state, {
+        [parent.id]: Object.assign({}, parent, { [`${childName}s`]: _children })
+      });
+    }
 
-  return state;
+    return state;
+  };
 };
+
+const assignRecursive = makeAssignRecursive('author', 'plug');
