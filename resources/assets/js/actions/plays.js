087e24c3dc8200297891a42125f16d7fbfd09137
teramotodaiki <i101323@gmail.com>
2016-08-06 09:09:20 +0900

    actions

--- resources/assets/js/actions/plays.js
+++ resources/assets/js/actions/plays.js
@@ -1,3 +1,21 @@
 import request from './request';
 
 export const ADD_PLAY = 'ADD_PLAY';
+
+export const addPlay = (play) => {
+  return { type: ADD_PLAY, play };
+};
+
+export const fetchPlays = ({ page = 1 } = {}) => {
+  return (dispatch) => {
+
+    return request
+      .get('/api/plays')
+      .query({ page })
+      .then((result) => {
+        result.body.data.forEach((play) => dispatch(addPlay(play)));
+        return result;
+      });
+
+  };
+};
