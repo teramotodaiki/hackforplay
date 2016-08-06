e8a6a2c4fe20da2e41697d5985424761611823df
teramotodaiki <i101323@gmail.com>
2016-08-06 09:09:10 +0900

    reducer

--- resources/assets/js/reducers/plays.js
+++ resources/assets/js/reducers/plays.js
@@ -6,3 +6,18 @@
  */
 
 import { ADD_PLAY } from '../actions/';
+
+export const plays = (state = {}, action) => {
+
+  switch (action.type) {
+    case ADD_PLAY:
+
+      return Object.assign({}, state, {
+        [action.play.id]: action.play
+      });
+
+    default:
+      return state;
+  }
+
+};
