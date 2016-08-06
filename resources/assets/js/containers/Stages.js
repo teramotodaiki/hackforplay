478bde28499443505c094229c43c2498f4f92e8c
teramotodaiki <i101323@gmail.com>
2016-08-06 22:27:06 +0900

    gives project

--- resources/assets/js/containers/Stages.js
+++ resources/assets/js/containers/Stages.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 import { connect } from 'react-redux';
 
-import { fetchPlays, fetchStageIfNeeded } from '../actions/';
+import { fetchPlays, fetchStageIfNeeded, fetchProjectIfNeeded } from '../actions/';
 import StageCard from '../components/StageCard';
 import Progress from '../components/Progress';
 
@@ -33,12 +33,15 @@ export default class Stages extends Component {
       .map((id) => plays[id].stage_id)
       .filter((stage_id, i, self) => self.indexOf(stage_id) === i)
       .map((stage_id) => dispatch(fetchStageIfNeeded(stage_id)))
-      .map((stage) => (
-        <StageCard
-          key={stage.id}
-          stage={stage}
-          isOwner={authUser.id === stage.user_id} />
-      ));
+      .map((stage) => {
+        return (
+          <StageCard
+            key={stage.id}
+            stage={stage}
+            isOwner={authUser.id === stage.user_id}
+            project={authUser.id === stage.user_id ? dispatch(fetchProjectIfNeeded(stage.project_id)) : null} />
+        );
+      });
 
     return (
       <div style={containerStyle}>
