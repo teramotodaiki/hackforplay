9b45f2adb4d00ae30371dafcfdfa7cdae32a28dd
teramotodaiki <i101323@gmail.com>
2016-08-06 17:30:47 +0900

    display all stages

--- resources/assets/js/containers/Stages.js
+++ resources/assets/js/containers/Stages.js
@@ -2,15 +2,41 @@ import React, { Component, PropTypes } from 'react';
 
 import { connect } from 'react-redux';
 
+import { fetchPlays, fetchStageIfNeeded } from '../actions/';
+import StageCard from '../components/StageCard';
+
 export default class Stages extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    const { dispatch, plays } = this.props;
+    if (!Object.keys(plays).length) {
+      dispatch(fetchPlays());
+    }
+  }
+
   render() {
+
+    const { dispatch, plays } = this.props;
+
+    const containerStyle = Object.assign({}, this.props.containerStyle, {
+      paddingLeft: 60,
+      paddingRight: 60,
+    });
+
+    const stageCards = Object.keys(plays)
+      .sort((a, b) => b - a)
+      .filter((id) => plays[id].deleted_at === null)
+      .map((id) => plays[id].stage_id)
+      .filter((stage_id, i, self) => self.indexOf(stage_id) === i)
+      .map((stage_id) => dispatch(fetchStageIfNeeded(stage_id)))
+      .map((stage) => (<StageCard key={stage.id} stage={stage} />));
+
     return (
-      <div style={this.props.containerStyle}>
-        Content
+      <div style={containerStyle}>
+        {stageCards}
       </div>
     );
   }
