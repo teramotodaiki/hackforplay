977e87bc1006faaea4555e08f91c033833a07d81
teramotodaiki <i101323@gmail.com>
2016-08-10 16:56:59 +0900

    gives context to stage card

--- resources/assets/js/components/StageCard.js
+++ resources/assets/js/components/StageCard.js
@@ -1,56 +1,73 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Card, CardHeader, CardActions, CardText, FlatButton, Avatar, FontIcon, FloatingActionButton } from 'material-ui';
-import {blue500} from 'material-ui/styles/colors';
 import AssignmentInd from 'material-ui/svg-icons/action/assignment-ind';
 import PlayArrow from 'material-ui/svg-icons/av/play-arrow';
 import FolderOpen from 'material-ui/svg-icons/file/folder-open';
 
-export default ({ stage, isOwner, project, style }) => {
+export default class StageCard extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const cardStyle = Object.assign({
-    width: 480,
-    minWidth: 480,
-  }, style);
+  render() {
+    const { stage, isOwner, project, style } = this.props;
 
-  const wrapStyle = {
-    overflowX: 'hidden',
-    textOverflow: 'ellipsis',
-    maxWidth: cardStyle.width / 2 >> 0,
-  };
+    const cardStyle = Object.assign({
+      width: 480,
+      minWidth: 480,
+    }, style);
 
-  const playButton = (
-    <FloatingActionButton
-      onTouchTap={() => location.href = "/s?id=" + stage.id}>
-      {stage.thumbnail ? (<img src={stage.thumbnail} alt="Play" />) : (<PlayArrow />)}
-    </FloatingActionButton>
-  );
+    const wrapStyle = {
+      overflowX: 'hidden',
+      textOverflow: 'ellipsis',
+      maxWidth: cardStyle.width / 2 >> 0,
+    };
 
-  const ownerActions = (
-    <CardActions style={{ paddingLeft: 16, paddingBottom: 14 }}>
+    const playButton = (
       <FloatingActionButton
-        onTouchTap={() => {
-          sessionStorage.setItem('project-token', project.token);
-      		location.href = '/s?id=' + stage.source_id + '&mode=restaging';
-        }}
-        disabled={!project || !project.token}
-        mini={true} secondary={true}>
-        <FolderOpen />
+        onTouchTap={() => location.href = "/s?id=" + stage.id}>
+        {stage.thumbnail ? (<img src={stage.thumbnail} alt="Play" />) : (<PlayArrow />)}
       </FloatingActionButton>
-    </CardActions>
-  );
+    );
 
-  return (
-    <Card style={cardStyle}>
-      <CardHeader
-        title={stage.title || '...'}
-        subtitle={stage.explain || ''}
-        avatar={playButton}
-        titleStyle={wrapStyle}
-        subtitleStyle={wrapStyle}
-      >
-        {isOwner ? <AssignmentInd color={blue500} /> : null}
-      </CardHeader>
-      {isOwner && ownerActions}
-    </Card>
-  );
+    const ownerActions = (
+      <CardActions style={{ paddingLeft: 16, paddingBottom: 14 }}>
+        <FloatingActionButton
+          onTouchTap={() => {
+            sessionStorage.setItem('project-token', project.token);
+        		location.href = '/s?id=' + stage.source_id + '&mode=restaging';
+          }}
+          disabled={!project || !project.token}
+          mini={true} secondary={true}>
+          <FolderOpen />
+        </FloatingActionButton>
+      </CardActions>
+    );
+
+    return (
+      <Card style={cardStyle}>
+        <CardHeader
+          title={stage.title || '...'}
+          subtitle={stage.explain || ''}
+          avatar={playButton}
+          titleStyle={wrapStyle}
+          subtitleStyle={wrapStyle}
+        >
+          {isOwner ? <AssignmentInd color={this.context.muiTheme.palette.primary2Color} /> : null}
+        </CardHeader>
+        {isOwner && ownerActions}
+      </Card>
+    );
+  }
+}
+
+StageCard.propTypes = {
+  stage: PropTypes.object.isRequired,
+  isOwner: PropTypes.bool,
+  project: PropTypes.object,
+  style: PropTypes.object,
+};
+
+StageCard.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
 };
