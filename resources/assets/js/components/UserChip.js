fdee2131224b07510b26b1687cdf90debe470f59
teramotodaiki <i101323@gmail.com>
2016-08-10 18:31:36 +0900

    user chip view

---
+++ resources/assets/js/components/UserChip.js
@@ -0,0 +1,39 @@
+import React, { PropTypes, Component } from 'react';
+
+import { Chip, Avatar } from 'material-ui';
+import AssignmentInd from 'material-ui/svg-icons/action/assignment-ind';
+import Face from 'material-ui/svg-icons/action/face';
+
+export default class UserChip extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { user, isOwner } = this.props;
+    const { palette } = this.context.muiTheme;
+
+    return (
+      <Chip
+        style={{ display: 'inline-block' }}
+        labelStyle={{ verticalAlign: 'bottom' }}
+        >
+        {isOwner ? (
+          <Avatar icon={<AssignmentInd />} backgroundColor={palette.accent3Color} />
+        ) : (
+          <Avatar icon={<Face />} />
+        )}
+        {user.nickname}
+      </Chip>
+    );
+  }
+}
+
+UserChip.propTypes = {
+  user: PropTypes.object.isRequired,
+  isOwner: PropTypes.bool.isRequired,
+};
+
+UserChip.contextTypes = {
+  muiTheme: PropTypes.object.isRequired
+};
