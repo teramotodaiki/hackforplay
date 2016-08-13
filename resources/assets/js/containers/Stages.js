a3e22e50388e53b0f85dffcf0c3e95a569638092
teramotodaiki <i101323@gmail.com>
2016-08-13 09:34:57 +0900

    tabs view

--- resources/assets/js/containers/Stages.js
+++ resources/assets/js/containers/Stages.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 import { connect } from 'react-redux';
+import { Tabs, Tab } from 'material-ui';
 
 import {
   fetchPlays,
@@ -38,9 +39,33 @@ export default class Stages extends Component {
     });
   }
 
-  render() {
+  getStageCardList({ is_mod, style }) {
+    const { dispatch, plays, authUser } = this.props;
+    const keyArrayOfPlays = Object.keys(plays);
 
-    const { dispatch, plays, authUser, containerStyle } = this.props;
+    return !keyArrayOfPlays.length ? null :
+      keyArrayOfPlays
+      .sort((a, b) => b - a)
+      .filter((id) => plays[id].deleted_at === null)
+      .map((id) => plays[id].stage_id)
+      .filter((stage_id, i, self) => self.indexOf(stage_id) === i)
+      .map((stage_id) => dispatch(getStageFromLocal(stage_id)))
+      .filter((stage) => !!+stage.is_mod === is_mod)
+      .map((stage) => (
+        <StageCard
+          key={stage.id}
+          stage={stage}
+          style={style}
+          isOwner={authUser.id == stage.user_id}
+          project={authUser.id == stage.user_id && stage.project_id ? dispatch(getProjectFromLocal(stage.project_id)) : null}
+          user={stage.user_id && dispatch(getUserFromLocal(stage.user_id))}
+          handleStageUpdate={(change) => dispatch(updateStage(stage.id, change))}
+          />
+      ));
+  }
+
+  render() {
+    const { dispatch, authUser, containerStyle } = this.props;
 
     const style = Object.assign({}, containerStyle, {
       paddingLeft: 60,
@@ -52,28 +77,22 @@ export default class Stages extends Component {
       width: style.width - style.paddingLeft - style.paddingRight
     };
 
-    const stageCards = Object.keys(plays)
-      .sort((a, b) => b - a)
-      .filter((id) => plays[id].deleted_at === null)
-      .map((id) => plays[id].stage_id)
-      .filter((stage_id, i, self) => self.indexOf(stage_id) === i)
-      .map((stage_id) => dispatch(getStageFromLocal(stage_id)))
-      .map((stage) => (
-        <StageCard
-          key={stage.id}
-          stage={stage}
-          style={cardStyle}
-          isOwner={authUser.id == stage.user_id}
-          project={authUser.id == stage.user_id && stage.project_id ? dispatch(getProjectFromLocal(stage.project_id)) : null}
-          user={stage.user_id && dispatch(getUserFromLocal(stage.user_id))}
-          handleStageUpdate={(change) => dispatch(updateStage(stage.id, change))} />
-      ));
-
     return (
       <div style={style}>
-        {stageCards.length ? stageCards : (
-          <Progress containerStyle={containerStyle} />
-        )}
+        <Tabs>
+          <Tab label="PRODUCT">
+            {
+              this.getStageCardList({ is_mod: false, style: cardStyle }) ||
+              (<Progress containerStyle={containerStyle} />)
+            }
+          </Tab>
+          <Tab label="MOD">
+            {
+              this.getStageCardList({ is_mod: true, style: cardStyle }) ||
+              (<Progress containerStyle={containerStyle} />)
+            }
+          </Tab>
+        </Tabs>
       </div>
     );
   }
