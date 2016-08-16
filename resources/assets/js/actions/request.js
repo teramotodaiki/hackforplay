5f99f767017eb7ffe262040b1aa89a3af6a824b0
teramotodaiki <i101323@gmail.com>
2016-08-16 14:47:24 +0900

    all request has csrf token

--- resources/assets/js/actions/request.js
+++ resources/assets/js/actions/request.js
@@ -1,2 +1,18 @@
+import request from 'superagent-use';
 import superagentPromisePlugin from 'superagent-promise-plugin';
-export default superagentPromisePlugin.patch(require('superagent'));
+
+request.use(superagentPromisePlugin);
+request.use((req) => {
+  req.set('X-Requested-With', 'XMLHttpRequest');
+  req.set('X-CSRF-TOKEN', getCsrfToken());
+  return req;
+})
+
+var _csrfToken = null;
+const getCsrfToken = () => {
+  if (_csrfToken) return _csrfToken || '';
+  const element = document.querySelector('meta[name="csrf-token"]');
+  return _csrfToken = element && element.getAttribute('content');
+};
+
+export default request;
