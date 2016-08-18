be8aa3f1ee537ee1bb6d22a1f1f24d0fedb89fa9
teramotodaiki <i101323@gmail.com>
2016-08-18 16:11:32 +0900

    router link

--- resources/assets/js/components/channel-card.js
+++ resources/assets/js/components/channel-card.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-import { Card, CardHeader, CardMedia, Avatar, FlatButton } from 'material-ui';
+import {
+  Card, CardHeader, CardMedia, CardTitle,
+  FlatButton,
+} from 'material-ui';
 import PermIdentity from 'material-ui/svg-icons/action/perm-identity';
 
-export default ({channel, user}) => {
 
-  const style = {
-    width: 320,
-    marginTop: 10,
-    marginLeft: 10,
-  };
+export default class ChannelCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Card style={style}>
-      <CardHeader
-        subtitle={channel.description}
-        avatar={<Avatar icon={<PermIdentity />} />}
+  render() {
+    const { channel, user } = this.props;
+    const { router } = this.context;
+
+    const style = {
+      width: 320,
+      marginTop: 10,
+      marginLeft: 10,
+    };
+
+    const mediaStyle = {
+      cursor: 'pointer',
+    };
+
+    const userButton = (
+      <FlatButton
+        label={user && user.nickname}
+        onTouchTap={() => location.href = '/m?id=' + user.id}
+        icon={<PermIdentity />}
       />
-      <CardMedia>
-        <img src={channel.thumbnail} />
-      </CardMedia>
-    </Card>
-  );
+    );
+
+    return (
+      <Card style={style}>
+        <CardHeader
+          title={userButton}
+        />
+        <CardMedia
+          style={mediaStyle}
+          overlay={<CardTitle title={channel.description} />}
+          onTouchTap={() => router.push(`/channels/${channel.id}/watch`)}
+        >
+          <img src={channel.thumbnail} />
+        </CardMedia>
+      </Card>
+    );
+  }
 }
+
+ChannelCard.propTypes = {
+};
+
+ChannelCard.contextTypes = {
+  router: PropTypes.object.isRequired,
+};
