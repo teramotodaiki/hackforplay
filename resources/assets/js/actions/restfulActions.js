4f642e748f19158d7f77810be19fa7277ba26886
teramotodaiki <i101323@gmail.com>
2016-08-25 17:46:51 +0900

    template RESTful API action creator

---
+++ resources/assets/js/actions/restfulActions.js
@@ -0,0 +1,53 @@
+import { createAction } from 'redux-actions';
+import request from './request';
+
+export default (name) => {
+
+  const prefix = `/api/${name}`;
+
+  return {
+    index: (action) =>
+    ({ page = 1 } = {}) =>
+      (dispatch) =>
+        request
+          .get(prefix)
+          .query({ page })
+          .then((result) => {
+            result.body.data.forEach((payload) => dispatch(action(payload)));
+            return result;
+          }),
+
+    show: (action) =>
+    ({ id }) =>
+      (dispatch) =>
+        request
+          .get(prefix + '/' + id)
+          .then((result) => {
+            dispatch(action(result.body));
+            return result;
+          }),
+
+    update: (action) =>
+    (id, change) =>
+      (dispatch) =>
+        request
+          .put(prefix + '/' + id)
+          .send(change)
+          .then((result) => {
+            dispatch(action(result.body));
+            return result;
+          }),
+
+    destroy: (action) =>
+    ({ id }) =>
+      (dispatch) =>
+        request
+          .del(prefix + '/' + id)
+          .then((result) => {
+            dispatch(action({ id }));
+            return result;
+          }),
+
+  };
+
+};
