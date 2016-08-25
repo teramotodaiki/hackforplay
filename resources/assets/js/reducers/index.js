ef2cc1f1006530b08769282a16802c3f29fcef4c
teramotodaiki <i101323@gmail.com>
2016-08-25 23:13:14 +0900

    plug and author reducers

--- resources/assets/js/reducers/index.js
+++ resources/assets/js/reducers/index.js
@@ -4,5 +4,6 @@ export * from './projects';
 export * from './stages';
 export * from './users';
 export * from './authors';
+export * from './plugs';
 export * from './plays';
 export * from './channels';
