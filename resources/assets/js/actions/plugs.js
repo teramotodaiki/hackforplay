2b2b923dd0797ee8bb2dcf35b51fc68e364f27d5
teramotodaiki <i101323@gmail.com>
2016-08-26 21:21:17 +0900

    add rest action creator

--- resources/assets/js/actions/plugs.js
+++ resources/assets/js/actions/plugs.js
@@ -10,4 +10,5 @@ export const deletePlug = createAction('DELETE_PLUG');
 export const indexPlug = REST.index(setPlug);
 export const showPlug = REST.show(setPlug);
 export const updatePlug = REST.update(setPlug);
+export const storePlug = REST.store(setPlug);
 export const destroyPlug = REST.destroy(deletePlug);
